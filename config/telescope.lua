bd7ed47a15827101e260d1788b23b75383cdd8b2
TuanHa <tuan.ha@tomosia.com>
2022-10-27 10:15:38 +0700

    update

--- config/telescope.lua
+++ config/telescope.lua
@@ -1,12 +1,8 @@
 local M = {}
 local telescope = require "telescope"
-local yanky, _ = pcall(require, "yanky")
 
 M.setup = function()
   telescope.load_extension "project"
-  if yanky then
-    telescope.load_extension "yank_history"
-  end
 end
 
 return M
--- config/yanky.lua
+++ config/yanky.lua
@@ -3,6 +3,9 @@ local ok, telescope = pcall(require, "telescope")
 
 M.setup = function()
   require("yanky").setup {}
+  if ok then
+    telescope.load_extension "yank_history"
+  end
   vim.keymap.set({ "n", "x" }, "y", "<Plug>(YankyYank)")
 end
 
