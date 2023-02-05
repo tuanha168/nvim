fdc7a91809cc4baaf67a003ea77dabac737d92c0
TuanHa <achicir2@gmail.com>
2023-02-06 02:07:28 +0700

    update

--- config/mason.lua
+++ config/mason.lua
@@ -6,13 +6,14 @@ local M = {
 
     -- web dev
     "typescript-language-server",
-    "angular-language-server",
+    -- "angular-language-server",
     -- "vetur-vls",
     "vue-language-server",
     "css-lsp",
     "html-lsp",
     "emmet-ls",
     "json-lsp",
+
     -- lint
     "eslint-lsp",
     "eslint_d",
