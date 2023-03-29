370bd2eed7b9890633e998300fc0981092703201
TuanHa <achicir2@gmail.com>
2023-03-29 16:17:56 +0700

    update

--- plugins/core.lua
+++ plugins/core.lua
@@ -117,6 +117,24 @@ return {
     },
   },
 
+  {
+    "chrisgrieser/nvim-spider",
+    event = "BufEnter",
+    config = function()
+      -- Keymaps
+      vim.keymap.set({ "n", "o", "x" }, "w", function() require("spider").motion "w" end, { desc = "Spider-w" })
+      vim.keymap.set({ "n", "o", "x" }, "e", function() require("spider").motion "e" end, { desc = "Spider-e" })
+      vim.keymap.set({ "n", "o", "x" }, "b", function() require("spider").motion "b" end, { desc = "Spider-b" })
+      vim.keymap.set({ "n", "o", "x" }, "ge", function() require("spider").motion "ge" end, { desc = "Spider-ge" })
+    end,
+  },
+
+  {
+    "chrisgrieser/nvim-various-textobjs",
+    event = "BufEnter",
+    opts = { useDefaultKeymaps = true },
+  },
+
   -- {
   --   "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
   --   event = "BufEnter",
