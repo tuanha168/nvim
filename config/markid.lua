6310e6a1a237d4b65c655d3cdfa9e3d123205754
TuanHa <achicir2@gmail.com>
2023-04-11 13:49:51 +0700

    update

--- config/markid.lua
+++ config/markid.lua
@@ -1,11 +1,15 @@
 return function()
   local m = require "markid"
   m.queries.vue = m.queries.typescript
+  m.queries.php = [[
+    (variable_name) @markid
+  ]]
   local configs = require "nvim-treesitter.configs"
   configs.setup {
     markid = {
       enable = true,
       colors = m.colors.bright,
+      query = m.queries,
     },
   }
 end
