ebe39b47857c73abae148e7d420d7bcbd46b2c56
TuanHa <achicir2@gmail.com>
2023-04-14 16:33:49 +0700

    update

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -1,8 +1,8 @@
 return {
   {
     "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      require "plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+    config = function(--[[ plugin, opts ]])
+      -- require "plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
       require("luasnip.loaders.from_vscode").lazy_load { paths = { "./lua/user" } } -- load snippets paths
     end,
   },
