71e4edc6a48016b120edde8be1d0e098702cbb9e
TuanHa <achicir2@gmail.com>
2023-04-25 16:51:36 +0700

    update

--- link.sh
+++
@@ -1,5 +0,0 @@
-if [[ $1 == 'astro' ]]; then
-  ln -s ${HOME}/.config/nvim/lua/user/ftplugin ${HOME}/.config/nvim/
-elif [[ $1 == 'nvchad' ]]; then
-  ln -s ${HOME}/.config/nvim/lua/custom/ftplugin ${HOME}/.config/nvim/
-fi
