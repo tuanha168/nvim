fa170cfdcc96a01be76ec32275696a2fe6056b52
TuanHa <achicir2@gmail.com>
2023-05-16 13:14:10 +0700

    update

--- diagnostics.lua
+++ diagnostics.lua
@@ -1,6 +1,3 @@
 -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-return {
-  update_in_insert = false,
-  underline = true,
-  virtual_text = true,
-}
+-- return {
+-- }
