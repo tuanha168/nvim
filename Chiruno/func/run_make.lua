749ed63095b4c909f6000a0f8e41018ed31bcfbe
TuanHa <achicir2@gmail.com>
2023-06-16 11:36:50 +0700

    Auto Update: 2023-06-16 11:36:50

--- Chiruno/func/run_make.lua
+++ Chiruno/func/run_make.lua
@@ -2,12 +2,16 @@ Chiruno.run_make = function()
   local makeCommand = vim.fn.input { prompt = "Enter command: ", default = vim.g.chiruno_make_command }
   if makeCommand == "" then return end
 
-  if (makeCommand == "vue-tsc") then
-    makeCommand = "yarn lint:tsc --noEmit --pretty false | sed -E 's/\\(([[:digit:]]+),([[:digit:]]+)\\)/:\\1:\\2/'"
-  elseif (makeCommand == "eslint") then
-    makeCommand = "yarn lint:eslint --format unix"
-  elseif (makeCommand == "stylelint") then
-    makeCommand = "yarn lint:style --formatter unix"
+  if string.find(makeCommand, "vue-tsc", 1, true) ~= nil then
+    makeCommand = string.gsub(
+      makeCommand,
+      "vue%-tsc",
+      "yarn lint:tsc --noEmit --pretty false | sed -E 's/\\(([[:digit:]]+),([[:digit:]]+)\\)/:\\1:\\2/'"
+    )
+  elseif string.find(makeCommand, "eslint", 1, true) ~= nil then
+    makeCommand = string.gsub(makeCommand, "eslint", "yarn lint:eslint --format unix")
+  elseif string.find(makeCommand, "stylelint", 1, true) ~= nil then
+    makeCommand = string.gsub(makeCommand, "stylelint", "yarn lint:style --formatter unix")
   end
 
   vim.g.chiruno_make_command = makeCommand
