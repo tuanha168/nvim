594184016f26b69cef4e2e16ee1f48d1da6673cf
TuanHa <achicir2@gmail.com>
2023-06-19 23:53:59 +0700

    Auto Update: 2023-06-19 23:53:59

--- Chiruno/func/auto-push.lua
+++ Chiruno/func/auto-push.lua
@@ -3,7 +3,7 @@ Chiruno = Chiruno or {}
 local function auto_push(path)
   if vim.fn.executable "git-auto-push" == 1 then
     Chiruno.print "Auto Pushing"
-    local cmd = 'silent !sh -c \"git-auto-push ' .. path .. ' >> ~/.cache/git-auto-push.log &\"'
+    local cmd = 'silent !sh -c \"git-auto-push ' .. path .. '\" >> ~/.cache/git-auto-push.log &'
     vim.cmd(cmd)
   end
 end
