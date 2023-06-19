730447c4e53a63dbe924814c3098a0540846561d
TuanHa <achicir2@gmail.com>
2023-06-20 00:19:30 +0700

    Auto Update: 2023-06-20 00:19:30

--- Chiruno/func/auto-push.lua
+++ Chiruno/func/auto-push.lua
@@ -3,7 +3,7 @@ Chiruno = Chiruno or {}
 local function auto_push(path)
   if vim.fn.executable "git-auto-push" == 1 then
     if not Chiruno.isdir_exist("~/.cache/git-auto-push") then
-      vim.fn.mkdir("~/.cache/git-auto-push")
+      vim.cmd("silent !mkdir ~/.cache/git-auto-push")
     end
     Chiruno.print "Auto Pushing"
     local folder_name = vim.fn.fnamemodify(path, ":t")
