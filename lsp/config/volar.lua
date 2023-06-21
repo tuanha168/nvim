90e8d3754b61b74eef3cd3ef9fbafe4a97f107f9
TuanHa <achicir2@gmail.com>
2023-06-21 23:01:10 +0700

    Auto Update: 2023-06-21 23:01:06

--- lsp/config/volar.lua
+++ lsp/config/volar.lua
@@ -1,15 +1,13 @@
-local util = require 'lspconfig.util'
+local util = require "lspconfig.util"
 local function get_typescript_server_path(root_dir)
-
-  local global_ts = os.getenv "HOME" .. '/.npm/lib/node_modules/typescript/lib'
+  local global_ts = os.getenv "HOME"
+    .. "/.local/share/nvim/mason/packages/typescript-language-server/node_modules/typescript-language-server/lib"
   -- Alternative location if installed as root:
   -- local global_ts = '/usr/local/lib/node_modules/typescript/lib'
-  local found_ts = ''
+  local found_ts = ""
   local function check_dir(path)
-    found_ts =  util.path.join(path, 'node_modules', 'typescript', 'lib')
-    if util.path.exists(found_ts) then
-      return path
-    end
+    found_ts = util.path.join(path, "node_modules", "typescript", "lib")
+    if util.path.exists(found_ts) then return path end
   end
   if util.search_ancestors(root_dir, check_dir) then
     return found_ts
