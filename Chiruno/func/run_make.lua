444cb334978ba9c7169bec66d3c93502bd04bf5b
TuanHa <achicir2@gmail.com>
2023-06-21 11:28:00 +0700

    Auto Update: 2023-06-21 11:27:56

--- Chiruno/func/run_make.lua
+++ Chiruno/func/run_make.lua
@@ -1,5 +1,5 @@
 Chiruno.run_make = function()
-  local makeCommand = vim.fn.input { prompt = "Enter command: ", default = vim.g.chiruno_make_command }
+  local makeCommand = vim.fn.input { prompt = "Enter command: " }
   if makeCommand == "" then return end
 
   if string.find(makeCommand, "vue-tsc", 1, true) ~= nil then
@@ -14,7 +14,6 @@ Chiruno.run_make = function()
     makeCommand = string.gsub(makeCommand, "stylelint", "yarn lint:style --formatter unix")
   end
 
-  vim.g.chiruno_make_command = makeCommand
-  vim.o.makeprg = vim.g.chiruno_make_command
+  vim.o.makeprg = makeCommand
   vim.cmd "Make"
 end
