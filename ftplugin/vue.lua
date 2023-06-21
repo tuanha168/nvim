0f6dfb1529cf3a4e5ebf503600865bb897695248
TuanHa <achicir2@gmail.com>
2023-06-21 17:01:36 +0700

    Auto Update: 2023-06-21 17:01:32

--- ftplugin/vue.lua
+++ ftplugin/vue.lua
@@ -43,10 +43,3 @@ vim.keymap.set("i", "/", function()
 
   return "/"
 end, { expr = true, buffer = true })
-
-local nPairs = require "nvim-autopairs"
-local Rule = require "nvim-autopairs.rule"
-local ts_conds = require "nvim-autopairs.ts-conds"
-nPairs.add_rules {
-  Rule("{{", "  }", "vue"):set_end_pair_length(2):with_pair(ts_conds.is_ts_node "text"),
-}
