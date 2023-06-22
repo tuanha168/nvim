44c965e8e587710532a8a4c62dbb3f4a5c108695
TuanHa <achicir2@gmail.com>
2023-06-22 23:10:38 +0700

    Auto Update: 2023-06-22 23:10:34

--- lsp/config/volar.lua
+++ lsp/config/volar.lua
@@ -17,7 +17,7 @@ local function get_typescript_server_path(root_dir)
 end
 
 return {
-  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+  -- filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
   on_new_config = function(new_config, new_root_dir)
     new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
   end,
