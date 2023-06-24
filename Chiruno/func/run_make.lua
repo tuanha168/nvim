2c5eefb69fcb558571c1c623a96f50c73ff7e508
TuanHa <achicir2@gmail.com>
2023-06-24 13:05:52 +0700

    Auto Update: 2023-06-24 13:05:52

--- Chiruno/func/run_make.lua
+++ Chiruno/func/run_make.lua
@@ -1,14 +1,11 @@
-vim.cmd [[
-  function! UserCommandCompletion(_a, _b, _c)
-    lua <<EOF
-      Chiruno.print('test')
-    EOF
-    let valid_args = ['vue-tsc', 'eslint', 'stylelint']
-    return join(valid_args, "\n")
-  endfunction
-]]
+function _G.custom_complete_list(arglead, cmdline, cursorpos)
+  Chiruno.print(arglead)
+  Chiruno.print(cmdline)
+  Chiruno.print(cursorpos)
+  return { "first", "second", "third" }
+end
 Chiruno.run_make = function()
-  vim.ui.input({ prompt = "Enter command", completion = "custom,UserCommandCompletion" }, function(makeCommand)
+  vim.ui.input({ prompt = "Enter command", completion = "customlist,v:lua.custom_complete_list" }, function(makeCommand)
     if makeCommand == nil or makeCommand == "" then return end
 
     if string.find(makeCommand, "vue-tsc", 1, true) ~= nil then
