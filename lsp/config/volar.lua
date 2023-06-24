9a0e1e75fecb990860f2c26e7686729e93c5c0e8
TuanHa <achicir2@gmail.com>
2023-06-25 01:22:47 +0700

    Auto Update: 2023-06-25 01:22:47

--- lsp/config/volar.lua
+++ lsp/config/volar.lua
@@ -26,7 +26,7 @@ return {
     "typescriptreact",
     "vue",
   },
-  -- on_new_config = function(new_config, new_root_dir)
-  --   new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
-  -- end,
+  on_new_config = function(new_config, new_root_dir)
+    new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+  end,
 }
