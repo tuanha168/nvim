a840f3cce70454eea6ff64bc76578202b093bb27
TuanHa <achicir2@gmail.com>
2023-07-04 11:59:41 +0700

    Auto Update: 2023-07-04 11:59:41

--- plugins/rust.lua
+++ plugins/rust.lua
@@ -36,6 +36,12 @@ return {
         -- adapter= codelldb_adapter,
         adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
       },
+      server = {
+        on_attach = function(_, bufnr)
+          local rt = require "rust-tools"
+          vim.keymap.set("n", "<leader>k", rt.hover_actions.hover_actions, { buffer = bufnr })
+        end,
+      },
     },
   },
   {
