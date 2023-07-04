91f7ffd35e59cb30cc481627d72b4d0e52ddca35
TuanHa <achicir2@gmail.com>
2023-07-04 11:47:24 +0700

    Auto Update: 2023-07-04 11:47:24

---
+++ plugins/rust.lua
@@ -0,0 +1,79 @@
+return {
+  {
+    "simrat39/rust-tools.nvim",
+    opts = {
+      tools = {
+        executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
+        reload_workspace_from_cargo_toml = true,
+        runnables = {
+          use_telescope = true,
+        },
+        inlay_hints = {
+          auto = true,
+          only_current_line = false,
+          show_parameter_hints = false,
+          parameter_hints_prefix = "<-",
+          other_hints_prefix = "=>",
+          max_len_align = false,
+          max_len_align_padding = 1,
+          right_align = false,
+          right_align_padding = 7,
+          highlight = "Comment",
+        },
+        hover_actions = {
+          border = "rounded",
+        },
+        on_initialized = function()
+          vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
+            pattern = { "*.rs" },
+            callback = function()
+              local _, _ = pcall(vim.lsp.codelens.refresh)
+            end,
+          })
+        end,
+      },
+      -- dap = {
+      --   -- adapter= codelldb_adapter,
+      --   adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+      -- },
+      server = {
+        on_attach = function(client, bufnr)
+          require("lvim.lsp").common_on_attach(client, bufnr)
+          local rt = require "rust-tools"
+          vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+        end,
+
+        capabilities = require("lvim.lsp").common_capabilities(),
+        settings = {
+          ["rust-analyzer"] = {
+            lens = {
+              enable = true,
+            },
+            checkOnSave = {
+              enable = true,
+              command = "clippy",
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "saecki/crates.nvim",
+    version = "v0.3.0",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      null_ls = {
+        enabled = true,
+        name = "crates.nvim",
+      },
+      popup = {
+        border = "rounded",
+      },
+    },
+  },
+  {
+    "j-hui/fidget.nvim",
+    opts = true,
+  },
+}
