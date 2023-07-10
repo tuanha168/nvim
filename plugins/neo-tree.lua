31d71079cdfc7aad6dc3637b11204ae5606967e1
TuanHa <achicir2@gmail.com>
2023-07-10 13:34:42 +0700

    Auto Update: 2023-07-10 13:34:42

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -29,18 +29,18 @@ return {
           size = { height = 0.5 },
         },
         {
-          title = "Neo-Tree Git",
-          ft = "neo-tree",
-          filter = function(buf) return vim.b[buf].neo_tree_source == "git_status" end,
-          pinned = true,
-          open = "Neotree position=right git_status",
-        },
-        {
           title = "Neo-Tree Buffers",
           ft = "neo-tree",
           filter = function(buf) return vim.b[buf].neo_tree_source == "buffers" end,
           pinned = true,
           open = "Neotree position=top buffers",
+        },
+        {
+          title = "Neo-Tree Git",
+          ft = "neo-tree",
+          filter = function(buf) return vim.b[buf].neo_tree_source == "git_status" end,
+          pinned = true,
+          open = "Neotree position=right git_status",
         },
         -- {
         --   ft = "Outline",
