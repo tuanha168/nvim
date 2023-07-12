bc0083371d14d1600a422bfa836d6cbf66eafd39
TuanHa <achicir2@gmail.com>
2023-07-12 12:51:46 +0700

    Auto Update: 2023-07-12 12:51:46

--- plugins/typescript.lua
+++ plugins/typescript.lua
@@ -2,8 +2,20 @@ return {
   {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    ft = {
+      "typescript",
+      "typescriptreact",
+      "typescript.tsx",
+      "javascript",
+      "javascriptreact",
+      "javascript.jsx",
+      "json",
+      "jsonc",
+      "vue",
+    },
     opts = {
       on_attach = require("astronvim.utils.lsp").on_attach,
+      handlers = { function(server) require("astronvim.utils.lsp").setup(server) end },
     },
   },
 }
