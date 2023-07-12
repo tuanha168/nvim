be5e7573b21c4d8c7e3f27e01892b006b6245870
TuanHa <achicir2@gmail.com>
2023-07-12 13:00:16 +0700

    Auto Update: 2023-07-12 13:00:16

--- plugins/typescript.lua
+++ plugins/typescript.lua
@@ -15,7 +15,10 @@ return {
     },
     opts = {
       on_attach = require("astronvim.utils.lsp").on_attach,
-      handlers = { function(server) require("astronvim.utils.lsp").setup(server) end },
+      handlers = {
+        Chiruno.print "test",
+        function(server) require("astronvim.utils.lsp").setup(server) end,
+      },
       settings = {
         tsserver_file_preferences = {
           includeInlayParameterNameHints = "all",
