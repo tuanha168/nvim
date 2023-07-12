d466459efecb729ceecde167f03bd08958ab6592
TuanHa <achicir2@gmail.com>
2023-07-12 13:31:21 +0700

    Auto Update: 2023-07-12 13:31:21

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -58,27 +58,6 @@ return {
     },
   },
   {
-    "MunifTanjim/prettier.nvim",
-    opts = {
-      bin = "prettierd", -- or `'prettierd'` (v0.23.3+)
-      filetypes = {
-        "css",
-        "graphql",
-        "html",
-        "javascript",
-        "javascriptreact",
-        "json",
-        "less",
-        "markdown",
-        "scss",
-        "typescript",
-        "typescriptreact",
-        "yaml",
-        "vue",
-      },
-    },
-  },
-  {
     "mfussenegger/nvim-dap",
     enabled = false,
   },
