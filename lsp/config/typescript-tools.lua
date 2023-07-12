615f0cd315adfbc7c38fb5b1b3b63dea490e1700
TuanHa <achicir2@gmail.com>
2023-07-12 12:58:33 +0700

    Auto Update: 2023-07-12 12:58:33

---
+++ lsp/config/typescript-tools.lua
@@ -0,0 +1,12 @@
+Chiruno.print "test"
+return {
+  filetypes = {
+    "javascript",
+    "javascript.jsx",
+    "javascriptreact",
+    "typescript",
+    "typescript.tsx",
+    "typescriptreact",
+    "vue",
+  },
+}
