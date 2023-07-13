6de482fa7df17c73f86b3c9ccae90fa959c66125
TuanHa <achicir2@gmail.com>
2023-07-13 12:51:08 +0700

    Auto Update: 2023-07-13 12:51:08

--- Chiruno/motions/format.lua
+++ Chiruno/motions/format.lua
@@ -1,12 +1,13 @@
 Chiruno.format_motion = function()
   local opts = require("astronvim.utils.lsp").format_opts
   local selection = Chiruno.get_selection()
+  local range = nil
   if selection ~= nil then
-    opts.range = {
+    range = {
       ["start"] = { selection.startRow, selection.startCol },
       ["end"] = { selection.finishRow, selection.finishCol },
     }
   end
 
-  vim.lsp.buf.format(opts)
+  vim.lsp.buf.format(vim.tbl_deep_extend("force", opts, { range = range }))
 end
