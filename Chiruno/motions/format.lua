74fa281e6af76a92662e83e40e4fe43627db2d09
TuanHa <achicir2@gmail.com>
2023-07-13 12:43:06 +0700

    Auto Update: 2023-07-13 12:43:06

--- Chiruno/motions/format.lua
+++ Chiruno/motions/format.lua
@@ -3,10 +3,12 @@ Chiruno.format_motion = function()
   local selection = Chiruno.get_selection()
   if selection ~= nil then
     opts.range = {
-      ["start"] = { selection.startRow, selection.startCol - 1 },
-      ["end"] = { selection.finishRow, selection.finishCol - 1 },
+      ["start"] = { selection.startRow + 1, selection.startCol + 1 },
+      ["end"] = { selection.finishRow - 1, selection.finishCol - 1 },
     }
   end
+
+  Chiruno.print(opts.range)
 
   vim.lsp.buf.format(opts)
 end
