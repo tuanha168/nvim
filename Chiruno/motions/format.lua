67feb603c0c1025e7dd60c67fd55477461382870
TuanHa <achicir2@gmail.com>
2023-07-13 12:44:22 +0700

    Auto Update: 2023-07-13 12:44:22

--- Chiruno/motions/format.lua
+++ Chiruno/motions/format.lua
@@ -3,8 +3,8 @@ Chiruno.format_motion = function()
   local selection = Chiruno.get_selection()
   if selection ~= nil then
     opts.range = {
-      ["start"] = { selection.startRow + 1, selection.startCol + 1 },
-      ["end"] = { selection.finishRow - 1, selection.finishCol - 1 },
+      ["start"] = { selection.startRow - 5, selection.startCol },
+      ["end"] = { selection.finishRow - 5, selection.finishCol },
     }
   end
 
