86c11f51fcdf9570d77a3968440d7a9983439179
TuanHa <achicir2@gmail.com>
2023-07-14 14:22:35 +0700

    Auto Update: 2023-07-14 14:22:35

--- plugins/neogit.lua
+++ plugins/neogit.lua
@@ -1,11 +1,11 @@
 return {
-  {
-    "NeogitOrg/neogit",
-    dependencies = "nvim-lua/plenary.nvim",
-    opts = {
-      kind = "split",
-      use_telescope = true,
-    },
-    cmd = "Neogit",
-  },
+  -- {
+  --   "NeogitOrg/neogit",
+  --   dependencies = "nvim-lua/plenary.nvim",
+  --   opts = {
+  --     kind = "split",
+  --     use_telescope = true,
+  --   },
+  --   cmd = "Neogit",
+  -- },
 }
