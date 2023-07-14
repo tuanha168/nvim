01a603f212e451ff1d66de2b989550d78389518c
TuanHa <achicir2@gmail.com>
2023-07-14 17:36:48 +0700

    Auto Update: 2023-07-14 17:36:48

--- lsp/setup_handlers.lua
+++ lsp/setup_handlers.lua
@@ -34,4 +34,37 @@ return {
       server = opts,
     }
   end,
+  efm = function()
+    local efmls = require "efmls-configs"
+
+    local eslint = require "efmls-configs.linters.eslint"
+    local prettierd = require "efmls-configs.formatters.prettier_d"
+    local stylua = require "efmls-configs.formatters.stylua"
+
+    efmls.init {
+      init_options = {
+        documentFormatting = true,
+        hover = true,
+        documentSymbol = true,
+        codeAction = true,
+        completion = true,
+      },
+      settings = {
+        rootMarkers = { ".git/" },
+        languages = {
+          lua = {},
+        },
+      },
+    }
+
+    efmls.setup {
+      lua = {
+        formatter = stylua,
+      },
+      javascript = {
+        linter = eslint,
+        formatter = prettierd,
+      },
+    }
+  end,
 }
