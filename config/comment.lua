fe816a4f7d7132fbf4175c893220e31750e56565
TuanHa <achicir2@gmail.com>
2023-07-18 19:45:24 +0700

    Auto Update: 2023-07-18 19:45:24

--- config/comment.lua
+++ config/comment.lua
@@ -1,4 +1,2 @@
 local ok, ft = pcall(require, "Comment.ft")
 if ok then ft.dosini = { "# %s" } end
-
-vim.keymap.set("v", "gc", "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>gv")
