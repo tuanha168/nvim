3fc70fc478c84a3fcd0a9eac5b024927dfe66300
TuanHa <achicir2@gmail.com>
2023-07-18 09:30:36 +0700

    Auto Update: 2023-07-18 09:30:36

--- lsp/handlers/efm/pint.lua
+++ lsp/handlers/efm/pint.lua
@@ -1,9 +1,10 @@
 local fs = require "efmls-configs.fs"
 
 local formatter = "pint"
-local command = string.format("%s ${INPUT}", fs.executable(formatter, fs.Scope.COMPOSER))
+local command = string.format("%s --no-interaction --quiet ${INPUT}", fs.executable(formatter, fs.Scope.COMPOSER))
 
 return {
   formatCommand = command,
   formatStdin = true,
+  rootMarkers = { "pint.json", "composer.json", "composer.lock" },
 }
