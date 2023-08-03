d0bb53bdc664c694c289604c3289a87d69e128aa
TuanHa <achicir2@gmail.com>
2023-08-03 17:32:31 +0700

    Auto Update: 2023-08-03 17:32:31

--- plugins/core.lua
+++ plugins/core.lua
@@ -461,4 +461,36 @@ return {
   --   "folke/which-key.nvim",
   --   enabled = false,
   -- },
+  {
+    "shellRaining/hlchunk.nvim",
+    event = { "UIEnter" },
+    otps = {
+      indent = {
+        chars = { "·", "·", "·", "·" }, -- more code can be found in https://unicodeplus.com/
+        --        chars = { "│", "¦", "┆", "┊", }, -- more code can be found in https://unicodeplus.com/
+
+        style = {
+          "#333333",
+        },
+      },
+      blank = {
+        enable = false,
+      },
+
+      chunk = {
+        chars = {
+          horizontal_line = "─",
+          vertical_line = "│",
+          left_top = "╭",
+          left_bottom = "╰",
+          right_arrow = ">",
+        },
+        style = "#00ffff",
+      },
+
+      line_num = {
+        style = "#00ffff",
+      },
+    },
+  },
 }
