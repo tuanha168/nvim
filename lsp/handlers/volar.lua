3ae39e9d4282075b1f3da4f296318b96a4cd2782
TuanHa <achicir2@gmail.com>
2023-09-07 10:27:57 +0700

    Auto Update: 2023-09-07 10:27:57 from Mac

--- lsp/handlers/volar.lua
+++
@@ -1,7 +0,0 @@
-return function(_, opts)
-  opts.document_config.default_config.cmd = {
-    "vue-language-server",
-    "--stdio",
-  }
-  require("lspconfig").volar.setup(opts)
-end
--- lsp/setup_handlers.lua
+++ lsp/setup_handlers.lua
@@ -1,5 +1,4 @@
 return {
   rust_analyzer = require "user.lsp.handlers.rust_analyzer",
   efm = require "user.lsp.handlers.efm",
-  volar = require "user.lsp.handlers.volar",
 }
