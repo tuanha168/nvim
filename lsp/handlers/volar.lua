8610d310aa6d85efbdefe8791f8801f72ca2ced1
TuanHa <achicir2@gmail.com>
2023-09-07 10:14:04 +0700

    Auto Update: 2023-09-07 10:14:04 from Mac

--- lsp/handlers/volar.lua
+++
@@ -1,8 +0,0 @@
-return function(_, opts)
-  opts.document_config.default_config.cmd = {
-    "node",
-    "/Users/tuanha/Documents/Yopaz/myhm/KnotBuilderWeb/frontend/builder/node_modules/@volar/language-core/out/index.js",
-    "--stdio",
-  }
-  return opts
-end
