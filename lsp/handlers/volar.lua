1e4166a34c08ddd8a20f975bf6d251406fd4834b
TuanHa <achicir2@gmail.com>
2023-09-07 09:54:53 +0700

    Auto Update: 2023-09-07 09:54:53 from Mac

--- lsp/handlers/volar.lua
+++ lsp/handlers/volar.lua
@@ -1,1 +1,8 @@
-return function(_, opts) Chiruno.print(opts) end
+return function(_, opts)
+  Chiruno.print(opts.document_config.default_config.cmd)
+  opts.document_config.default_config.cmd = {
+    "vue-language-server",
+    "--stdio",
+  }
+  return opts
+end
