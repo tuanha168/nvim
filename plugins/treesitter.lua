1c382f3ce67fc81a51770190e24757c5d511d6b2
TuanHa <achicir2@gmail.com>
2023-09-13 00:42:00 +0700

    Auto Update: 2023-09-13 00:42:00 from Linux

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -163,4 +163,14 @@ return {
     "jwalton512/vim-blade",
     ft = "blade",
   },
+
+  {
+    "Jezda1337/nvim-html-css",
+    ft = "html",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-lua/plenary.nvim",
+    },
+    config = function() require("html-css"):setup() end,
+  },
 }
