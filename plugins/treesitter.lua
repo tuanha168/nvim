536fc4a81e7f351ec457ebb1bdce0edd872ab3e2
TuanHa <achicir2@gmail.com>
2023-09-13 00:47:26 +0700

    Auto Update: 2023-09-13 00:47:26 from Linux

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -166,7 +166,7 @@ return {
 
   {
     "Jezda1337/nvim-html-css",
-    ft = "html",
+    event = { "InsertEnter" },
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       "nvim-lua/plenary.nvim",
