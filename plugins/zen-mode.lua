f59fcd3f64fccb07aa77f1a289818d35a5635341
TuanHa <achicir2@gmail.com>
2023-09-15 17:39:34 +0700

    Auto Update: 2023-09-15 17:39:34 from Mac

--- plugins/zen-mode.lua
+++ plugins/zen-mode.lua
@@ -5,5 +5,15 @@ return {
     keys = {
       { "<leader>z", "<cmd>TZAtaraxis<CR>", desc = "Zen Mode" },
     },
+    opts = {
+      modes = {
+        ataraxis = {
+          callbacks = {
+            open_pre = function() require("lualine").hide() end,
+            close_pre = function() require("lualine").hide { unhide = true } end,
+          },
+        },
+      },
+    },
   },
 }
