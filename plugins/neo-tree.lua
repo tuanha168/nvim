1ae511e547e25e2c007b041bda735eb349606b2d
TuanHa <achicir2@gmail.com>
2023-09-18 17:33:19 +0700

    Auto Update: 2023-09-18 17:33:19 from Mac

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -16,35 +16,35 @@ return {
         "<c-n>",
         function()
           local ok, neo = pcall(require, "neo-tree.command")
-          if ok then neo.execute { action = "toggle" } end
+          if ok then neo.execute { toggle = true } end
 
-          -- local Split = require "nui.split"
-          -- local event = require("nui.utils.autocmd").event
-          --
-          -- local highlights = require "neo-tree.ui.highlights"
-          -- local split = Split {
-          --   ns_id = highlights.ns_id,
-          --   relative = "editor",
-          --   position = "left",
-          --   size = "20%",
-          --   buf_options = {
-          --     buftype = "nofile",
-          --     modifiable = false,
-          --     swapfile = false,
-          --     filetype = "neo-tree",
-          --     undolevels = -1,
-          --   },
-          --   win_options = {
-          --     colorcolumn = "",
-          --     signcolumn = "no",
-          --   },
-          -- }
-          --
-          -- -- mount/open the component
-          -- split:mount()
-          --
-          -- -- unmount component when cursor leaves buffer
-          -- split:on(event.BufLeave, function() split:unmount() end)
+          local Split = require "nui.split"
+          local event = require("nui.utils.autocmd").event
+
+          local highlights = require "neo-tree.ui.highlights"
+          local split = Split {
+            ns_id = highlights.ns_id,
+            relative = "editor",
+            position = "left",
+            size = "20%",
+            buf_options = {
+              buftype = "nofile",
+              modifiable = false,
+              swapfile = false,
+              filetype = "neo-tree",
+              undolevels = -1,
+            },
+            win_options = {
+              colorcolumn = "",
+              signcolumn = "no",
+            },
+          }
+
+          -- mount/open the component
+          split:mount()
+
+          -- unmount component when cursor leaves buffer
+          split:on(event.BufLeave, function() split:unmount() end)
         end,
         desc = "Neo Tree",
       },
