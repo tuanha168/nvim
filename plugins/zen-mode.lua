46368f89cd18f8b7ece37cc4a971d12fa5db36aa
TuanHa <achicir2@gmail.com>
2023-09-18 09:41:12 +0700

    Auto Update: 2023-09-18 09:41:12 from Mac

--- plugins/zen-mode.lua
+++ plugins/zen-mode.lua
@@ -14,5 +14,13 @@ return {
   {
     "folke/twilight.nvim",
     event = "BufRead",
+    opts = {
+      expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
+        "function",
+        "method",
+        "table",
+        "if_statement",
+      },
+    },
   },
 }
