8e791878d1e5345a839bb5e69cfe053fbe1a7401
TuanHa <achicir2@gmail.com>
2023-09-18 13:07:45 +0700

    Auto Update: 2023-09-18 13:07:45 from Mac

--- plugins/zen-mode.lua
+++ plugins/zen-mode.lua
@@ -24,7 +24,6 @@ return {
   {
     "folke/twilight.nvim",
     opts = {
-
       expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
         "function",
         "method",
