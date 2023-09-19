0520f8ae8a9e1d4a28e668ec01d32214bdcc2c6a
TuanHa <achicir2@gmail.com>
2023-09-19 16:39:05 +0700

    Auto Update: 2023-09-19 16:39:05 from Mac

--- plugins/codi.lua
+++ plugins/codi.lua
@@ -1,9 +1,9 @@
 return {
+  { "metakirby5/codi.vim", cmd = "Codi", build = "npm install -g tsun" },
   {
     "m-demare/attempt.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      { "metakirby5/codi.vim", cmd = "Codi", build = "npm install -g tsun" },
     },
     event = "VeryLazy",
     keys = {
