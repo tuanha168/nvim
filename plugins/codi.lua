ce52ab350634fafc2dd591fbb2e9540468392707
TuanHa <achicir2@gmail.com>
2023-09-19 16:49:05 +0700

    Auto Update: 2023-09-19 16:49:05 from Mac

--- plugins/codi.lua
+++ plugins/codi.lua
@@ -1,49 +1,49 @@
 return {
   { "metakirby5/codi.vim", cmd = "Codi", build = "npm install -g tsun" },
-  -- {
-  --   "m-demare/attempt.nvim",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --   },
-  --   event = "VeryLazy",
-  --   keys = {
-  --     {
-  --       "<leader>at",
-  --       function()
-  --         local ok, attempt = pcall(require, "attempt")
-  --         if not ok then return end
-  --
-  --         attempt.new_select()
-  --       end,
-  --       desc = "Attempt",
-  --     },
-  --     {
-  --       "<leader>aa",
-  --       "<cmd>Telescope attempt<cr>",
-  --       desc = "Telescope Attempt",
-  --     },
-  --   },
-  --   config = function()
-  --     require("attempt").setup {
-  --       dir = os.getenv "HOME" .. "/.config/nvim/lua/user/scratch/src",
-  --       ext_options = { "lua", "js", "ts", "py", "cpp", "c" }, -- Options to choose from
-  --     }
-  --
-  --     local ok, telescope = pcall(require, "telescope")
-  --     if not ok then return end
-  --     telescope.load_extension "attempt"
-  --
-  --     -- vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-  --     --   pattern = "*",
-  --     --   callback = function(e)
-  --     --     if string.find(vim.api.nvim_buf_get_name(e.buf), "scratch/src", 1, true) then
-  --     --       vim.defer_fn(function()
-  --     --         -- vim.cmd "e"
-  --     --         -- vim.cmd "Codi"
-  --     --       end, 1000)
-  --     --     end
-  --     --   end,
-  --     -- })
-  --   end,
-  -- },
+  {
+    "m-demare/attempt.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>at",
+        function()
+          local ok, attempt = pcall(require, "attempt")
+          if not ok then return end
+
+          attempt.new_select()
+        end,
+        desc = "Attempt",
+      },
+      {
+        "<leader>aa",
+        "<cmd>Telescope attempt<cr>",
+        desc = "Telescope Attempt",
+      },
+    },
+    config = function()
+      require("attempt").setup {
+        dir = os.getenv "HOME" .. "/.config/nvim/lua/user/scratch/src",
+        ext_options = { "lua", "js", "ts", "py", "cpp", "c" }, -- Options to choose from
+      }
+
+      local ok, telescope = pcall(require, "telescope")
+      if not ok then return end
+      telescope.load_extension "attempt"
+
+      vim.api.nvim_create_autocmd({ "BufNew" }, {
+        pattern = "*",
+        callback = function(e)
+          if string.find(vim.api.nvim_buf_get_name(e.buf), "scratch/src", 1, true) then
+            vim.defer_fn(function()
+              vim.cmd "e"
+              vim.cmd "Codi"
+            end, 1000)
+          end
+        end,
+      })
+    end,
+  },
 }
