79c5e2070d9d102f7844620349b072ec266d4fa8
TuanHa <achicir2@gmail.com>
2023-09-19 14:23:58 +0700

    Auto Update: 2023-09-19 14:23:58 from Mac

--- plugins/core.lua
+++ plugins/core.lua
@@ -118,11 +118,11 @@ return {
     "ray-x/lsp_signature.nvim",
   },
 
-  -- {
-  --   "metakirby5/codi.vim",
-  --   cmd = "Codi",
-  --   build = "npm install -g tsun",
-  -- },
+  {
+    "metakirby5/codi.vim",
+    cmd = "Codi",
+    build = "npm install -g tsun",
+  },
   {
     "LintaoAmons/scratch.nvim",
     cmd = { "Scratch", "ScratchOpenFzf" },
