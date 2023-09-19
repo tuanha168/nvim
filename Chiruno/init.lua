eeb752fb53327d607e4f8d4411759c87ac6e1021
TuanHa <achicir2@gmail.com>
2023-09-19 10:14:54 +0700

    Auto Update: 2023-09-19 10:14:54 from Mac

--- Chiruno/init.lua
+++ Chiruno/init.lua
@@ -1,11 +1,16 @@
 Chiruno = {}
 
+-- require "user.Chiruno.env"
+
 local paths = vim.split(vim.fn.glob(string.gsub(debug.getinfo(1).source, "^@(.*)init.lua$", "%1") .. "**/*.lua"), "\n")
 
 for _, path in ipairs(paths) do
   local path_split = string.gsub(string.gsub(path, "/", "."), ".*" .. ..., "")
   local file = string.gsub(path_split, "%.lua?$", "") -- trim off .lua\n
-  if file ~= ".init" then require(... .. file) end
+  if file ~= ".init" then
+    vim.notify(file, vim.log.levels.WARN)
+    require(... .. file)
+  end
 end
 
 return Chiruno
