4ed1f96f3e090be05baab2dccda3b9966acb5599
TuanHa <achicir2@gmail.com>
2023-09-20 11:29:57 +0700

    Auto Update: 2023-09-20 11:29:57 from Mac

--- Chiruno/func/extends_table.lua
+++ Chiruno/func/extends_table.lua
@@ -1,10 +1,5 @@
 Chiruno = Chiruno or {}
 
-function Chiruno.extends_table(t1, t2)
-  for k, v in pairs(t2) do
-    t1[k] = v
-  end
-  return t1
-end
+function Chiruno.extends_table(t1, t2) return vim.tbl_deep_extend("force", t1, t2) end
 
 return Chiruno.extends_table
