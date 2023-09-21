1d04d690348d04b53b3b958cc539681e183d5a90
TuanHa <achicir2@gmail.com>
2023-09-21 19:08:20 +0700

    Auto Update: 2023-09-21 19:08:20 from Arch Linux

--- plugins/codi.lua
+++ plugins/codi.lua
@@ -1,5 +1,10 @@
 return {
-  { "metakirby5/codi.vim", cmd = "Codi", build = "npm install -g tsun" },
+  {
+    "metakirby5/codi.vim",
+    cmd = "Codi",
+    build = "npm install -g tsun",
+    config = function() vim.g["codi#virtual_text"] = 0 end,
+  },
   {
     "m-demare/attempt.nvim",
     dependencies = {
