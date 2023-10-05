d3adf1880f44a997673caed31ef1effa748b0529
TuanHa <achicir2@gmail.com>
2023-10-05 15:24:29 +0700

    Auto Update: 2023-10-05 15:24:29 from Mac

---
+++ lsp/config/phpactor.lua
@@ -0,0 +1,10 @@
+return {
+  init_options = {
+
+        "language_server_completion.trim_leading_dollar": true,
+        "completion_worse.completor.class.limit": 100,
+        "worse_reflection.enable_cache": true,
+        "worse_reflection.cache_dir": "%cache%/worse-reflection",
+        "worse_reflection.stub_dir": "%application_root%/_ide_helper.php"
+  }
+}
