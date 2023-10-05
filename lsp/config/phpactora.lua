2ad6e1e10e27c7cbe86812d5e2393c9cb7085d80
TuanHa <achicir2@gmail.com>
2023-10-05 15:39:26 +0700

    Auto Update: 2023-10-05 15:39:26 from Mac

--- lsp/config/phpactora.lua
+++ lsp/config/phpactora.lua
@@ -1,7 +1,7 @@
 return {
   on_new_config = function(new_config)
     new_config.init_options = {
-      ["language_server_completion.trim_leading_dollar"] = true,
+      ["language_server_completion.trim_leading_dollar"] = false,
       -- ["completion_worse.completor.class.limit"] = 100,
       -- ["worse_reflection.enable_cache"] = true,
       -- ["worse_reflection.cache_dir"] = "%cache%/worse-reflection",
