1cc67469a100b2749642201fb98eead903579664
TuanHa <achicir2@gmail.com>
2023-10-09 11:03:13 +0700

    Auto Update: 2023-10-09 11:03:13 from Mac

--- plugins/codeium.lua
+++ plugins/codeium.lua
@@ -16,8 +16,18 @@ return {
     config = function()
       vim.g.codeium_disable_bindings = 1
       vim.keymap.set("i", "<C-f>", function() return vim.fn["codeium#Accept"]() end, { expr = true, silent = true })
-      vim.keymap.set("i", "<C-j>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true, silent = true })
-      vim.keymap.set("i", "<C-k>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true, silent = true })
+      vim.keymap.set(
+        "i",
+        "<C-j>",
+        function() return vim.fn["codeium#CycleCompletions"](1) end,
+        { expr = true, silent = true }
+      )
+      vim.keymap.set(
+        "i",
+        "<C-k>",
+        function() return vim.fn["codeium#CycleCompletions"](-1) end,
+        { expr = true, silent = true }
+      )
     end,
   },
 }
