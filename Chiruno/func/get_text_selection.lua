80d046f00d3e6ac49bbe775e7c0e8889a570e8ab
TuanHa <achicir2@gmail.com>
2023-10-17 14:56:20 +0700

    Auto Update: 2023-10-17 14:56:20 from Mac

--- Chiruno/func/get_text_selection.lua
+++ Chiruno/func/get_text_selection.lua
@@ -2,10 +2,13 @@ function Chiruno.get_text_selection(motion_type)
   local selection = Chiruno.get_selection()
   if selection == nil then return end
 
+  local selectedText = {}
+  local escapedText = {}
+
   if motion_type == "line" then
-    return vim.api.nvim_buf_get_lines(0, selection.startRow - 1, selection.finishRow, true)
+    selectedText = vim.api.nvim_buf_get_lines(0, selection.startRow - 1, selection.finishRow, true)
   elseif motion_type == "char" then
-    return vim.api.nvim_buf_get_text(
+    selectedText = vim.api.nvim_buf_get_text(
       0,
       selection.startRow - 1,
       selection.startCol,
@@ -18,14 +21,11 @@ function Chiruno.get_text_selection(motion_type)
     for i, line in ipairs(selected_lines) do
       selected_lines[i] = line:sub(selection.startCol + 1, selection.finishCol + 1)
     end
-    return selected_lines
+    selectedText = selected_lines
   end
 
   local special_symbols = "^$\\"
   if vim.api.nvim_get_option "magic" then special_symbols = "*^$.~[]\\" end
-
-  local selectedText = { selection.startText }
-  local escapedText = {}
 
   -- Escape all special_symbols
   for index, line in ipairs(selectedText) do
