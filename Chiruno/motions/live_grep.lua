0271102d5dbc73469539c7fd4f73c38c094d1164
TuanHa <achicir2@gmail.com>
2023-10-17 13:36:11 +0700

    Auto Update: 2023-10-17 13:36:11 from Mac

--- Chiruno/motions/live_grep.lua
+++ Chiruno/motions/live_grep.lua
@@ -4,7 +4,7 @@ function Chiruno.live_grep_motion()
     -- search = Chiruno.get_text_selection().text,
     hidden = true,
   }
-  vim.api.nvim_feedkeys(text .. "<ESC>", "i", false)
+  vim.schedule(function() vim.api.nvim_feedkeys(text .. "<ESC>", "i", false) end)
 end
 
 return Chiruno.live_grep_motion
