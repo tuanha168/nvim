1f6e0d850299af0afb295ea2f08b09bdd34ed5ce
TuanHa <achicir2@gmail.com>
2023-10-17 13:21:00 +0700

    Auto Update: 2023-10-17 13:21:00 from Mac

--- Chiruno/motions/live_grep.lua
+++ Chiruno/motions/live_grep.lua
@@ -1,7 +1,6 @@
 function Chiruno.live_grep_motion()
-  local search = vim.api.nvim_replace_termcodes(Chiruno.get_text_selection().text, true, false, true)
   require("telescope.builtin").grep_string {
-    search = search,
+    search = Chiruno.get_text_selection().text,
     hidden = true,
   }
   vim.api.nvim_feedkeys("<ESC>", "i", false)
