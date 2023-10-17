68a35814ba73c4f693ce46df944800cf7c3cb355
TuanHa <achicir2@gmail.com>
2023-10-17 13:24:56 +0700

    Auto Update: 2023-10-17 13:24:56 from Mac

--- Chiruno/func/get_text_selection.lua
+++ Chiruno/func/get_text_selection.lua
@@ -1,4 +1,4 @@
-function Chiruno.get_text_selection()
+function Chiruno.get_text_selection(motion_type)
   local selection = Chiruno.get_selection()
   if selection == nil then return end
 
@@ -7,7 +7,15 @@ function Chiruno.get_text_selection()
     vim.list_extend(selectedText, vim.list_slice(selection.currentLine, 2, #selection.currentLine - 1))
   end
   table.insert(selectedText, selection.endText)
-  return { text = table.concat(selectedText, "\n"), selection = selection }
+
+  local pattern_to_search = table.concat(selectedText, "\\n")
+  if motion_type == "line" then
+    pattern_to_search = "^" .. selectedText .. "\\n"
+  elseif motion_type == "block" then
+    pattern_to_search = ".*" .. table.concat(selectedText, ".*\\n.*") .. ".*"
+  end
+
+  return { text = pattern_to_search, selection = selection }
 end
 
 return Chiruno.get_text_selection
