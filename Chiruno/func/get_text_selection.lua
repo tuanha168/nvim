a4b91fea5853f03050c7a5fd02953b12794340eb
TuanHa <achicir2@gmail.com>
2023-10-17 13:28:04 +0700

    Auto Update: 2023-10-17 13:28:04 from Mac

--- Chiruno/func/get_text_selection.lua
+++ Chiruno/func/get_text_selection.lua
@@ -2,11 +2,20 @@ function Chiruno.get_text_selection(motion_type)
   local selection = Chiruno.get_selection()
   if selection == nil then return end
 
+  local special_symbols = "^$\\|"
+  if vim.api.nvim_get_option "magic" then special_symbols = "*^$.~[]\\|" end
+
   local selectedText = { selection.startText }
-  if #selection.currentLine > 2 then
-    vim.list_extend(selectedText, vim.list_slice(selection.currentLine, 2, #selection.currentLine - 1))
+
+  -- Escape all special_symbols
+  for index, line in ipairs(selectedText) do
+    selectedText[index] = vim.fn.escape(line, special_symbols)
   end
-  table.insert(selectedText, selection.endText)
+
+  -- if #selection.currentLine > 2 then
+  --   vim.list_extend(selectedText, vim.list_slice(selection.currentLine, 2, #selection.currentLine - 1))
+  -- end
+  -- table.insert(selectedText, selection.endText)
 
   local pattern_to_search = table.concat(selectedText, "\\n")
   if motion_type == "line" then
