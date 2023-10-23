6d467d1686d95fb02414c275f5ef88830aebef19
TuanHa <achicir2@gmail.com>
2023-10-23 10:47:12 +0700

    Auto Update: 2023-10-23 10:47:12 from Mac

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -40,18 +40,13 @@ return {
 
       local snip_status_ok, luasnip = pcall(require, "luasnip")
 
-      local function has_words_before()
-        local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-      end
-
       opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
         elseif snip_status_ok and luasnip.expand_or_jumpable() then
           luasnip.expand_or_jump()
-        elseif has_words_before() then
-          cmp.complete()
+        elseif vim.snippet.jumpable(1) then
+          vim.snippet.jump(1)
         else
           fallback()
         end
@@ -61,6 +56,8 @@ return {
           cmp.select_prev_item()
         elseif luasnip.jumpable(-1) then
           luasnip.jump(-1)
+        elseif vim.snippet.jumpable(-1) then
+          vim.snippet.jump(-1)
         else
           fallback()
         end
