9a2845fabbfcb62354e029186ed0110d149d6900
TuanHa <achicir2@gmail.com>
2023-10-25 10:08:31 +0700

    Auto Update: 2023-10-25 10:08:31 from Mac

--- mappings.lua
+++ mappings.lua
@@ -27,7 +27,7 @@ local mappings = {
       noremap = true,
       desc = "Resize split right",
     },
-    ["*"] = { function() Chiruno.operatorfunc_lua "search" end },
+    ["*"] = { function() Chiruno.search "normal" end },
     ["<leader>fs"] = {
       function() Chiruno.operatorfunc_lua "live_grep_motion" end,
       desc = "Live grep motion",
