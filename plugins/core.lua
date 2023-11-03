561deb6a92a35bc3611a28c74d298fe0112562f0
TuanHa <achicir2@gmail.com>
2023-11-04 01:03:02 +0700

    Auto Update: 2023-11-04 01:03:02 from Arch Linux

--- plugins/core.lua
+++ plugins/core.lua
@@ -591,8 +591,11 @@ return {
   { "Aasim-A/scrollEOF.nvim", opts = {}, event = "BufEnter" },
   {
     "stevearc/oil.nvim",
-    opts = {},
     cmd = { "Oil" },
-    config = function() vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }) end,
+    opts = function()
+      require("oil").setup()
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+      return {}
+    end,
   },
 }
