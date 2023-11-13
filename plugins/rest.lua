a10ec71f750e8baab0f60eca27553805f9b5746a
TuanHa <achicir2@gmail.com>
2023-11-13 11:40:50 +0700

    Auto Update: 2023-11-13 11:40:50 from Mac

--- plugins/rest.lua
+++ plugins/rest.lua
@@ -24,7 +24,7 @@ return {
       show_url = true,
       -- show the generated curl command in case you want to launch
       -- the same request via the terminal (can be verbose)
-      show_curl_command = false,
+      show_curl_command = true,
       show_http_info = true,
       show_headers = true,
       -- executables or functions for formatting response body [optional]
