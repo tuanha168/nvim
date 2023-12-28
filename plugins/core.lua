a542ed5b94d00963aee55ce5b19b289b16c3445f
yopaz-tuanha <tuanha@yopaz.vn>
2023-12-28 15:52:43 +0700

    Auto Update: 2023-12-28 15:52:43 from Mac

--- plugins/core.lua
+++ plugins/core.lua
@@ -644,6 +644,28 @@ return {
         end,
       })
       return {
+        content = {
+          filter = function(entry) return entry.name ~= ".DS_Store" end,
+          sort = function(entries)
+            local all_paths = table.concat(vim.tbl_map(function(entry) return entry.path end, entries), "\n")
+            local output_lines = {}
+            local job_id = vim.fn.jobstart({ "git", "check-ignore", "--stdin" }, {
+              stdout_buffered = true,
+              on_stdout = function(_, data) output_lines = data end,
+            })
+
+            -- command failed to run
+            if job_id < 1 then return entries end
+
+            -- send paths via STDIN
+            vim.fn.chansend(job_id, all_paths)
+            vim.fn.chanclose(job_id, "stdin")
+            vim.fn.jobwait { job_id }
+            return require("mini.files").default_sort(
+              vim.tbl_filter(function(entry) return not vim.tbl_contains(output_lines, entry.path) end, entries)
+            )
+          end,
+        },
         mappings = {
           close = "q",
           go_in = "L",
