2bc2f9048cebd2d793a8ad5c20f33937133ef567
yopaz-tuanha <tuanha@yopaz.vn>
2023-12-28 13:04:38 +0700

    Auto Update: 2023-12-28 13:04:38 from Mac

--- Chiruno/lsp/lsp_rename.lua
+++ Chiruno/lsp/lsp_rename.lua
@@ -13,6 +13,25 @@ function Chiruno.lsp.get_clients(opts)
   return opts and opts.filter and vim.tbl_filter(opts.filter, ret) or ret
 end
 
+function Chiruno.lsp.on_rename(from, to)
+  local clients = Chiruno.lsp.get_clients()
+
+  for _, client in ipairs(clients) do
+    if client.supports_method "workspace/willRenameFiles" then
+      ---@diagnostic disable-next-line: invisible
+      local resp = client.request_sync("workspace/willRenameFiles", {
+        files = {
+          {
+            oldUri = vim.uri_from_fname(from),
+            newUri = vim.uri_from_fname(to),
+          },
+        },
+      }, 1000, 0)
+      if resp and resp.result ~= nil then vim.lsp.util.apply_workspace_edit(resp.result, client.offset_encoding) end
+    end
+  end
+end
+
 function M.will_rename_files(files, options)
   options = options or {}
   local clients = Chiruno.lsp.get_clients()
