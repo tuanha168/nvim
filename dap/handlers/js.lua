ec64828bba5b14955e3d3abf2cfa67840f348109
TuanHA <achicir2@gmail.com>
2024-01-02 23:44:52 +0700

    Auto Update: 2024-01-02 23:44:52 from Arch Linux

--- dap/handlers/js.lua
+++ dap/handlers/js.lua
@@ -28,6 +28,26 @@ return {
       restart = true,
     },
     {
+      type = "pwa-chrome",
+      request = "launch",
+      name = "Launch & Debug Chrome",
+      url = function()
+        local co = coroutine.running()
+        return coroutine.create(function()
+          vim.ui.input({ prompt = "URL: ", default = "http://localhost:3000" }, function(url)
+            if url == nil or url == "" then return end
+
+            coroutine.resume(co, url)
+          end)
+        end)
+      end,
+      webRoot = "${workspaceFolder}",
+      skipFiles = { "<node_internals>/**" },
+      protocol = "inspector",
+      sourceMaps = true,
+      userDataDir = false,
+    },
+    {
       type = "pwa-node",
       request = "attach",
       name = "Attach Program (port 9229)",
