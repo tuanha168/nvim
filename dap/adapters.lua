47282603bc77f3466cee193804382705a88ff583
TuanHA <achicir2@gmail.com>
2024-01-03 00:16:57 +0700

    Auto Update: 2024-01-03 00:16:57 from Arch Linux

--- dap/adapters.lua
+++ dap/adapters.lua
@@ -1,18 +1,14 @@
 return {
-  node2 = {
-    type = "executable",
-    command = vim.fn.exepath "node-debug2-adapter",
-  },
   js = {
     type = "executable",
     command = vim.fn.exepath "js-debug-adapter",
   },
-  ["pwa-node"] = {
-    type = "executable",
-    command = vim.fn.exepath "js-debug-adapter",
-  },
-  ["pwa-chrome"] = {
-    type = "executable",
-    command = vim.fn.exepath "js-debug-adapter",
-  },
+  -- ["pwa-node"] = {
+  --   type = "executable",
+  --   command = vim.fn.exepath "js-debug-adapter",
+  -- },
+  -- ["pwa-chrome"] = {
+  --   type = "executable",
+  --   command = vim.fn.exepath "js-debug-adapter",
+  -- },
 }
