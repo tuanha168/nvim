717861725e0134e0acc1b2a8bf901329bf8ab4bb
TuanHA <achicir2@gmail.com>
2024-01-03 00:19:04 +0700

    Auto Update: 2024-01-03 00:19:04 from Arch Linux

--- dap/init.lua
+++ dap/init.lua
@@ -20,10 +20,11 @@ M.handlers = function(packages)
     if not present then goto continue end
     handlers[v] = function(config)
       config = vim.tbl_deep_extend("force", config or {}, package)
-      require("mason-nvim-dap").default_setup(config)
+      return require("mason-nvim-dap").default_setup(config)
     end
     ::continue::
   end
+  Chiruno.func.print(handlers)
   return handlers
 end
 
