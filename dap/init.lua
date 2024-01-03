d0f0bff4d56651c984af5d4251e5b7f59694e844
TuanHA <achicir2@gmail.com>
2024-01-04 00:29:24 +0700

    Auto Update: 2024-01-04 00:29:24 from Arch Linux

--- dap/init.lua
+++ dap/init.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-M.handlers = function(packages)
+M.handlers = function()
   -- local dap = require("dap")
   -- local adapters = require("user.dap.adapters")
   -- for k, v in pairs(adapters) do
@@ -16,11 +16,11 @@ M.handlers = function(packages)
     end,
   }
 
-  for _, v in ipairs(packages) do
-    local is_ok, dap_handlers = pcall(require, "user/dap/handlers/" .. v)
-    if not is_ok then goto continue end
+  local adapters = require("user.dap.handlers.adapters")
+
+  for k, v in ipairs(adapters) do
     handlers[v] = function(config)
-      config = vim.tbl_deep_extend("force", config or {}, dap_handlers)
+      config = vim.tbl_deep_extend("force", config or {}, )
       config.adapters = require("user.dap.adapters")
       require("mason-nvim-dap").default_setup(config)
     end
