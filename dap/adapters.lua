f97ef1e8e428205b6d175863abd5cf514cdedfd1
TuanHA <achicir2@gmail.com>
2024-01-04 00:46:23 +0700

    Auto Update: 2024-01-04 00:46:23 from Arch Linux

---
+++ dap/adapters.lua
@@ -0,0 +1,13 @@
+return {
+  setup = function(dap)
+    dap.adapters["pwa-node"] = {
+      type = "server",
+      host = "localhost",
+      port = "${port}",
+      executable = {
+        command = "js-debug-adapter",
+        args = { "${port}" },
+      },
+    }
+  end,
+}
