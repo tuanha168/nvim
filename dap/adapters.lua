df1b906c368e1d98d3ce32b9987d8b2acb975df5
TuanHA <achicir2@gmail.com>
2024-01-04 00:23:12 +0700

    Auto Update: 2024-01-04 00:23:12 from Arch Linux

--- dap/adapters.lua
+++ dap/adapters.lua
@@ -1,13 +1,13 @@
 return {
   ["pwa-node"] = {
     type = "executable",
-    command = "js-debug-adapter",
+    command = vim.fn.exepath "js-debug-adapter",
     -- command = "node",
     -- args = { vim.fn.stdpath "data" .. "/lazy/vscode-js-debug" },
   },
   ["pwa-chrome"] = {
     type = "executable",
-    command = "js-debug-adapter",
+    command = vim.fn.exepath "js-debug-adapter",
     -- command = "node",
     -- args = { vim.fn.stdpath "data" .. "/lazy/vscode-js-debug" },
   },
