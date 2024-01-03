c3e00b6dd6d6732625a71846b19a8ef2c8317495
TuanHA <achicir2@gmail.com>
2024-01-04 00:26:08 +0700

    Auto Update: 2024-01-04 00:26:08 from Arch Linux

--- dap/handlers/configurations.lua
+++ dap/handlers/configurations.lua
@@ -1,77 +1,65 @@
 return {
-  filetypes = {
-    "javascript",
-    "javascriptreact",
-    "javascript.jsx",
-    "typescript",
-    "typescriptreact",
-    "typescript.tsx",
-    "vue",
+  {
+    type = "pwa-node",
+    request = "launch",
+    name = "Launch file",
+    program = "${file}",
+    cwd = "${workspaceFolder}",
+    sourceMaps = true,
+    skipFiles = { "<node_internals>/**", "!**/node_modules/**", "**/$KNOWN_TOOLS$/**" },
+    restart = true,
   },
+  {
+    type = "pwa-node",
+    request = "attach",
+    name = "Attach",
+    processId = require("dap.utils").pick_process,
+    cwd = "${workspaceFolder}",
+    sourceMaps = true,
+    skipFiles = { "<node_internals>/**", "!**/node_modules/**", "**/$KNOWN_TOOLS$/**" },
+    restart = true,
+  },
+  {
+    type = "pwa-chrome",
+    request = "launch",
+    name = "Launch & Debug Chrome",
+    url = function()
+      local co = coroutine.running()
+      return coroutine.create(function()
+        vim.ui.input({ prompt = "URL: ", default = "http://localhost:3000" }, function(url)
+          if url == nil or url == "" then return end
 
-  configurations = {
-    {
-      type = "pwa-node",
-      request = "launch",
-      name = "Launch file",
-      program = "${file}",
-      cwd = "${workspaceFolder}",
-      sourceMaps = true,
-      skipFiles = { "<node_internals>/**", "!**/node_modules/**", "**/$KNOWN_TOOLS$/**" },
-      restart = true,
-    },
-    {
-      type = "pwa-node",
-      request = "attach",
-      name = "Attach",
-      processId = require("dap.utils").pick_process,
-      cwd = "${workspaceFolder}",
-      sourceMaps = true,
-      skipFiles = { "<node_internals>/**", "!**/node_modules/**", "**/$KNOWN_TOOLS$/**" },
-      restart = true,
-    },
-    {
-      type = "pwa-chrome",
-      request = "launch",
-      name = "Launch & Debug Chrome",
-      url = function()
-        local co = coroutine.running()
-        return coroutine.create(function()
-          vim.ui.input({ prompt = "URL: ", default = "http://localhost:3000" }, function(url)
-            if url == nil or url == "" then return end
-
-            coroutine.resume(co, url)
-          end)
+          coroutine.resume(co, url)
         end)
-      end,
-      port = 9222,
-      webRoot = "${workspaceFolder}",
-      skipFiles = { "<node_internals>/**", "!**/node_modules/**", "**/$KNOWN_TOOLS$/**", "**/**/*.js" },
-      protocol = "inspector",
-      sourceMaps = true,
-      userDataDir = false,
-    },
-    {
-      type = "pwa-node",
-      request = "attach",
-      name = "Attach Program (port 9229)",
-      cwd = "${workspaceFolder}",
-      sourceMaps = true,
-      restart = true,
-      skipFiles = { "<node_internals>/**", "!**/node_modules/**", "**/$KNOWN_TOOLS$/**" },
-      port = 9229,
-    },
-    {
-      type = "pwa-node",
-      request = "attach",
-      name = "Attach Program From Docker (port 9229)",
-      cwd = "${workspaceFolder}",
-      sourceMaps = true,
-      restart = true,
-      skipFiles = { "<node_internals>/**", "!**/node_modules/**", "**/$KNOWN_TOOLS$/**" },
-      port = 9229,
-      localRoot = "${workspaceFolder}",
-      remoteRoot = "/var/www",
-    },
+      end)
+    end,
+    port = 9222,
+    webRoot = "${workspaceFolder}",
+    skipFiles = { "<node_internals>/**", "!**/node_modules/**", "**/$KNOWN_TOOLS$/**", "**/**/*.js" },
+    protocol = "inspector",
+    sourceMaps = true,
+    userDataDir = false,
+  },
+  {
+    type = "pwa-node",
+    request = "attach",
+    name = "Attach Program (port 9229)",
+    cwd = "${workspaceFolder}",
+    sourceMaps = true,
+    restart = true,
+    skipFiles = { "<node_internals>/**", "!**/node_modules/**", "**/$KNOWN_TOOLS$/**" },
+    port = 9229,
+  },
+  {
+    type = "pwa-node",
+    request = "attach",
+    name = "Attach Program From Docker (port 9229)",
+    cwd = "${workspaceFolder}",
+    sourceMaps = true,
+    restart = true,
+    skipFiles = { "<node_internals>/**", "!**/node_modules/**", "**/$KNOWN_TOOLS$/**" },
+    port = 9229,
+    localRoot = "${workspaceFolder}",
+    remoteRoot = "/var/www",
   },
 }
