b71bc957964401d5005a77da8b13d61e583a2028
TuanHA <achicir2@gmail.com>
2024-01-04 00:48:37 +0700

    Auto Update: 2024-01-04 00:48:37 from Arch Linux

--- dap/adapters.lua
+++ dap/adapters.lua
@@ -1,13 +1,13 @@
 return {
   setup = function(dap)
     dap.adapters["pwa-node"] = {
+      type = "executable",
+      command = "js-debug-adapter",
+    }
+
+    dap.adapters["pwa-chrome"] = {
       type = "server",
-      host = "localhost",
-      port = "${port}",
-      executable = {
-        command = "js-debug-adapter",
-        args = { "${port}" },
-      },
+      command = "js-debug-adapter",
     }
   end,
 }
