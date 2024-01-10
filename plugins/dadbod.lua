04f388d6b42b07100529d514d7975a12929ab8da
yopaz-tuanha <tuanha@yopaz.vn>
2024-01-10 13:43:07 +0700

    Auto Update: 2024-01-10 13:43:06 from Mac

--- plugins/dadbod.lua
+++ plugins/dadbod.lua
@@ -14,6 +14,18 @@ return {
     init = function()
       -- Your DBUI configuration
       vim.g.db_ui_use_nerd_fonts = 1
+      -- autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })
+
+      local ok, cmp = pcall(require, "cmp")
+      if not ok then return end
+
+      local autocmd = vim.api.nvim_create_autocmd
+
+      autocmd("FileType", {
+        pattern = { "sql", "mysql", "plsql" },
+        callback = function() cmp.setup.buffer { sources = { { name = "vim-dadbod-completion" } } } end,
+        once = true,
+      })
     end,
   },
 }
