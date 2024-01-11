f947c5bbc11dbf5e968fa25b2020bae500fa3940
yopaz-tuanha <tuanha@yopaz.vn>
2024-01-11 14:06:28 +0700

    Auto Update: 2024-01-11 14:06:28 from Mac

--- plugins/dadbod.lua
+++ plugins/dadbod.lua
@@ -4,7 +4,21 @@ return {
     dependencies = {
       { "tpope/vim-dadbod" },
       { "pbogut/vim-dadbod-ssh" },
-      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" } },
+      {
+        "kristijanhusak/vim-dadbod-completion",
+        ft = { "sql", "mysql", "plsql" },
+        init = function()
+          local ok, cmp = pcall(require, "cmp")
+          if not ok then return end
+
+          local autocmd = vim.api.nvim_create_autocmd
+
+          autocmd("FileType", {
+            pattern = { "sql", "mysql", "plsql" },
+            callback = function() cmp.setup.buffer { sources = { { name = "vim-dadbod-completion" } } } end,
+          })
+        end,
+      },
     },
     keys = {
       {
@@ -23,21 +37,6 @@ return {
       "DBUIAddConnection",
       "DBUIFindBuffer",
     },
-    init = function()
-      -- Your DBUI configuration
-      vim.g.db_ui_use_nerd_fonts = 1
-      -- autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })
-
-      local ok, cmp = pcall(require, "cmp")
-      if not ok then return end
-
-      local autocmd = vim.api.nvim_create_autocmd
-
-      autocmd("FileType", {
-        pattern = { "sql", "mysql", "plsql" },
-        callback = function() cmp.setup.buffer { sources = { { name = "vim-dadbod-completion" } } } end,
-        once = true,
-      })
-    end,
+    init = function() vim.g.db_ui_use_nerd_fonts = 1 end,
   },
 }
