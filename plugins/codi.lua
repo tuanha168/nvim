293f66d89266479e2463879e8e8b6cd3a155dddd
TuanHA <achicir2@gmail.com>
2024-02-11 23:53:53 +0700

    Auto Update: 2024-02-11 23:53:53 from Arch Linux

--- plugins/codi.lua
+++ plugins/codi.lua
@@ -1,9 +1,9 @@
 return {
-  {
-    "metakirby5/codi.vim",
-    cmd = "Codi",
-    build = "npm install -g tsun",
-  },
+  -- {
+  --   "metakirby5/codi.vim",
+  --   cmd = "Codi",
+  --   build = "npm install -g tsun",
+  -- },
   {
     "0x100101/lab.nvim",
     cmd = "Lab",
@@ -55,32 +55,33 @@ return {
         list_buffers = true,
       }
 
-      -- vim.api.nvim_create_autocmd({ "BufReadPost" }, {
-      --   pattern = "*",
-      --   callback = function(e)
-      --     if vim.b[e.buf].scratch_entered then return end
-      --     if string.find(vim.api.nvim_buf_get_name(e.buf), "scratch/src/scratch", 1, true) then
-      --       vim.b[e.buf].scratch_entered = true
-      --       local buffers = vim.fn.getwininfo()
-      --       if not buffers then return end
-      --       local haveNeoTree = false
-      --       for _, buf in ipairs(buffers) do
-      --         if vim.api.nvim_get_option_value("filetype", { buf = buf.bufnr }) == "neo-tree" then
-      --           haveNeoTree = true
-      --         end
-      --       end
-      --       local _ok, neo = pcall(require, "neo-tree.command")
-      --       if _ok then neo.execute { action = "close" } end
-      --
-      --       local status = Chiruno.func.get_null_window_status()
-      --       Chiruno.func.close_null_window()
-      --       vim.cmd "Codi"
-      --       Chiruno.func.open_null_window { left = not not status.splitLeft, right = not not status.splitRight }
-      --
-      --       if _ok and haveNeoTree then neo.execute { action = "toggle" } end
-      --     end
-      --   end,
-      -- })
+      vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+        pattern = "*",
+        callback = function(e)
+          if vim.b[e.buf].scratch_entered then return end
+          if string.find(vim.api.nvim_buf_get_name(e.buf), "scratch/src/scratch", 1, true) then
+            vim.b[e.buf].scratch_entered = true
+            local buffers = vim.fn.getwininfo()
+            if not buffers then return end
+            local haveNeoTree = false
+            for _, buf in ipairs(buffers) do
+              if vim.api.nvim_get_option_value("filetype", { buf = buf.bufnr }) == "neo-tree" then
+                haveNeoTree = true
+              end
+            end
+            local _ok, neo = pcall(require, "neo-tree.command")
+            if _ok then neo.execute { action = "close" } end
+
+            local status = Chiruno.func.get_null_window_status()
+            Chiruno.func.close_null_window()
+            -- vim.cmd "Codi"
+            vim.cmd "Lab code run"
+            Chiruno.func.open_null_window { left = not not status.splitLeft, right = not not status.splitRight }
+
+            if _ok and haveNeoTree then neo.execute { action = "toggle" } end
+          end
+        end,
+      })
     end,
   },
 }
