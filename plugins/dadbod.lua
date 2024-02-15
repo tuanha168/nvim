d9e139ab6c428fcef1e72c26f6fd488a48063d6d
yopaz-tuanha <tuanha@yopaz.vn>
2024-02-15 17:23:43 +0700

    Auto Update: 2024-02-15 17:23:43 from Mac

--- plugins/dadbod.lua
+++ plugins/dadbod.lua
@@ -41,6 +41,15 @@ return {
       "DBUIAddConnection",
       "DBUIFindBuffer",
     },
-    init = function() vim.g.db_ui_use_nerd_fonts = 1 end,
+    init = function()
+      vim.g.db_ui_use_nerd_fonts = 1
+
+      vim.api.nvim_create_autocmd("VimLeavePre", {
+        pattern = "*",
+        callback = function()
+          if vim.g.db_ui_ssh_connection then vim.uv.kill(vim.g.db_ui_ssh_connection) end
+        end,
+      })
+    end,
   },
 }
