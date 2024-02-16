6e64e85c97a3ed00fe82c6605fde3edf7b2bd2ea
yopaz-tuanha <tuanha@yopaz.vn>
2024-02-16 17:57:25 +0700

    Auto Update: 2024-02-16 17:57:25 from Mac

--- types.lua
+++ types.lua
@@ -1,11 +1,11 @@
 ---@class AutocmdCallbackEvent
----@field id number
----@field event string
----@field group number
----@field match string
----@field buf number
----@field file string
----@field data any
+---@field id number autocommand id
+---@field event string name of the triggered event `autocmd-events`
+---@field group number|nil autocommand group id, if any
+---@field match string expanded value of `<amatch>`
+---@field buf number expanded value of `<abuf>`
+---@field file string expanded value of `<afile>`
+---@field data any arbitrary data passed from `nvim_exec_autocmds()`
 
 ---@class NullWindowOptions
 ---@field left boolean
