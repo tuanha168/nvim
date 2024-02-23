e7f508cf118c67d2c3c3d4e99ba7b4b3c8cf61c8
yopaz-tuanha <tuanha@yopaz.vn>
2024-02-23 14:46:39 +0700

    Auto Update: 2024-02-23 14:46:39 from Mac

--- plugins/codi.lua
+++ plugins/codi.lua
@@ -3,6 +3,12 @@ return {
     "metakirby5/codi.vim",
     cmd = "Codi",
     build = "npm install -g tsun",
+    config = function()
+      require('codi.vim')
+      vim.cmd [[
+        g:codi#autocmd = None
+      ]]
+    end,
   },
   -- {
   --   "0x100101/lab.nvim",
