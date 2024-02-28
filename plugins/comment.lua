bbe16a168d5778e0ebe343f0b4993c0edcb81af0
yopaz-tuanha <tuanha@yopaz.vn>
2024-02-28 14:00:48 +0700

    Auto Update: 2024-02-28 14:00:48 from Mac

--- plugins/comment.lua
+++ plugins/comment.lua
@@ -7,7 +7,14 @@ return {
     },
     opts = function()
       local commentstring_avail, commentstring = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
-      return commentstring_avail and commentstring and { pre_hook = commentstring.create_pre_hook() } or {}
+      local opts = {
+        ignore = "^$",
+      }
+      if commentstring_avail and commentstring then opts.pre_hook = commentstring.create_pre_hook() end
+      local ft = require('Comment.ft')
+      ft.set('json', '#%s')
+
+      return opts
     end,
   },
 }
