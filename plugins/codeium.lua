793fa3c74a8adf0c4bac1f02c5ae1ef10f6d12f7
TuanHA <achicir2@gmail.com>
2024-03-04 19:53:12 +0700

    Auto Update: 2024-03-04 19:53:12 from Arch Linux

--- plugins/codeium.lua
+++ plugins/codeium.lua
@@ -14,10 +14,33 @@ return {
     -- enabled = false,
     event = "InsertEnter",
     keys = {
-      { mode = "n", "<C-g>", function() vim.fn["codeium#Chat"]() end, expr = true, silent = true },
-      { mode = "i", "<C-f>", function() vim.fn["codeium#Accept"]() end, expr = true, silent = true },
-      { mode = "i", "<C-j>", function() vim.fn["codeium#CycleCompletions"](1) end, expr = true, silent = true },
-      { mode = "i", "<C-k>", function() vim.fn["codeium#CycleCompletions"](-1) end, expr = true, silent = true },
+      {
+        mode = "n",
+        "<C-g>",
+        function() vim.api.nvim_call_function("codeium#Chat", {}) end,
+
+      },
+      {
+        mode = "i",
+        "<C-f>",
+        function() vim.api.nvim_call_function("codeium#Accept", {}) end,
+        expr = true,
+        silent = true,
+      },
+      {
+        mode = "i",
+        "<C-j>",
+        function() vim.api.nvim_call_function("codeium#CycleCompletions", {}) end,
+        expr = true,
+        silent = true,
+      },
+      {
+        mode = "i",
+        "<C-k>",
+        function() vim.api.nvim_call_function("codeium#CycleCompletions", {}) end,
+        expr = true,
+        silent = true,
+      },
     },
     config = function() vim.g.codeium_disable_bindings = 1 end,
   },
