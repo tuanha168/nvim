b01a15e42fad6729d69728b7a6b4195abd41a699
TuanHA <achicir2@gmail.com>
2024-03-04 19:44:49 +0700

    Auto Update: 2024-03-04 19:44:49 from Arch Linux

--- plugins/codeium.lua
+++ plugins/codeium.lua
@@ -14,7 +14,8 @@ return {
     -- enabled = false,
     event = "InsertEnter",
     keys = {
-      { mode = { "n" }, "<C-g>", function() return vim.fn["codeium#Chat"]() end },
+      { mode = "n", "<C-g>", function() return vim.fn["codeium#Chat"]() end },
+      { mode = "i", "<C-f>", function() return vim.fn["codeium#Accept"]() end, { expr = true, silent = true } },
     },
     config = function()
       vim.g.codeium_disable_bindings = 1
