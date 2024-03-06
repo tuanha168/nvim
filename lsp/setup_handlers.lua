8b85942294e887f45411d51dfb858658dc31689f
yopaz-tuanha <tuanha@yopaz.vn>
2024-03-06 10:54:47 +0700

    Auto Update: 2024-03-06 10:54:47 from Mac

--- lsp/setup_handlers.lua
+++ lsp/setup_handlers.lua
@@ -1,5 +1,4 @@
 return {
   rust_analyzer = require "user.lsp.handlers.rust_analyzer",
   efm = require "user.lsp.handlers.efm",
-  tsserver = require "user.lsp.handlers.tsserver",
 }
--- plugins/typescript-tools.lua
+++
@@ -1,3 +0,0 @@
-return {
-  { "pmizio/typescript-tools.nvim" },
-}
