c3e4c2fc602cadc4ef562e81d3c0e31d58271173
yopaz-tuanha <tuanha@yopaz.vn>
2024-03-06 09:11:05 +0700

    Auto Update: 2024-03-06 09:11:05 from Mac

--- lsp/config/tsserver.lua
+++ lsp/config/tsserver.lua
@@ -1,13 +1,20 @@
+local util = require "lspconfig.util"
+local function get_typescript_server_path(root_dir)
+  local global_ts = os.getenv "HOME"
+    .. "/.local/share/nvim/mason/packages/vue-language-server/node_modules/@vue/typescript-plugin"
+  local found_ts = ""
+  local function check_dir(path)
+    found_ts = util.path.join(path, "node_modules", "@vue", "typescript-plugin")
+    if util.path.exists(found_ts) then return path end
+  end
+  if util.search_ancestors(root_dir, check_dir) then
+    return found_ts
+  else
+    return global_ts
+  end
+end
+
 return {
-  init_options = {
-    plugins = {
-      {
-        name = "@vue/typescript-plugin",
-        location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
-        languages = { "javascript", "typescript", "vue" },
-      },
-    },
-  },
   filetypes = {
     "javascript",
     "javascript.jsx",
@@ -17,4 +24,8 @@ return {
     "typescriptreact",
     "vue",
   },
+
+  on_new_config = function(new_config, new_root_dir)
+    new_config.init_options.plugins = get_typescript_server_path(new_root_dir)
+  end,
 }
