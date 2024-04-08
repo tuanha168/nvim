6129a49f40a59c7f3ed8b6b136eafdeb03d692e9
TuanHA <achicir2@gmail.com>
2024-04-08 19:42:31 +0700

    Auto Update: 2024-04-08 19:42:31 from Arch Linux

--- lsp/config/tsserver.lua
+++ lsp/config/tsserver.lua
@@ -1,44 +1,6 @@
-local util = require "lspconfig.util"
-local function get_typescript_server_path(root_dir)
-  local global_ts = os.getenv "HOME"
-    .. "/.local/share/nvim/mason/packages/vue-language-server/node_modules/@vue/typescript-plugin"
-
-  if not root_dir then return global_ts end
-
-  local found_ts = ""
-  local function check_dir(path)
-    found_ts = util.path.join(path, "node_modules", "@vue", "typescript-plugin")
-    if util.path.exists(found_ts) then return path end
-  end
-  if util.search_ancestors(root_dir, check_dir) then
-    return found_ts
-  else
-    return global_ts
-  end
-end
-
 return {
   compilerOptions = {
     noErrorTruncate = true,
-  },
-  init_options = {
-    -- preferences = {
-    --   includeInlayParameterNameHints = "all",
-    --   includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-    --   includeInlayFunctionParameterTypeHints = true,
-    --   includeInlayVariableTypeHints = true,
-    --   includeInlayPropertyDeclarationTypeHints = true,
-    --   includeInlayFunctionLikeReturnTypeHints = true,
-    --   includeInlayEnumMemberValueHints = true,
-    --   importModuleSpecifierPreference = "non-relative",
-    -- },
-    -- plugins = {
-    --   {
-    --     name = "@vue/typescript-plugin",
-    --     location = get_typescript_server_path(),
-    --     languages = { "javascript", "typescript", "vue" },
-    --   },
-    -- },
   },
   filetypes = {
     "javascript",
@@ -53,7 +15,8 @@ return {
     new_config.init_options.plugins = {
       {
         name = "@vue/typescript-plugin",
-        location = get_typescript_server_path(),
+        location = require("mason-registry").get_package("vue-language-server"):get_install_path()
+          .. "/node_modules/@vue/language-server",
         languages = { "javascript", "typescript", "vue" },
       },
     }
