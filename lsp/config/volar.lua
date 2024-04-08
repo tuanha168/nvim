197c779daddf7dfd54b24a713e09538e3efe80ea
TuanHA <achicir2@gmail.com>
2024-04-08 19:36:56 +0700

    Auto Update: 2024-04-08 19:36:56 from Arch Linux

--- lsp/config/volar.lua
+++ lsp/config/volar.lua
@@ -13,7 +13,4 @@ return {
       hybridMode = false,
     },
   },
-  -- on_new_config = function(new_config, new_root_dir)
-  --   new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
-  -- end,
 }
