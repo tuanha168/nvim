d52b623004da945053ab6b6ad5b8cd11894502d6
TuanHA <achicir2@gmail.com>
2024-04-08 19:34:01 +0700

    Auto Update: 2024-04-08 19:34:01 from Arch Linux

--- lsp/config/volar.lua
+++ lsp/config/volar.lua
@@ -24,6 +24,11 @@ return {
     "typescriptreact",
     "vue",
   },
+  init_options = {
+    vue = {
+      hybridMode = false,
+    },
+  },
 
   on_new_config = function(new_config, new_root_dir)
     new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
