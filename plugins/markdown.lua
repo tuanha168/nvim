4115befe09df99d348962a74a274ab26213b082d
yopaz-tuanha <tuanha@yopaz.vn>
2024-04-08 11:07:40 +0700

    Auto Update: 2024-04-08 11:07:40 from Mac

--- plugins/markdown.lua
+++ plugins/markdown.lua
@@ -1,3 +1,9 @@
 return {
-  { "ellisonleao/glow.nvim", config = true, cmd = "Glow" }
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function() vim.g.mkdp_filetypes = { "markdown" } end,
+    ft = { "markdown" },
+  },
 }
