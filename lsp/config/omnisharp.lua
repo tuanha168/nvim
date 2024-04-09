ab34140a5bf304b62302cd6d60f969e0ea72a6d3
yopaz-tuanha <tuanha@yopaz.vn>
2024-04-09 16:46:11 +0700

    Auto Update: 2024-04-09 16:46:11 from Mac

---
+++ lsp/config/omnisharp.lua
@@ -0,0 +1,9 @@
+return {
+  enable_editorconfig_support = true,
+  enable_ms_build_load_projects_on_demand = false,
+  enable_roslyn_analyzers = false,
+  organize_imports_on_format = true,
+  enable_import_completion = true,
+  sdk_include_prereleases = true,
+  analyze_open_documents_only = false,
+}
