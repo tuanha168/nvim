fbbc09744151aec5e95641df900f40dc5048c452
yopaz-tuanha <tuanha@yopaz.vn>
2024-04-11 09:01:14 +0700

    Auto Update: 2024-04-11 09:01:13 from Mac

--- plugins/mini.lua
+++ plugins/mini.lua
@@ -35,31 +35,36 @@ return {
       local ok, minifiles = pcall(require, "mini.files")
       if not ok then return end
 
-      local get_ignore_entries = function(entries)
-        local ignored_entries = {}
-        local all_paths = table.concat(vim.tbl_map(function(entry) return entry.path end, entries), "\n")
-        local job_id = vim.fn.jobstart({ "git", "check-ignore", "--stdin" }, {
+      local git_ignore_sorter = function(entries)
+        -- technically can filter entries here too, and checking gitignore for _every entry individually_
+        -- like I would have to in `content.filter` above is too slow. Here we can give it _all_ the entries
+        -- at once, which is much more performant.
+        local all_paths = table.concat(
+          vim.tbl_map(function(entry)
+            return entry.path
+          end, entries),
+          '\n'
+        )
+        local output_lines = {}
+        local job_id = vim.fn.jobstart({ 'git', 'check-ignore', '--stdin' }, {
           stdout_buffered = true,
-          on_stdout = function(_, data) ignored_entries = data end,
+          on_stdout = function(_, data)
+            output_lines = data
+          end,
         })
 
         -- command failed to run
-        if job_id < 1 then return entries end
+        if job_id < 1 then
+          return entries
+        end
 
         -- send paths via STDIN
         vim.fn.chansend(job_id, all_paths)
-        vim.fn.chanclose(job_id, "stdin")
-        vim.fn.jobwait { job_id }
-        return ignored_entries
-      end
-
-      local git_ignore_sorter = function(entries)
-        return minifiles.default_sort(
-          vim.tbl_filter(
-            function(entry) return not vim.tbl_contains(get_ignore_entries(entries), entry.path) end,
-            entries
-          )
-        )
+        vim.fn.chanclose(job_id, 'stdin')
+        vim.fn.jobwait({ job_id })
+        return require('mini.files').default_sort(vim.tbl_filter(function(entry)
+          return not vim.tbl_contains(output_lines, entry.path)
+        end, entries))
       end
 
       vim.api.nvim_create_autocmd("User", {
