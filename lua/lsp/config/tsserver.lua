3c85815aaeee201245bd57ac39121c36de7a5c38
TuanHA <achicir2@gmail.com>
2024-05-03 03:08:05 +0700

    Auto Update: 2024-05-03 03:08:05 from Arch Linux

--- lua/lsp/config/tsserver.lua
+++ lua/lsp/config/tsserver.lua
@@ -1,5 +1,5 @@
 local registry = require "mason-registry"
-registry.refresh(function() Print(require("mason-lspconfig").get_available_servers()) end)
+registry.update(function() Print(require("mason-lspconfig").get_available_servers()) end)
 
 Print("outside", require("mason-lspconfig").get_available_servers())
 local tsdk = registry.get_package("typescript-language-server"):get_install_path() .. "/node_modules/typescript/lib"
