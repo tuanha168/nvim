6c6f15b35431dc60198a2ddd74c6050cc8b75fc1
TuanHA <achicir2@gmail.com>
2024-05-03 03:13:02 +0700

    Auto Update: 2024-05-03 03:13:02 from Arch Linux

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -2,6 +2,7 @@
 -- Configuration documentation can be found with `:h astrolsp`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
 --       as this provides autocomplete and documentation while editing
+if true then return {} end
 
 ---@type LazySpec
 return {
