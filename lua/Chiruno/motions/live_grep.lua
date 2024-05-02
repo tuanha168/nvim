d6e190c5f51ce4a9f1531bbf869b4097e1f5f2dc
TuanHA <achicir2@gmail.com>
2024-05-03 03:59:57 +0700

    Auto Update: 2024-05-03 03:59:57 from Arch Linux

--- lua/Chiruno/motions/live_grep.lua
+++ lua/Chiruno/motions/live_grep.lua
@@ -1,12 +1,16 @@
 function Chiruno.motions.live_grep_motion(mode)
-  local ok, telescope = pcall(require, "telescope")
-  if ok then
+  local telescope_ok, telescope = pcall(require, "telescope")
+  if telescope_ok then
     telescope.grep_string {
       search = Chiruno.func.get_text_selection(mode).text,
       hidden = true,
     }
     vim.schedule(function() Chiruno.func.feedkeys("<ESC>", "i") end)
+    return
   end
+
+  local fzf_ok, fzf = pcall(require, "fzf-lua")
+  if fzf_ok then fzf.grep() end
 end
 
 return Chiruno.motions.live_grep_motion
