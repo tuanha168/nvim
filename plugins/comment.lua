8e4b2390a6d9883ccfc70dc7696a26b806b87e4e
yopaz-tuanha <tuanha@yopaz.vn>
2024-05-02 14:21:57 +0700

    Auto Update: 2024-05-02 14:21:57 from Mac

--- plugins/comment.lua
+++ plugins/comment.lua
@@ -6,13 +6,14 @@ return {
       { "gb", mode = { "n", "v" }, desc = "Comment toggle blockwise" },
     },
     opts = function()
-      local commentstring_avail, commentstring = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
+      -- local commentstring_avail, commentstring = pcall(require, "ts_context_commentstring")
+      -- if commentstring_avail and commentstring then commentstring.setup {} end
+      vim.g.skip_ts_context_commentstring_module = true
       local opts = {
         ignore = "^$",
       }
-      if commentstring_avail and commentstring then opts.pre_hook = commentstring.create_pre_hook() end
-      local ft = require('Comment.ft')
-      ft.set('json', '#%s')
+      local ft = require "Comment.ft"
+      ft.set("json", "#%s")
 
       return opts
     end,
