d63db5a19e5ee33ec4bf134a2f05de9c116e446c
TuanHA <achicir2@gmail.com>
2024-05-03 03:20:11 +0700

    Auto Update: 2024-05-03 03:20:11 from Arch Linux

--- lua/lsp/config/tsserver.lua
+++ lua/lsp/config/tsserver.lua
@@ -1,47 +1,45 @@
 return function()
-  vim.schedule(function()
-    local registry = require "mason-registry"
+  local registry = require "mason-registry"
 
-    local tsdk = registry.get_package("typescript-language-server"):get_install_path() .. "/node_modules/typescript/lib"
-    local vuePlugin = registry.get_package("vue-language-server"):get_install_path()
-      .. "/node_modules/@vue/language-server"
+  local tsdk = registry.get_package("typescript-language-server"):get_install_path() .. "/node_modules/typescript/lib"
+  local vuePlugin = registry.get_package("vue-language-server"):get_install_path()
+    .. "/node_modules/@vue/language-server"
 
-    return {
-      compilerOptions = {
-        noErrorTruncate = true,
-      },
-      filetypes = {
-        "javascript",
-        "javascript.jsx",
-        "javascriptreact",
-        "typescript",
-        "typescript.tsx",
-        "typescriptreact",
-        "vue",
-      },
-      init_options = {
-        plugins = {
-          {
-            name = "@vue/typescript-plugin",
-            location = vuePlugin,
-            languages = { "javascript", "typescript", "vue" },
-          },
-        },
-        tsserver = {
-          -- This overwrite the path from the local project, in case your project ts version is not compatible with the plugin
-          path = tsdk,
-        },
-        preferences = {
-          includeInlayParameterNameHints = "all",
-          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-          includeInlayFunctionParameterTypeHints = true,
-          -- includeInlayVariableTypeHints = true,
-          includeInlayPropertyDeclarationTypeHints = true,
-          -- includeInlayFunctionLikeReturnTypeHints = true,
-          includeInlayEnumMemberValueHints = true,
-          importModuleSpecifierPreference = "non-relative",
+  return {
+    compilerOptions = {
+      noErrorTruncate = true,
+    },
+    filetypes = {
+      "javascript",
+      "javascript.jsx",
+      "javascriptreact",
+      "typescript",
+      "typescript.tsx",
+      "typescriptreact",
+      "vue",
+    },
+    init_options = {
+      plugins = {
+        {
+          name = "@vue/typescript-plugin",
+          location = vuePlugin,
+          languages = { "javascript", "typescript", "vue" },
         },
       },
-    }
-  end)
+      tsserver = {
+        -- This overwrite the path from the local project, in case your project ts version is not compatible with the plugin
+        path = tsdk,
+      },
+      preferences = {
+        includeInlayParameterNameHints = "all",
+        includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+        includeInlayFunctionParameterTypeHints = true,
+        -- includeInlayVariableTypeHints = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        -- includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+        importModuleSpecifierPreference = "non-relative",
+      },
+    },
+  }
 end
