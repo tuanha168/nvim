455eeb5f37b66a7ee31a11774e2556d06bbd1554
yopaz-tuanha <tuanha@yopaz.vn>
2024-05-02 14:23:40 +0700

    Auto Update: 2024-05-02 14:23:40 from Mac

--- plugins/comment.lua
+++ plugins/comment.lua
@@ -8,12 +8,12 @@ return {
     opts = function()
       -- local commentstring_avail, commentstring = pcall(require, "ts_context_commentstring")
       -- if commentstring_avail and commentstring then commentstring.setup {} end
-      vim.g.skip_ts_context_commentstring_module = true
-      local opts = {
-        ignore = "^$",
-      }
-      local ft = require "Comment.ft"
-      ft.set("json", "#%s")
+      -- vim.g.skip_ts_context_commentstring_module = true
+      -- local opts = {
+      --   ignore = "^$",
+      -- }
+      -- local ft = require "Comment.ft"
+      -- ft.set("json", "#%s")
 
       return opts
     end,
