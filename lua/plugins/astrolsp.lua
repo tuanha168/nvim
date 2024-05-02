bb6bf5e579bb647fa6cc886a59153332f331c6da
TuanHA <achicir2@gmail.com>
2024-05-03 02:05:01 +0700

    Auto Update: 2024-05-03 02:05:01 from Arch Linux

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -61,7 +61,7 @@ return {
       omnisharp = require "lsp.config.omnisharp",
       stylelint_lsp = require "lsp.config.stylelint_lsp",
       tailwindcss = require "lsp.config.tailwindcss",
-      -- tsserver = require "lsp.config.tsserver",
+      tsserver = require "lsp.config.tsserver",
       volar = require "lsp.config.volar",
     },
     -- customize how language servers are attached
