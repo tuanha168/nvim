20013aa1aa1dc60172b7cc31b0d09f5f8a09292c
TuanHA <achicir2@gmail.com>
2024-05-03 02:30:33 +0700

    Auto Update: 2024-05-03 02:30:33 from Arch Linux

--- lua/lsp/config/tsserver.lua
+++ lua/lsp/config/tsserver.lua
@@ -1,42 +1,44 @@
-local tsdk = require("mason-registry").get_package("typescript-language-server"):get_install_path()
-    .. "/node_modules/typescript/lib"
-local vuePlugin = require("mason-registry").get_package("vue-language-server"):get_install_path()
-    .. "/node_modules/@vue/language-server"
+return function()
+  local tsdk = require("mason-registry").get_package("typescript-language-server"):get_install_path()
+      .. "/node_modules/typescript/lib"
+  local vuePlugin = require("mason-registry").get_package("vue-language-server"):get_install_path()
+      .. "/node_modules/@vue/language-server"
 
-return {
-  compilerOptions = {
-    noErrorTruncate = true,
-  },
-  filetypes = {
-    "javascript",
-    "javascript.jsx",
-    "javascriptreact",
-    "typescript",
-    "typescript.tsx",
-    "typescriptreact",
-    "vue",
-  },
-  init_options = {
-    plugins = {
-      {
-        name = "@vue/typescript-plugin",
-        location = vuePlugin,
-        languages = { "javascript", "typescript", "vue" },
+  return {
+    compilerOptions = {
+      noErrorTruncate = true,
+    },
+    filetypes = {
+      "javascript",
+      "javascript.jsx",
+      "javascriptreact",
+      "typescript",
+      "typescript.tsx",
+      "typescriptreact",
+      "vue",
+    },
+    init_options = {
+      plugins = {
+        {
+          name = "@vue/typescript-plugin",
+          location = vuePlugin,
+          languages = { "javascript", "typescript", "vue" },
+        },
+      },
+      tsserver = {
+        -- This overwrite the path from the local project, in case your project ts version is not compatible with the plugin
+        path = tsdk,
+      },
+      preferences = {
+        includeInlayParameterNameHints = "all",
+        includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+        includeInlayFunctionParameterTypeHints = true,
+        -- includeInlayVariableTypeHints = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        -- includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+        importModuleSpecifierPreference = "non-relative",
       },
     },
-    tsserver = {
-      -- This overwrite the path from the local project, in case your project ts version is not compatible with the plugin
-      path = tsdk,
-    },
-    preferences = {
-      includeInlayParameterNameHints = "all",
-      includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-      includeInlayFunctionParameterTypeHints = true,
-      -- includeInlayVariableTypeHints = true,
-      includeInlayPropertyDeclarationTypeHints = true,
-      -- includeInlayFunctionLikeReturnTypeHints = true,
-      includeInlayEnumMemberValueHints = true,
-      importModuleSpecifierPreference = "non-relative",
-    },
-  },
-}
+  }
+end
--- lua/lsp/setup_handlers.lua
+++ lua/lsp/setup_handlers.lua
@@ -1,4 +1,5 @@
 return {
   rust_analyzer = require "lsp.handlers.rust_analyzer",
   efm = require "lsp.handlers.efm",
+  tsserver = require "lsp.config.tsserver",
 }
