8833100ef7e5a5454a7e43e7771d7f9a4126899a
yopaz-tuanha <tuanha@yopaz.vn>
2024-05-03 11:54:44 +0700

    Auto Update: 2024-05-03 11:54:44 from Mac

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -17,7 +17,17 @@ return {
     keys = {
       { mode = "n", "<C-g>", function() return vim.fn["codeium#Chat"]() end, expr = true, silent = true },
       { mode = "i", "<C-f>", function() return vim.fn["codeium#Accept"]() end, expr = true, silent = true },
-      { mode = "i", "<C-j>", function() return vim.fn["codeium#CycleCompletions"](1) end, expr = true, silent = true },
+      {
+        mode = "i",
+        "<C-j>",
+        function()
+          local cmp_ok, cmp = pcall(require, "cmp")
+          if cmp_ok then cmp.mapping.abort() end
+          return vim.fn["codeium#CycleCompletions"](1)
+        end,
+        expr = true,
+        silent = true,
+      },
       { mode = "i", "<C-k>", function() return vim.fn["codeium#CycleCompletions"](-1) end, expr = true, silent = true },
     },
     config = function() vim.g.codeium_disable_bindings = 1 end,
