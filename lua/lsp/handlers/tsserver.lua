1ef96bfc7d201d117d08503b9f056fa8c9769597
yopaz-tuanha <tuanha@yopaz.vn>
2024-05-06 17:07:31 +0700

    Auto Update: 2024-05-06 17:07:31 from Mac

--- lua/lsp/handlers/tsserver.lua
+++ lua/lsp/handlers/tsserver.lua
@@ -5,13 +5,13 @@ return function(_, opts)
   local vuePlugin = registry.get_package("vue-language-server"):get_install_path()
     .. "/node_modules/@vue/typescript-plugin"
 
-  opts.init_options.plugins = {
-    {
-      name = "@vue/typescript-plugin",
-      location = vuePlugin,
-      languages = { "javascript", "typescript", "vue" },
-    },
-  }
+  -- opts.init_options.plugins = {
+  --   {
+  --     name = "@vue/typescript-plugin",
+  --     location = vuePlugin,
+  --     languages = { "javascript", "typescript", "vue" },
+  --   },
+  -- }
   opts.init_options.tsserver = {
     -- This overwrite the path from the local project, in case your project ts version is not compatible with the plugin
     path = tsdk,
