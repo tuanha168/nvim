6d46da8fe77fd828103f07bfa4068abda161c03a
yopaz-tuanha <tuanha@yopaz.vn>
2024-05-10 13:59:00 +0700

    Auto Update: 2024-05-10 13:59:00 from Mac

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -37,8 +37,8 @@ return {
       --     fallback()
       --   end
       -- end, { "i", "s" })
-      opts.mapping["<C-k>"] = cmp.mapping(function(fallback) fallback() end)
-      opts.mapping["<C-j>"] = cmp.mapping(function(fallback) fallback() end)
+      opts.mapping["<C-k>"] = cmp.mapping(function() end)
+      opts.mapping["<C-j>"] = cmp.mapping(function() end)
 
       local snip_status_ok, luasnip = pcall(require, "luasnip")
 
