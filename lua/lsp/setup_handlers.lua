a96c7cbf42524d9a282377d4c81269666db213a1
TuanHA <achicir2@gmail.com>
2024-05-19 00:48:23 +0700

    Auto Update: 2024-05-19 00:48:23 from Arch Linux

--- lua/lsp/setup_handlers.lua
+++ lua/lsp/setup_handlers.lua
@@ -3,6 +3,7 @@ return {
   efm = require "lsp.handlers.efm",
   tsserver = require "lsp.handlers.tsserver",
   eslint = function(_, opts)
+    Print(opts)
     require("lspconfig").eslint.setup(opts)
   end,
   -- volar = function() end,
