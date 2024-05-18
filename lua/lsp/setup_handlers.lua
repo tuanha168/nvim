cb458b3f72a050511dc0a9760e38af325f40e0c3
TuanHA <achicir2@gmail.com>
2024-05-19 00:47:33 +0700

    Auto Update: 2024-05-19 00:47:33 from Arch Linux

--- lua/lsp/setup_handlers.lua
+++ lua/lsp/setup_handlers.lua
@@ -2,5 +2,8 @@ return {
   rust_analyzer = require "lsp.handlers.rust_analyzer",
   efm = require "lsp.handlers.efm",
   tsserver = require "lsp.handlers.tsserver",
+  eslint = function(_, opts)
+    require("lspconfig").eslint.setup(opts)
+  end,
   -- volar = function() end,
 }
