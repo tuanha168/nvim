110a1bb0ee11ba4c8d5043083ea04c51cd078b15
TuanHA <achicir2@gmail.com>
2024-05-23 22:33:32 +0700

    Auto Update: 2024-05-23 22:33:32 from Arch Linux

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -3,6 +3,10 @@ return {
     "huggingface/llm.nvim",
     opts = {
       backend = "ollama",
+      lsp = {
+        bin_path = vim.api.nvim_call_function("stdpath", { "data" }) .. "/mason/bin/llm-ls",
+      },
+      enable_suggestions_on_startup = true,
       -- model = "codellama:7b",
       -- url = "http://localhost:11434/api/generate",
       -- -- cf https://github.com/ollama/ollama/blob/main/docs/api.md#parameters
