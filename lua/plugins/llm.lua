71853de6068de08fc21c09c492dc0ac33d3b7957
TuanHA <achicir2@gmail.com>
2024-05-23 23:07:32 +0700

    Auto Update: 2024-05-23 23:07:29 from Arch Linux

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -7,14 +7,23 @@ return {
         bin_path = vim.api.nvim_call_function("stdpath", { "data" }) .. "/mason/bin/llm-ls",
       },
       enable_suggestions_on_startup = true,
-      model = "deepseek-coder:6.7b",
+      -- model = "deepseek-coder:6.7b",
+      model = "llama3",
       url = "http://localhost:11434/api/generate",
+      tokens_to_clear = { "<|endoftext|>" },
+      fim = {
+        enabled = true,
+        prefix = "<fim_prefix>",
+        middle = "<fim_middle>",
+        suffix = "<fim_suffix>",
+      },
+      context_window = 8192,
       accept_keymap = "<C-f>",
       dismiss_keymap = "<C-d>",
       tls_skip_verify_insecure = false,
       request_body = {
-        parameters = {
-          max_new_tokens = 60,
+        options = {
+          -- max_new_tokens = 60,
           temperature = 0.2,
           top_p = 0.95,
         },
