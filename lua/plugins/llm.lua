2809ec8ea9e39bccf14870549be342d89a42aaec
TuanHA <achicir2@gmail.com>
2024-05-23 22:34:20 +0700

    Auto Update: 2024-05-23 22:34:19 from Arch Linux

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -7,9 +7,8 @@ return {
         bin_path = vim.api.nvim_call_function("stdpath", { "data" }) .. "/mason/bin/llm-ls",
       },
       enable_suggestions_on_startup = true,
-      -- model = "codellama:7b",
-      -- url = "http://localhost:11434/api/generate",
-      -- -- cf https://github.com/ollama/ollama/blob/main/docs/api.md#parameters
+      model = "llama3",
+      url = "http://localhost:11434/api/generate",
       -- request_body = {
       --   -- Modelfile options for the model you use
       --   options = {
