870d7855371ad4fb511ef36e7672b8d331722828
TuanHA <achicir2@gmail.com>
2024-05-23 22:57:34 +0700

    Auto Update: 2024-05-23 22:57:30 from Arch Linux

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -14,7 +14,7 @@ return {
         ollama = require("codecompanion.adapters").use("ollama", {
           schema = {
             model = {
-              default = "deepseek-coder:6.7b",
+              default = "llama3",
             },
           },
         }),
