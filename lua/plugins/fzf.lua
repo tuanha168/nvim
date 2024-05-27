e9da4e318867c4b17399c5689c4b1a73143e1e5c
TuanHA <achicir2@gmail.com>
2024-05-28 02:03:29 +0700

    Auto Update: 2024-05-28 02:03:28 from Arch Linux

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -6,7 +6,13 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       -- calling `setup` is optional for customization
-      require("fzf-lua").setup {}
+      require("fzf-lua").setup {
+        keymap = {
+          fzf = {
+            ["ctrl-q"] = "select-all+accept",
+          },
+        },
+      }
     end,
     keys = {
       {
