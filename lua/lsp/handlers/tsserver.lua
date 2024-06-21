c3aaeb06c4bd3288a65932184c3a43e0414ffc42
yopaz-tuanha <tuanha@yopaz.vn>
2024-06-21 11:18:12 +0700

    Auto Update: 2024-06-21 11:18:12 from Arch Linux

--- lua/lsp/handlers/tsserver.lua
+++ lua/lsp/handlers/tsserver.lua
@@ -1,7 +1,7 @@
 return function(_, opts)
   local registry = require "mason-registry"
 
-  local tsdk = registry.get_package("typescript-language-server"):get_install_path() .. "/node_modules/typescript/lib"
+  -- local tsdk = registry.get_package("typescript-language-server"):get_install_path() .. "/node_modules/typescript/lib"
   local vuePlugin = registry.get_package("vue-language-server"):get_install_path()
     .. "/node_modules/@vue/language-server"
 
@@ -12,9 +12,9 @@ return function(_, opts)
       languages = { "javascript", "typescript", "vue" },
     },
   }
-  opts.init_options.tsserver = {
-    -- This overwrite the path from the local project, in case your project ts version is not compatible with the plugin
-    path = tsdk,
-  }
+  -- opts.init_options.tsserver = {
+  --   -- This overwrite the path from the local project, in case your project ts version is not compatible with the plugin
+  --   path = tsdk,
+  -- }
   require("lspconfig").tsserver.setup(opts)
 end
