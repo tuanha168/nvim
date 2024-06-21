c1b826f0ce3e46a29ac23ee1ca181165a9c7ffe7
yopaz-tuanha <tuanha@yopaz.vn>
2024-06-21 17:17:42 +0700

    Auto Update: 2024-06-21 17:17:42 from Arch Linux

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -23,7 +23,6 @@ return {
         function()
           -- local cmp_ok, cmp = pcall(require, "cmp")
           -- if cmp_ok then cmp.abort() end
-          Print('c-j')
           return vim.fn["codeium#CycleCompletions"](1)
         end,
         expr = true,
@@ -35,7 +34,6 @@ return {
         function()
           -- local cmp_ok, cmp = pcall(require, "cmp")
           -- if cmp_ok then cmp.abort() end
-          Print('c-k')
           return vim.fn["codeium#CycleCompletions"](-1)
         end,
         expr = true,
