a49c44bac74de0d4c9950bdf886f00b1e5c3dfe6
TuanHA <achicir2@gmail.com>
2024-06-23 00:25:00 +0700

    Auto Update: 2024-06-23 00:24:59 from Arch Linux

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,6 +10,12 @@ local mappings = {
     ["<Leader>gl"] = false,
     ["<Leader>gL"] = false,
     ["<Leader>h"] = false,
+    ["<C-N>"] = {
+      function()
+        local astro = require "astrocore"
+        astro.toggle_term_cmd("yazi")
+      end,
+    },
     ["\\"] = false,
     ["|"] = false,
     ["<C-S-Up>"] = { function() require("smart-splits").resize_up() end, noremap = true, desc = "Resize split up" },
