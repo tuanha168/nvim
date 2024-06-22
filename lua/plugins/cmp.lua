5c717ef96476806c39a8209702d743a112feb738
TuanHA <achicir2@gmail.com>
2024-06-22 09:11:45 +0700

    Auto Update: 2024-06-22 09:11:45 from Arch Linux

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -50,7 +50,12 @@ return {
 
       opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
-          cmp.select_next_item()
+          local entry = cmp.get_selected_entry()
+          if not entry then
+            cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
+          else
+            cmp.confirm()
+          end
         elseif snip_status_ok and luasnip.expand_or_jumpable() then
           luasnip.expand_or_jump()
         elseif vim.snippet.active { direction = 1 } then
