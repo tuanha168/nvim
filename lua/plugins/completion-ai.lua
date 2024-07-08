ab30630f1d15b145c650540d10b2570d6f31aed2
yopaz-tuanha <tuanha@yopaz.vn>
2024-07-08 18:43:30 +0700

    Auto Update: 2024-07-08 18:43:30 from Arch Linux

--- lua/plugins/completion-ai.lua
+++ lua/plugins/completion-ai.lua
@@ -41,53 +41,55 @@ return {
   --   build = "./dl_binaries.sh",
   --   config = function() require("config.tabnine").setupClient() end,
   -- },
-  {
-    "olimorris/codecompanion.nvim",
-    enabled = false,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    init = function()
-      require("codecompanion").setup {
-        strategies = {
-          chat = "ollama",
-          inline = "ollama",
-        },
 
-        adapters = {
-          ollama = require("codecompanion.adapters").use("ollama", {
-            schema = {
-              model = {
-                default = "deepseek-coder:6.7b",
-              },
-            },
-          }),
-        },
-      }
-    end,
-    config = function()
-      -- Expand `cc` into CodeCompanion in the command line
-      vim.cmd [[cab cc CodeCompanion]]
-    end,
-    keys = {
-      {
-        "<leader>ic",
-        "<cmd>CodeCompanionToggle<CR>",
-        desc = "A[I] [C]hat",
-        mode = { "n", "v" },
-      },
-      {
-        "<leader>ia",
-        "<cmd>CodeCompanion<CR>",
-        desc = "A[I] [A]sk",
-        mode = { "n", "v" },
-      },
-      {
-        "<leader>il",
-        "<cmd>CodeCompanionActions<CR>",
-        desc = "A[I] [L]ist of actions",
-        mode = { "n", "v" },
-      },
-    },
-  },
+  -- olimorris
+  -- {
+  --   "olimorris/codecompanion.nvim",
+  --   enabled = false,
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --   },
+  --   init = function()
+  --     require("codecompanion").setup {
+  --       strategies = {
+  --         chat = "ollama",
+  --         inline = "ollama",
+  --       },
+
+  --       adapters = {
+  --         ollama = require("codecompanion.adapters").use("ollama", {
+  --           schema = {
+  --             model = {
+  --               default = "deepseek-coder:6.7b",
+  --             },
+  --           },
+  --         }),
+  --       },
+  --     }
+  --   end,
+  --   config = function()
+  --     -- Expand `cc` into CodeCompanion in the command line
+  --     vim.cmd [[cab cc CodeCompanion]]
+  --   end,
+  --   keys = {
+  --     {
+  --       "<leader>ic",
+  --       "<cmd>CodeCompanionToggle<CR>",
+  --       desc = "A[I] [C]hat",
+  --       mode = { "n", "v" },
+  --     },
+  --     {
+  --       "<leader>ia",
+  --       "<cmd>CodeCompanion<CR>",
+  --       desc = "A[I] [A]sk",
+  --       mode = { "n", "v" },
+  --     },
+  --     {
+  --       "<leader>il",
+  --       "<cmd>CodeCompanionActions<CR>",
+  --       desc = "A[I] [L]ist of actions",
+  --       mode = { "n", "v" },
+  --     },
+  --   },
+  -- },
 }
