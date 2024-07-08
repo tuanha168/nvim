e03be0f0770ee8cb5b33cb5268507cd243d1d91d
yopaz-tuanha <tuanha@yopaz.vn>
2024-07-08 18:44:54 +0700

    Auto Update: 2024-07-08 18:44:54 from Arch Linux

--- lua/plugins/completion-ai.lua
+++ lua/plugins/completion-ai.lua
@@ -31,6 +31,27 @@ return {
     end,
   },
 
+  -- copilot
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    enabled = false,
+    opts = {
+      panel = {
+        enabled = false,
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<C-f>",
+          next = "<C-j>",
+          prev = "<C-k>",
+        },
+      },
+    },
+  },
+
   -- codeium
   -- {
   --   "jcdickinson/codeium.nvim",
