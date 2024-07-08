8381a5dd2b2aacb2abbc78439d129979498a61cf
yopaz-tuanha <tuanha@yopaz.vn>
2024-07-08 18:44:47 +0700

    Auto Update: 2024-07-08 18:44:47 from Arch Linux

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,22 +1,3 @@
 ---@type LazySpec
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    event = "InsertEnter",
-    enabled = false,
-    opts = {
-      panel = {
-        enabled = false,
-      },
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        keymap = {
-          accept = "<C-f>",
-          next = "<C-j>",
-          prev = "<C-k>",
-        },
-      },
-    },
-  },
 }
