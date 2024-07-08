392b830f7ff902a6ab371a21b545149f5d047318
yopaz-tuanha <tuanha@yopaz.vn>
2024-07-08 18:36:34 +0700

    Auto Update: 2024-07-08 18:36:34 from Arch Linux

--- lua/plugins/super-maven.lua
+++ lua/plugins/super-maven.lua
@@ -2,9 +2,9 @@ return {
   {
     "supermaven-inc/supermaven-nvim",
     opts = {
-      keymaps = {
-        accept_suggestion = "<C-F>",
-      },
+      -- keymaps = {
+      --   accept_suggestion = "<C-F>",
+      -- },
       -- ignore_filetypes = { cpp = true },
       -- color = {
       --   suggestion_color = "#ffffff",
@@ -12,7 +12,7 @@ return {
       -- },
       -- log_level = "info", -- set to "off" to disable logging completely
       -- disable_inline_completion = false, -- disables inline completion for use with cmp
-      -- disable_keymaps = false, -- disables built in keymaps for more manual control
+      disable_keymaps = true, -- disables built in keymaps for more manual control
     },
   },
 }
