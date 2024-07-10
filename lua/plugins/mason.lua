28e1900c162e81a3004e2c8746e3f51040b17934
yopaz-tuanha <tuanha@yopaz.vn>
2024-07-10 11:32:58 +0700

    Auto Update: 2024-07-10 11:32:57 from Arch Linux

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -64,8 +64,15 @@ return {
         "pint",
         "tfsec",
       })
-      opts.handlers.prettierd = function ()
-        Print("prettierd")
+      opts.handlers.prettierd = function()
+        require("null-ls").register(require("null-ls").builtins.formatting.prettier.with {
+          condition = function(utils)
+            return utils.root_has_file "package.json"
+              or utils.root_has_file ".prettierrc"
+              or utils.root_has_file ".prettierrc.json"
+              or utils.root_has_file ".prettierrc.js"
+          end,
+        })
       end
     end,
   },
