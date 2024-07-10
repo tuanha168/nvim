45a67a01e7c426cc6564585ee340fd258a25133d
yopaz-tuanha <tuanha@yopaz.vn>
2024-07-10 18:02:29 +0700

    Auto Update: 2024-07-10 18:02:29 from Arch Linux

--- lua/lsp/config/eslint.lua
+++ lua/lsp/config/eslint.lua
@@ -1,6 +1,7 @@
 local util = require "lspconfig.util"
 
 return {
+  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "json" },
   root_dir = util.root_pattern(
     ".eslintrc.cjs",
     ".eslintrc.js",
