2b9af40a739cc80ca1993ec9fedc6b89fa71cf23
yopaz-tuanha <tuanha@yopaz.vn>
2024-07-12 13:26:09 +0700

    Auto Update: 2024-07-12 13:26:09 from Arch Linux

--- lua/lsp/config/tsserver.lua
+++ lua/lsp/config/tsserver.lua
@@ -13,11 +13,11 @@ return {
   },
   init_options = {
     preferences = {
-      -- includeInlayParameterNameHints = "all",
-      -- includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-      -- includeInlayFunctionParameterTypeHints = true,
-      -- includeInlayPropertyDeclarationTypeHints = true,
-      -- includeInlayEnumMemberValueHints = true,
+      includeInlayParameterNameHints = "all",
+      includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+      includeInlayFunctionParameterTypeHints = true,
+      includeInlayPropertyDeclarationTypeHints = true,
+      includeInlayEnumMemberValueHints = true,
       importModuleSpecifierPreference = "non-relative",
 
       -- includeInlayVariableTypeHints = true,
