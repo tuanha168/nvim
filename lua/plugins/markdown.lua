c6aee88c8623bfb05b7e2f24c2e60263a55334d0
yopaz-tuanha <tuanha@yopaz.vn>
2024-08-05 10:38:16 +0700

    Auto Update: 2024-08-05 10:38:16 from Arch Linux

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -8,10 +8,9 @@ return {
   --   ft = { "markdown" },
   -- },
   {
-    "tadmccorkle/markdown.nvim",
-    ft = "markdown", -- or 'event = "VeryLazy"'
-    opts = {
-      -- configuration here or empty for defaults
-    },
+    "MeanderingProgrammer/markdown.nvim",
+    opts = {},
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
   },
 }
