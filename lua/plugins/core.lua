c761b5307d2d8c4bb38ee7f8d6f74387ab30d12a
yopaz-tuanha <tuanha@yopaz.vn>
2024-08-06 15:24:55 +0700

    Auto Update: 2024-08-06 15:24:55 from Arch Linux

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -59,6 +59,7 @@ return {
 
   {
     "max397574/better-escape.nvim",
+    commit = "7e86edafb8c7e73699e0320f225464a298b96d12",
     opts = function(_, opts)
       opts.mapping = { "jk", "jj", "kk" }
       opts.clear_empty_lines = true
