37af2f30902f17200fc7f85caad182c7eaacd203
yopaz-tuanha <tuanha@yopaz.vn>
2024-09-10 13:07:46 +0700

    Auto Update: 2024-09-10 13:07:46 from Arch Linux

--- lua/Chiruno/lsp/lsp_rename.lua
+++ lua/Chiruno/lsp/lsp_rename.lua
@@ -1,16 +1,9 @@
-function Chiruno.lsp.get_clients(opts)
-  local ret = {} ---@type lsp.Client[]
+function Chiruno.lsp.get_clients()
+  local ret = {}
   if vim.lsp.get_clients then
-    ret = vim.lsp.get_clients(opts)
-  else
-    ---@diagnostic disable-next-line: deprecated
-    ret = vim.lsp.get_active_clients(opts)
-    if opts and opts.method then
-      ---@param client lsp.Client
-      ret = vim.tbl_filter(function(client) return client.supports_method(opts.method, { bufnr = opts.bufnr }) end, ret)
-    end
+    ret = vim.lsp.get_clients()
   end
-  return opts and opts.filter and vim.tbl_filter(opts.filter, ret) or ret
+  return ret
 end
 
 function Chiruno.lsp.on_rename(from, to)
