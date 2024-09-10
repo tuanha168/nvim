0f95839870f1c10c4ef923d375aa16c906312de3
yopaz-tuanha <tuanha@yopaz.vn>
2024-09-10 17:55:27 +0700

    Auto Update: 2024-09-10 17:55:27 from Arch Linux

--- lua/lsp/handlers/lua_ls.lua
+++ lua/lsp/handlers/lua_ls.lua
@@ -1,4 +1,7 @@
 return function(_, opts)
+  opts.settings = opts.settings or {}
+  opts.settings.hint = opts.settings.hint or {}
+
   opts.settings.hint.enable = true
 
   require("lspconfig").lua_ls.setup(opts)
