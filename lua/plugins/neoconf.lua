1e40d7ed9cb7d9310aca154e6a7e2af472526626
yopaz-tuanha <tuanha@yopaz.vn>
2024-09-10 19:20:58 +0700

    Auto Update: 2024-09-10 19:20:58 from Arch Linux

--- lua/plugins/neoconf.lua
+++ lua/plugins/neoconf.lua
@@ -5,10 +5,69 @@ return {
     build = "luarocks install json.lua",
     opts = {
       plugin = {
-        ---@type lspconfig.options
         lspconfig = {
-
-        }
+          ---@type lspconfig.options.volar
+          volar = {
+            settings = {
+              vue = {
+                server = {
+                  hybridMode = false,
+                },
+                inlayHints = {
+                  missingProps = true,
+                  optionsWrapper = true,
+                  vBindShorthand = true,
+                  destructuredProps = true,
+                  inlineHandlerLeading = true,
+                },
+              },
+              typescript = {
+                tsserver = {
+                  web = {
+                    projectWideIntellisense = {
+                      enabled = "true",
+                    },
+                  },
+                },
+                inlayHints = {
+                  enumMemberValues = { enabled = "true" },
+                  propertyDeclarationTypes = { enabled = "true" },
+                  parameterNames = {
+                    enabled = "all",
+                    suppressWhenArgumentMatchesName = "true",
+                  },
+                  -- functionLikeReturnTypes = { enabled = "true" },
+                  -- variableTypes = {
+                  --   enabled = "true",
+                  --   suppressWhenTypeMatchesName = "true",
+                  -- },
+                  parameterTypes = { enabled = "true" },
+                },
+              },
+              css = {
+                customData = {
+                  home .. "/.config/nvim/.vscode/tailwind.json",
+                },
+              },
+              javascript = {
+                inlayHints = {
+                  enumMemberValues = { enabled = "true" },
+                  propertyDeclarationTypes = { enabled = "true" },
+                  parameterNames = {
+                    enabled = "all",
+                    suppressWhenArgumentMatchesName = "true",
+                  },
+                  -- functionLikeReturnTypes = { enabled = "true" },
+                  -- variableTypes = {
+                  --   enabled = "true",
+                  --   suppressWhenTypeMatchesName = "true",
+                  -- },
+                  parameterTypes = { enabled = "true" },
+                },
+              },
+            },
+          },
+        },
       },
       lua_ls = {
         enabled = true,
