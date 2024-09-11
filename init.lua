178f8f37a46836dae4ed2ee01c6b989a41165952
yopaz-tuanha <tuanha@yopaz.vn>
2024-09-11 15:46:51 +0700

    Auto Update: 2024-09-11 15:46:51 from Arch Linux

--- init.lua
+++ init.lua
@@ -18,24 +18,23 @@ if not pcall(require, "lazy") then
   vim.cmd.quit()
 end
 
-local ellipsis = '...'
+local ellipsis = "..."
 local methods = vim.lsp.protocol.Methods
 local inlay_hint_handler = vim.lsp.handlers[methods.textDocument_inlayHint]
 
 vim.lsp.handlers[methods.textDocument_inlayHint] = function(err, result, ctx, config)
-    local client = vim.lsp.get_client_by_id(ctx.client_id)
-    if client and (client.name == 'ts_ls' or client.name == 'volar') then
-        result = vim.iter(result):map(function(hint)
-            local label = hint.label ---@type string
-            if label:len() >= 5 then
-                label = label:sub(1, 4) .. ellipsis
-            end
-            hint.label = label
-            return hint
-        end)
-    end
+  local client = vim.lsp.get_client_by_id(ctx.client_id)
+  if client and (client.name == "ts_ls" or client.name == "volar") then
+    result = vim.iter(result):map(function(hint)
+      local label = hint.label ---@type string
+      if label:len() >= 30 then label = label:sub(1, 29) .. ellipsis end
+      hint.label = label
+      return hint
+    end)
+  end
+      Print(result)
 
-    inlay_hint_handler(err, result, ctx, config)
+  inlay_hint_handler(err, result, ctx, config)
 end
 
 require "Chiruno"
