fde5ded9d3c94386b990e6219d5bf5c5c624320d
yopaz-tuanha <tuanha@yopaz.vn>
2024-09-11 15:43:27 +0700

    Auto Update: 2024-09-11 15:43:27 from Arch Linux

--- init.lua
+++ init.lua
@@ -27,8 +27,8 @@ vim.lsp.handlers[methods.textDocument_inlayHint] = function(err, result, ctx, co
     if client and client.name == 'typescript-tools' then
         result = vim.iter.map(function(hint)
             local label = hint.label ---@type string
-            if label:len() >= 30 then
-                label = label:sub(1, 2) .. ellipsis
+            if label:len() >= 5 then
+                label = label:sub(1, 4) .. ellipsis
             end
             hint.label = label
             return hint
