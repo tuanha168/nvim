288e96b4024baaf096b1bde30f2ea89acaa43690
yopaz-tuanha <tuanha@yopaz.vn>
2024-09-11 15:50:15 +0700

    Auto Update: 2024-09-11 15:50:15 from Arch Linux

--- init.lua
+++ init.lua
@@ -25,7 +25,6 @@ local inlay_hint_handler = vim.lsp.handlers[methods.textDocument_inlayHint]
 vim.lsp.handlers[methods.textDocument_inlayHint] = function(err, result, ctx, config)
   local client = vim.lsp.get_client_by_id(ctx.client_id)
   if client and (client.name == "ts_ls" or client.name == "volar") then
-    Print("before", result)
     result = vim.iter(pairs(result)):map(function(hint)
       local label = hint.label ---@type string
       if label:len() >= 30 then label = label:sub(1, 29) .. ellipsis end
