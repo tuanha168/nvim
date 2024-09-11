903275cf9bb94c2a46a258bd46b7e7d8a093a046
yopaz-tuanha <tuanha@yopaz.vn>
2024-09-11 17:02:20 +0700

    Auto Update: 2024-09-11 17:02:20 from Arch Linux

--- lua/polish.lua
+++ lua/polish.lua
@@ -159,7 +159,7 @@ vim.lsp.handlers[methods.textDocument_inlayHint] = function(err, result, ctx, co
 
           if type(label) == "table" then
             for _, lb in ipairs(label) do
-              if type(lb) == "string" and lb:len() >= 30 then lb = lb:sub(1, 29) .. ellipsis end
+              if type(lb) == "string" and lb:len() >= 2 then lb = lb:sub(1, 1) .. ellipsis end
             end
           end
 
