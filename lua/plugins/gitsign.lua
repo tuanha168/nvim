af8d3bc6d90dbdd9c4fd2ec78244a31b71806420
yopaz-tuanha <tuanha@yopaz.vn>
2024-11-07 13:29:04 +0700

    Auto Update: 2024-11-07 13:29:04 from Arch Linux

--- lua/plugins/gitsign.lua
+++ lua/plugins/gitsign.lua
@@ -14,18 +14,10 @@ return {
         local haveRepoLink, _ = pcall(require, "repolink")
         if not haveRepoLink then return end
 
-        vim.keymap.set({ "n", "x" }, "<Leader>gL", function()
-          local prefix = ""
-          if vim.api.nvim_get_mode().mode == "V" or vim.api.nvim_get_mode().mode == "v" then prefix = "'<,'>" end
-          vim.ui.input({ prompt = "Origin (default: origin): " }, function(origin)
-            if origin == nil or origin == "" then origin = "origin" end
-            local current_branch = string.sub(vim.fn.system "git branch --show-current", 1, -2)
-            vim.ui.input({ prompt = "Branch:", default = current_branch }, function(branch)
-              if branch == nil or branch == "" then branch = current_branch end
-              Chiruno.func.feedkeys(":" .. prefix .. "RepoLink! " .. branch .. " " .. origin .. "<CR>", "n")
-            end)
-          end)
-        end, { buffer = bufnr })
+        local keys = require("config.repolink").lazyKeys
+        for _, key in ipairs(keys) do
+          vim.keymap.set(key.mode, key[1], key[2], { buffer = bufnr, desc = key.desc })
+        end
       end
 
       return opts
