9abcbe64a9deabb47754236417bda4e4925e9e6a
yopaz-tuanha <tuanha@yopaz.vn>
2024-11-07 13:49:34 +0700

    Auto Update: 2024-11-07 13:49:34 from Arch Linux

--- lua/lsp/config/eslint.lua
+++ lua/lsp/config/eslint.lua
@@ -1,7 +1,7 @@
 local util = require "lspconfig.util"
 
 local function get_eslint_server_path(root_dir)
-  local global_bin = require("mason-registry").get_package("eslint"):get_install_path()
+  local global_bin = require("mason-registry").get_package("eslint-lsp"):get_install_path()
   Print(global_bin)
 
   local found_bin = ""
