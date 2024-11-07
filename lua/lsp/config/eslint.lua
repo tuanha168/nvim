6f9096bcb0e5bd9d0ce3c24593b5e7e194053bb7
yopaz-tuanha <tuanha@yopaz.vn>
2024-11-07 14:12:03 +0700

    Auto Update: 2024-11-07 14:12:03 from Arch Linux

--- lua/lsp/config/eslint.lua
+++ lua/lsp/config/eslint.lua
@@ -3,18 +3,19 @@ local util = require "lspconfig.util"
 local function get_eslint_server_path(root_dir)
   local global_bin = require("mason-registry").get_package("eslint-lsp"):get_install_path()
     .. "/node_modules/.bin/vscode-eslint-language-server"
+  return { "vscode-eslint-language-server", "--stdio" }
 
-  local found_bin = ""
+  -- local found_bin = ""
 
-  local function check_dir(path)
-    found_bin = util.path.join(path, "node_modules", ".bin", "eslint")
-    if util.path.exists(found_bin) then return path end
-  end
-  if util.search_ancestors(root_dir, check_dir) then
-    return { found_bin }
-  else
-    return { global_bin, "--stdio" }
-  end
+  -- local function check_dir(path)
+  --   found_bin = util.path.join(path, "node_modules", ".bin", "eslint")
+  --   if util.path.exists(found_bin) then return path end
+  -- end
+  -- if util.search_ancestors(root_dir, check_dir) then
+  --   return { found_bin }
+  -- else
+  --   return { global_bin, "--stdio" }
+  -- end
 end
 
 return {
