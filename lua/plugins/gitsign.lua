56d34138c5c856a2a05f2389c27fa51d95314f74
yopaz-tuanha <tuanha@yopaz.vn>
2024-11-07 13:23:12 +0700

    Auto Update: 2024-11-07 13:23:12 from Arch Linux

--- lua/plugins/gitsign.lua
+++ lua/plugins/gitsign.lua
@@ -8,6 +8,25 @@ return {
         opts[k] = v
       end
 
+      opts.on_attach = function(bufnr)
+        opts.on_attach(bufnr)
+        local haveRepoLink, _ = pcall(require, "repolink")
+        if not haveRepoLink then return end
+
+        vim.keymap.set("n", "<Leader>gL", function()
+          local prefix = ""
+          if vim.api.nvim_get_mode().mode == "V" or vim.api.nvim_get_mode().mode == "v" then prefix = "'<,'>" end
+          vim.ui.input({ prompt = "Origin (default: origin): " }, function(origin)
+            if origin == nil or origin == "" then origin = "origin" end
+            local current_branch = string.sub(vim.fn.system "git branch --show-current", 1, -2)
+            vim.ui.input({ prompt = "Branch:", default = current_branch }, function(branch)
+              if branch == nil or branch == "" then branch = current_branch end
+              Chiruno.func.feedkeys(":" .. prefix .. "RepoLink! " .. branch .. " " .. origin .. "<CR>", "n")
+            end)
+          end)
+        end, { buffer = bufnr })
+      end
+
       return opts
     end,
   },
