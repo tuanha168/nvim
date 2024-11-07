aaf98ff0b59ce993aae4bdf90961d2b72f007d3b
yopaz-tuanha <tuanha@yopaz.vn>
2024-11-07 13:41:44 +0700

    Auto Update: 2024-11-07 13:41:44 from Arch Linux

--- lua/lsp/config/eslint.lua
+++ lua/lsp/config/eslint.lua
@@ -1,19 +1,35 @@
 local util = require "lspconfig.util"
 
-local masonBin = require("mason-registry").get_package("eslint"):get_install_path()
-  .. "/node_modules/typescript/lib"
+local root_dir = util.root_pattern(
+  ".eslintrc.cjs",
+  ".eslintrc.js",
+  ".eslintrc.ts",
+  ".eslintrc.json",
+  ".eslintrc.yml",
+  "eslint.config.js",
+  "eslint.config.mjs",
+  "eslint.config.ts"
+)
+
+local function get_eslint_server_path()
+  local global_bin = require("mason-registry").get_package("eslint-lsp"):get_install_path()
+    .. "/node_modules/typescript/lib"
+
+  local found_bin = ""
+
+  local function check_dir(path)
+    found_bin = util.path.join(path, "node_modules", ".bin", "eslint")
+    if util.path.exists(found_bin) then return path end
+  end
+  if util.search_ancestors(root_dir, check_dir) then
+    return found_bin
+  else
+    return global_bin
+  end
+end
 
 return {
   -- filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "json" },
-  cmd = { "eslint", "--stdio" },
-  root_dir = util.root_pattern(
-    ".eslintrc.cjs",
-    ".eslintrc.js",
-    ".eslintrc.ts",
-    ".eslintrc.json",
-    ".eslintrc.yml",
-    "eslint.config.js",
-    "eslint.config.mjs",
-    "eslint.config.ts"
-  ),
+  cmd = get_eslint_server_path(),
+  root_dir = root_dir,
 }
