1643bfdc2dc1bb7f139a91a68d51236ea0f56ff7
TuanHa <achicir2@gmail.com>
2024-11-11 03:31:34 +0700

    Auto Update: 2024-11-11 03:31:34 from Arch Linux

--- lua/Chiruno/func/auto_push.lua
+++ lua/Chiruno/func/auto_push.lua
@@ -5,8 +5,7 @@ function Chiruno.func.auto_push(path)
     if not Chiruno.func.isdir_exist(logsDir) then vim.cmd("silent !mkdir " .. logsDir) end
     Print("Auto Pushing", path)
     local folder_name = vim.fn.fnamemodify(path, ":t")
-    local cmd = "silent !disown git-auto-push " .. path .. " >> " .. logsDir .. "/" .. folder_name .. ".log &"
-    Print(cmd)
+    local cmd = "silent !bash -c \"git-auto-push " .. path .. " >> " .. logsDir .. "/" .. folder_name .. ".log &\""
     vim.schedule(function() vim.cmd(cmd) end)
   end
 end
