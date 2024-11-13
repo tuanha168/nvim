43b50f673220708653034ba198202228cf0a5339
yopaz-tuanha <tuanha@yopaz.vn>
2024-11-13 09:49:24 +0700

    Auto Update: 2024-11-13 09:49:24 from Arch Linux

--- lua/Chiruno/autocmd/null_window.lua
+++ lua/Chiruno/autocmd/null_window.lua
@@ -22,7 +22,7 @@ function Chiruno.autocmd.null_window()
     group = null_window_augroup,
   })
 
-  autocmd({ "WinEnter", "WinClosed" }, {
+  autocmd({ "WinNew", "WinClosed" }, {
     pattern = "*",
     callback = Chiruno.func.on_null_win_enter,
     group = null_window_augroup,
--- lua/Chiruno/func/null_window.lua
+++ lua/Chiruno/func/null_window.lua
@@ -154,23 +154,23 @@ end
 function Chiruno.func.on_null_win_enter()
   local count = 0
 
-  vim.defer_fn(function()
-    for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
-      if win ~= splitLeft.winid and not is_float(win) then count = count + 1 end
-    end
-    if count > 1 then return Chiruno.func.close_null_window() end
+  --vim.defer_fn(function()
+  --  for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+  --    if win ~= splitLeft.winid and not is_float(win) then count = count + 1 end
+  --  end
+  --  if count > 1 then return Chiruno.func.close_null_window() end
 
-    local win = vim.api.nvim_get_current_win()
-    if is_float(win) then return end
+  --  local win = vim.api.nvim_get_current_win()
+  --  if is_float(win) then return end
 
-    if (splitLeft and win == splitLeft.winid) or (splitRight and win == splitRight.winid) then
-      Chiruno.func.toggle_null_window()
-      Chiruno.func.toggle_null_window()
-      return
-    end
+  --  if (splitLeft and win == splitLeft.winid) or (splitRight and win == splitRight.winid) then
+  --    Chiruno.func.toggle_null_window()
+  --    Chiruno.func.toggle_null_window()
+  --    return
+  --  end
 
-    if not splitLeft or not splitRight then Chiruno.func.check_null_window() end
-  end, 10)
+  --  if not splitLeft or not splitRight then Chiruno.func.check_null_window() end
+  --end, 10)
 end
 
 return Chiruno.func.check_null_window
