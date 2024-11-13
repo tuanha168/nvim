9688aeb867fc722072a8785def526b97a542bea9
yopaz-tuanha <tuanha@yopaz.vn>
2024-11-13 09:26:12 +0700

    Auto Update: 2024-11-13 09:26:12 from Arch Linux

--- lua/Chiruno/func/null_window.lua
+++ lua/Chiruno/func/null_window.lua
@@ -145,6 +145,11 @@ function Chiruno.func.check_null_window(e)
 end
 
 function Chiruno.func.on_null_win_enter()
+  local win_amount = #vim.api.nvim_tabpage_list_wins(0)
+  if win_amount > 2 then
+    return Chiruno.func.close_null_window()
+  end
+
   local win = vim.api.nvim_get_current_win()
   if win == splitLeft.winid then
     Chiruno.func.toggle_null_window()
