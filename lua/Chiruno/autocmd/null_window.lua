423b40cb4eb20c9aceb97126d5f2b0d5d3547eb6
yopaz-tuanha <tuanha@yopaz.vn>
2024-11-13 09:34:36 +0700

    Auto Update: 2024-11-13 09:34:35 from Arch Linux

--- lua/Chiruno/autocmd/null_window.lua
+++ lua/Chiruno/autocmd/null_window.lua
@@ -22,7 +22,7 @@ function Chiruno.autocmd.null_window()
     group = null_window_augroup,
   })
 
-  autocmd({ "WinEnter", "WinClosed" }, {
+  autocmd({ "WinEnter", "BufEnter" }, {
     pattern = "*",
     callback = Chiruno.func.on_null_win_enter,
     group = null_window_augroup,
