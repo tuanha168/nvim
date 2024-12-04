851cc2c60013382a865fccb0e3816470dd37af9c
yopaz-tuanha <tuanha@yopaz.vn>
2024-12-04 13:30:35 +0700

    Auto Update: 2024-12-04 13:30:34 from Arch Linux

--- lua/plugins/lsp_plugin.lua
+++ lua/plugins/lsp_plugin.lua
@@ -23,12 +23,12 @@ return {
   --     debounce = 200,
   --   },
   -- },
-  {
-    "rachartier/tiny-inline-diagnostic.nvim",
-    event = "VeryLazy", -- Or `LspAttach`
-    priority = 1000, -- needs to be loaded in first
-    opts = {
-      preset = "powerline",
-    },
-  },
+  -- {
+  --   "rachartier/tiny-inline-diagnostic.nvim",
+  --   event = "VeryLazy", -- Or `LspAttach`
+  --   priority = 1000, -- needs to be loaded in first
+  --   opts = {
+  --     preset = "powerline",
+  --   },
+  -- },
 }
