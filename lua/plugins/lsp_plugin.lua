055e5bd901a8128dc7b6026a3e134ac4eb461185
yopaz-tuanha <tuanha@yopaz.vn>
2024-12-04 13:12:22 +0700

    Auto Update: 2024-12-04 13:12:22 from Arch Linux

--- lua/plugins/lsp_plugin.lua
+++ lua/plugins/lsp_plugin.lua
@@ -27,6 +27,10 @@ return {
     "rachartier/tiny-inline-diagnostic.nvim",
     event = "VeryLazy", -- Or `LspAttach`
     priority = 1000, -- needs to be loaded in first
-    config = function() require("tiny-inline-diagnostic").setup() end,
+    opts = {
+      options = {
+        use_icons_from_diagnostic = true,
+      },
+    },
   },
 }
