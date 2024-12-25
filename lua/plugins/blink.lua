1e209e344bf9a04b39cb1434b10fab71c9d2f36a
yopaz-tuanha <tuanha@yopaz.vn>
2024-12-25 13:02:54 +0700

    Auto Update: 2024-12-25 13:02:54 from Arch Linux

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,7 +1,10 @@
 return {
   {
     "saghen/blink.cmp",
+    dependencies = "rafamadriz/friendly-snippets",
 
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
     opts = {
       sources = {
         default = { "lsp", "path", "snippets", "buffer" },
