681ae4f1cbcd967282fb3d5a85f4718b44846752
yopaz-tuanha <tuanha@yopaz.vn>
2025-03-14 17:28:44 +0700

    Auto Update: 2025-03-14 17:28:44 from Arch Linux

--- lua/lsp/mappings.lua
+++ lua/lsp/mappings.lua
@@ -25,11 +25,14 @@ return {
       -- function() Chiruno.func.operatorfunc_lua "format_motion" end,
       function()
         local ok, conform = pcall(require, "conform")
+        local lint_ok, lint = pcall(require, "lint")
         if ok then
           conform.format({}, function(err, did_edit)
             if err and not did_edit then
               vim.lsp.buf.format(require("astrolsp").format_opts --[[@as vim.lsp.buf.format.Opts?]])
             end
+
+            if lint_ok then lint.try_lint() end
           end)
           return
         end
