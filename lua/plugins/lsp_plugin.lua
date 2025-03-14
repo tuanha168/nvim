555699eff2b74ccd8ac25cab57833323add202bf
yopaz-tuanha <tuanha@yopaz.vn>
2025-03-14 17:55:43 +0700

    Auto Update: 2025-03-14 17:55:43 from Arch Linux

--- lua/plugins/lsp_plugin.lua
+++ lua/plugins/lsp_plugin.lua
@@ -40,8 +40,12 @@ return {
         vue = { "eslint" },
       }
 
-      vim.api.nvim_create_autocmd({ "BufReadPost", "InsertLeave", "TextChanged" }, {
-        callback = function() require("lint").try_lint() end,
+      vim.api.nvim_create_autocmd({ "LspAttach", "InsertLeave", "TextChanged" }, {
+        callback = function()
+          local get_clients = vim.lsp.get_clients or vim.lsp.get_active_clients
+          local client = get_clients({ bufnr = 0 })[1] or {}
+          require("lint").try_lint(nil, { cwd = client.root_dir })
+        end,
       })
     end,
   },
