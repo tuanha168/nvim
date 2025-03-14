1c46f0b379368750391c1fa24f8722dc4fb1b452
yopaz-tuanha <tuanha@yopaz.vn>
2025-03-14 18:49:45 +0700

    Auto Update: 2025-03-14 18:49:45 from Arch Linux

--- lua/config/dap/listeners.lua
+++ lua/config/dap/listeners.lua
@@ -4,8 +4,14 @@ return {
     if not ok then return end
 
     dap.listeners.before.attach["dap-view-config"] = function() dv.open() end
-    dap.listeners.before.launch["dap-view-config"] = function() dv.open() end
-    dap.listeners.before.event_terminated["dap-view-config"] = function() dv.close() end
+    dap.listeners.before.launch["dap-view-config"] = function()
+      vim.keymap.set("n", "<leader>k", function() require("dap.ui.widgets").hover() end, { buffer = 0 })
+      dv.open()
+    end
+    dap.listeners.before.event_terminated["dap-view-config"] = function()
+      vim.keymap.set("n", "<leader>k", function() vim.lsp.buf.hover() end, { buffer = 0 })
+      dv.close()
+    end
     dap.listeners.before.event_exited["dap-view-config"] = function() dv.close() end
   end,
 }
