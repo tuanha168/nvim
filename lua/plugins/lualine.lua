1d006a08993ddfa0144bf6caf02b968d0caa06fa
yopaz-tuanha <tuanha@yopaz.vn>
2025-03-24 18:31:06 +0700

    Auto Update: 2025-03-24 18:31:06 from Arch Linux

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,19 +1,24 @@
 return {
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   event = "VeryLazy",
+  --   dependencies = { "nvim-tree/nvim-web-devicons" },
+  --   opts = {
+  --     tabline = {
+  --       lualine_a = {
+  --         {
+  --           "buffers",
+  --         },
+  --       },
+  --       lualine_x = {
+  --         "filetype",
+  --       },
+  --     },
+  --   },
+  -- },
   {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-      tabline = {
-        lualine_a = {
-          {
-            "buffers",
-          },
-        },
-        lualine_x = {
-          "filetype",
-        },
-      },
-    },
-  },
+  	"rebelot/heirline.nvim",
+  	event = "VeryLazy",
+  	opts = {}
+  }
 }
