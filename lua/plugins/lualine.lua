c86613e670d5b4ec7cce9490b9f8262c685d5bcb
yopaz-tuanha <tuanha@yopaz.vn>
2025-03-24 18:12:08 +0700

    Auto Update: 2025-03-24 18:12:08 from Arch Linux

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      tabline = {
+        lualine_a = {
+          {
+            "buffers",
+          },
+        },
+      },
+    },
+  },
+}
