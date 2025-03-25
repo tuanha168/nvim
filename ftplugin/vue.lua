ff367ef7fd321db836aa0ec417260aa4d68f3304
yopaz-tuanha <tuanha@yopaz.vn>
2025-03-25 15:08:42 +0700

    Auto Update: 2025-03-25 15:08:41 from Arch Linux

--- ftplugin/vue.lua
+++ ftplugin/vue.lua
@@ -1,6 +1,6 @@
-vim.keymap.set("i", "=", function()
-  -- The cursor location does not give us the correct node in this case, so we
-  -- need to get the node to the left of the cursor
+-- vim.keymap.set("i", "=", function()
+--   -- The cursor location does not give us the correct node in this case, so we
+--   -- need to get the node to the left of the cursor
 --   local cursor = vim.api.nvim_win_get_cursor(0)
 --   local left_of_cursor_range = { cursor[1] - 1, cursor[2] - 1 }
 
