daea93e840e20884d8eb852a3ce5178a856bede6
yopaz-tuanha <tuanha@yopaz.vn>
2025-03-25 15:07:25 +0700

    Auto Update: 2025-03-25 15:07:25 from Arch Linux

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -43,9 +43,9 @@ return {
               local entry = cmp.get_selected_entry()
               if not entry then
                 cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
-                cmp.confirm()
+                -- cmp.confirm({ select = true })
               else
-                cmp.confirm()
+                cmp.confirm({ select = true })
               end
             else
               fallback()
