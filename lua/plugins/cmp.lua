59515a7e5151c61d4f44aa8071c796e2ddf09cc4
yopaz-tuanha <tuanha@yopaz.vn>
2025-03-25 16:02:46 +0700

    Auto Update: 2025-03-25 16:02:46 from Arch Linux

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -130,108 +130,48 @@ return {
   {
     "saghen/blink.cmp",
     event = "InsertEnter",
-    version = "v0.*", -- REQUIRED `tag` needed to download pre-built binary
+    -- use a release tag to download pre-built binaries
+    version = "*",
 
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- 'super-tab' for mappings similar to vscode (tab to accept)
+      -- 'enter' for enter to accept
+      -- 'none' for no mappings
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      keymap = { preset = 'default' },
+
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono'
+      },
+
+      -- (Default) Only show the documentation popup when manually triggered
+      completion = { documentation = { auto_show = false } },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
-        -- completion = {
-        --   enabled_providers = { "lsp", "path", "snippets", "buffer" },
-        -- },
-        providers = {
-          snippets = {
-            min_keyword_length = 1, -- don't show when triggered manually, useful for JSON keys
-            score_offset = -1,
-          },
-          path = {
-            opts = { get_cwd = vim.fn.getcwd },
-          },
-          buffer = {
-            max_items = 4,
-            min_keyword_length = 4,
-            score_offset = -3,
-          },
-        },
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
       },
-      keymap = {
-        ["<D-c>"] = { "show" },
-        ["<S-CR>"] = { "hide" },
-        ["<CR>"] = { "select_and_accept", "fallback" },
-        ["<Tab>"] = { "select_next", "fallback" },
-        ["<S-Tab>"] = { "select_prev", "fallback" },
-        ["<Down>"] = { "select_next", "fallback" },
-        ["<Up>"] = { "select_prev", "fallback" },
-        ["<PageDown>"] = { "scroll_documentation_down" },
-        ["<PageUp>"] = { "scroll_documentation_up" },
-      },
-      -- windows = {
-      --   documentation = {
-      --     border = vim.g.borderStyle,
-      --     min_width = 15,
-      --     max_width = 45, -- smaller, due to https://github.com/Saghen/blink.cmp/issues/194
-      --     max_height = 10,
-      --     auto_show = true,
-      --     auto_show_delay_ms = 250,
-      --   },
-      --   autocomplete = {
-      --     border = vim.g.borderStyle,
-      --     min_width = 10,               -- max_width controlled by draw-function
-      --     max_height = 10,
-      --     cycle = { from_top = false }, -- cycle at bottom, but not at the top
-      --     draw = function(ctx)
-      --       -- https://github.com/Saghen/blink.cmp/blob/9846c2d2bfdeaa3088c9c0143030524402fffdf9/lua/blink/cmp/types.lua#L1-L6
-      --       -- https://github.com/Saghen/blink.cmp/blob/9846c2d2bfdeaa3088c9c0143030524402fffdf9/lua/blink/cmp/windows/autocomplete.lua#L298-L349
-      --       -- differentiate LSP snippets from user snippets and emmet snippets
-      --       local source, client = ctx.item.source_id, ctx.item.client_id
-      --       if
-      --           client and vim.lsp.get_client_by_id(client).name == "emmet_language_server"
-      --       then
-      --         source = "emmet"
-      --       end
 
-      --       local sourceIcons = { snippets = "󰩫", buffer = "󰦨", emmet = "" }
-      --       local icon = sourceIcons[source] or ctx.kind_icon
-
-      --       return {
-      --         {
-      --           " " .. ctx.item.label .. " ",
-      --           fill = true,
-      --           hl_group = ctx.deprecated and "BlinkCmpLabelDeprecated" or "BlinkCmpLabel",
-      --           max_width = 40,
-      --         },
-      --         { icon .. " ", hl_group = "BlinkCmpKind" .. ctx.kind },
-      --       }
-      --     end,
-      --   },
-      -- },
-      -- kind_icons = {
-      --   Text = "",
-      --   Method = "󰊕",
-      --   Function = "󰊕",
-      --   Constructor = "",
-      --   Field = "󰇽",
-      --   Variable = "󰂡",
-      --   Class = "󰜁",
-      --   Interface = "",
-      --   Module = "",
-      --   Property = "󰜢",
-      --   Unit = "",
-      --   Value = "󰎠",
-      --   Enum = "",
-      --   Keyword = "󰌋",
-      --   Snippet = "󰒕",
-      --   Color = "󰏘",
-      --   Reference = "",
-      --   File = "",
-      --   Folder = "󰉋",
-      --   EnumMember = "",
-      --   Constant = "󰏿",
-      --   Struct = "",
-      --   Event = "",
-      --   Operator = "󰆕",
-      --   TypeParameter = "󰅲",
-      -- },
+      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      --
+      -- See the fuzzy documentation for more information
+      fuzzy = { implementation = "prefer_rust_with_warning" }
     },
+    opts_extend = { "sources.default" }
   },
 }
