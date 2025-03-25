278be353a013994bae4f5c58b8c69b596f8e13af
yopaz-tuanha <tuanha@yopaz.vn>
2025-03-25 15:34:08 +0700

    Auto Update: 2025-03-25 15:34:08 from Arch Linux

--- lua/lsp/on_attach.lua
+++ lua/lsp/on_attach.lua
@@ -1,11 +1,4 @@
 return function(client, bufnr)
-	vim.diagnostic.config({
-		virtual_text = true,
-		signs = true,
-		underline = true,
-		update_in_insert = false,
-		show_diagnostic_autocmds = { 'InsertLeave', 'TextChanged' },
-	})
 	-- if client.name == "rust_analyzer" then
 	--   local rt = require "rust-tools"
 	--   vim.keymap.set("n", "<leader>k", rt.hover_actions.hover_actions, { buffer = bufnr })
--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -46,6 +46,14 @@ return {
           end,
         },
       }
+
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        show_diagnostic_autocmds = { 'InsertLeave', 'TextChanged' },
+      })
     end,
   },
 }
