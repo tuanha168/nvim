4d7828f6a52d9f00877bc647f61b086fcd228bb7
yopaz-tuanha <tuanha@yopaz.vn>
2025-03-25 15:47:16 +0700

    Auto Update: 2025-03-25 15:47:16 from Arch Linux

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -133,11 +133,6 @@ return {
     version = "v0.*", -- REQUIRED `tag` needed to download pre-built binary
 
     opts = {
-      highlight = {
-        -- supporting themes: tokyonight
-        -- not supported: nightfox
-        use_nvim_cmp_as_default = true,
-      },
       sources = {
         completion = {
           enabled_providers = { "lsp", "path", "snippets", "buffer" },
@@ -183,30 +178,30 @@ return {
           min_width = 10,               -- max_width controlled by draw-function
           max_height = 10,
           cycle = { from_top = false }, -- cycle at bottom, but not at the top
-          -- draw = function(ctx)
-          --   -- https://github.com/Saghen/blink.cmp/blob/9846c2d2bfdeaa3088c9c0143030524402fffdf9/lua/blink/cmp/types.lua#L1-L6
-          --   -- https://github.com/Saghen/blink.cmp/blob/9846c2d2bfdeaa3088c9c0143030524402fffdf9/lua/blink/cmp/windows/autocomplete.lua#L298-L349
-          --   -- differentiate LSP snippets from user snippets and emmet snippets
-          --   local source, client = ctx.item.source_id, ctx.item.client_id
-          --   if
-          --       client and vim.lsp.get_client_by_id(client).name == "emmet_language_server"
-          --   then
-          --     source = "emmet"
-          --   end
+          draw = function(ctx)
+            -- https://github.com/Saghen/blink.cmp/blob/9846c2d2bfdeaa3088c9c0143030524402fffdf9/lua/blink/cmp/types.lua#L1-L6
+            -- https://github.com/Saghen/blink.cmp/blob/9846c2d2bfdeaa3088c9c0143030524402fffdf9/lua/blink/cmp/windows/autocomplete.lua#L298-L349
+            -- differentiate LSP snippets from user snippets and emmet snippets
+            local source, client = ctx.item.source_id, ctx.item.client_id
+            if
+                client and vim.lsp.get_client_by_id(client).name == "emmet_language_server"
+            then
+              source = "emmet"
+            end
 
-          --   local sourceIcons = { snippets = "󰩫", buffer = "󰦨", emmet = "" }
-          --   local icon = sourceIcons[source] or ctx.kind_icon
+            local sourceIcons = { snippets = "󰩫", buffer = "󰦨", emmet = "" }
+            local icon = sourceIcons[source] or ctx.kind_icon
 
-          --   return {
-          --     {
-          --       " " .. ctx.item.label .. " ",
-          --       fill = true,
-          --       hl_group = ctx.deprecated and "BlinkCmpLabelDeprecated" or "BlinkCmpLabel",
-          --       max_width = 40,
-          --     },
-          --     { icon .. " ", hl_group = "BlinkCmpKind" .. ctx.kind },
-          --   }
-          -- end,
+            return {
+              {
+                " " .. ctx.item.label .. " ",
+                fill = true,
+                hl_group = ctx.deprecated and "BlinkCmpLabelDeprecated" or "BlinkCmpLabel",
+                max_width = 40,
+              },
+              { icon .. " ", hl_group = "BlinkCmpKind" .. ctx.kind },
+            }
+          end,
         },
       },
       kind_icons = {
