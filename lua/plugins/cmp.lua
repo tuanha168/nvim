5fcadab0c7000f54f5c97f2fe0a7c2d29eb8fab2
yopaz-tuanha <tuanha@yopaz.vn>
2025-03-25 15:48:56 +0700

    Auto Update: 2025-03-25 15:48:56 from Arch Linux

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -135,6 +135,11 @@ return {
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
+      highlight = {
+        -- supporting themes: tokyonight
+        -- not supported: nightfox
+        use_nvim_cmp_as_default = true,
+      },
       sources = {
         completion = {
           enabled_providers = { "lsp", "path", "snippets", "buffer" },
@@ -166,73 +171,73 @@ return {
         ["<PageDown>"] = { "scroll_documentation_down" },
         ["<PageUp>"] = { "scroll_documentation_up" },
       },
-      windows = {
-        documentation = {
-          border = vim.g.borderStyle,
-          min_width = 15,
-          max_width = 45, -- smaller, due to https://github.com/Saghen/blink.cmp/issues/194
-          max_height = 10,
-          auto_show = true,
-          auto_show_delay_ms = 250,
-        },
-        autocomplete = {
-          border = vim.g.borderStyle,
-          min_width = 10,               -- max_width controlled by draw-function
-          max_height = 10,
-          cycle = { from_top = false }, -- cycle at bottom, but not at the top
-          draw = function(ctx)
-            -- https://github.com/Saghen/blink.cmp/blob/9846c2d2bfdeaa3088c9c0143030524402fffdf9/lua/blink/cmp/types.lua#L1-L6
-            -- https://github.com/Saghen/blink.cmp/blob/9846c2d2bfdeaa3088c9c0143030524402fffdf9/lua/blink/cmp/windows/autocomplete.lua#L298-L349
-            -- differentiate LSP snippets from user snippets and emmet snippets
-            local source, client = ctx.item.source_id, ctx.item.client_id
-            if
-                client and vim.lsp.get_client_by_id(client).name == "emmet_language_server"
-            then
-              source = "emmet"
-            end
+      -- windows = {
+      --   documentation = {
+      --     border = vim.g.borderStyle,
+      --     min_width = 15,
+      --     max_width = 45, -- smaller, due to https://github.com/Saghen/blink.cmp/issues/194
+      --     max_height = 10,
+      --     auto_show = true,
+      --     auto_show_delay_ms = 250,
+      --   },
+      --   autocomplete = {
+      --     border = vim.g.borderStyle,
+      --     min_width = 10,               -- max_width controlled by draw-function
+      --     max_height = 10,
+      --     cycle = { from_top = false }, -- cycle at bottom, but not at the top
+      --     draw = function(ctx)
+      --       -- https://github.com/Saghen/blink.cmp/blob/9846c2d2bfdeaa3088c9c0143030524402fffdf9/lua/blink/cmp/types.lua#L1-L6
+      --       -- https://github.com/Saghen/blink.cmp/blob/9846c2d2bfdeaa3088c9c0143030524402fffdf9/lua/blink/cmp/windows/autocomplete.lua#L298-L349
+      --       -- differentiate LSP snippets from user snippets and emmet snippets
+      --       local source, client = ctx.item.source_id, ctx.item.client_id
+      --       if
+      --           client and vim.lsp.get_client_by_id(client).name == "emmet_language_server"
+      --       then
+      --         source = "emmet"
+      --       end
 
-            local sourceIcons = { snippets = "󰩫", buffer = "󰦨", emmet = "" }
-            local icon = sourceIcons[source] or ctx.kind_icon
+      --       local sourceIcons = { snippets = "󰩫", buffer = "󰦨", emmet = "" }
+      --       local icon = sourceIcons[source] or ctx.kind_icon
 
-            return {
-              {
-                " " .. ctx.item.label .. " ",
-                fill = true,
-                hl_group = ctx.deprecated and "BlinkCmpLabelDeprecated" or "BlinkCmpLabel",
-                max_width = 40,
-              },
-              { icon .. " ", hl_group = "BlinkCmpKind" .. ctx.kind },
-            }
-          end,
-        },
-      },
-      kind_icons = {
-        Text = "",
-        Method = "󰊕",
-        Function = "󰊕",
-        Constructor = "",
-        Field = "󰇽",
-        Variable = "󰂡",
-        Class = "󰜁",
-        Interface = "",
-        Module = "",
-        Property = "󰜢",
-        Unit = "",
-        Value = "󰎠",
-        Enum = "",
-        Keyword = "󰌋",
-        Snippet = "󰒕",
-        Color = "󰏘",
-        Reference = "",
-        File = "",
-        Folder = "󰉋",
-        EnumMember = "",
-        Constant = "󰏿",
-        Struct = "",
-        Event = "",
-        Operator = "󰆕",
-        TypeParameter = "󰅲",
-      },
+      --       return {
+      --         {
+      --           " " .. ctx.item.label .. " ",
+      --           fill = true,
+      --           hl_group = ctx.deprecated and "BlinkCmpLabelDeprecated" or "BlinkCmpLabel",
+      --           max_width = 40,
+      --         },
+      --         { icon .. " ", hl_group = "BlinkCmpKind" .. ctx.kind },
+      --       }
+      --     end,
+      --   },
+      -- },
+      -- kind_icons = {
+      --   Text = "",
+      --   Method = "󰊕",
+      --   Function = "󰊕",
+      --   Constructor = "",
+      --   Field = "󰇽",
+      --   Variable = "󰂡",
+      --   Class = "󰜁",
+      --   Interface = "",
+      --   Module = "",
+      --   Property = "󰜢",
+      --   Unit = "",
+      --   Value = "󰎠",
+      --   Enum = "",
+      --   Keyword = "󰌋",
+      --   Snippet = "󰒕",
+      --   Color = "󰏘",
+      --   Reference = "",
+      --   File = "",
+      --   Folder = "󰉋",
+      --   EnumMember = "",
+      --   Constant = "󰏿",
+      --   Struct = "",
+      --   Event = "",
+      --   Operator = "󰆕",
+      --   TypeParameter = "󰅲",
+      -- },
     },
   },
 }
