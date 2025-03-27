a549147f3e0a03aca99fc763c2c097045d07b273
yopaz-tuanha <tuanha@yopaz.vn>
2025-03-27 14:19:26 +0700

    Auto Update: 2025-03-27 14:19:26 from Arch Linux

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -57,20 +57,18 @@ return {
     end,
   },
 
-  {
-    "jay-babu/mason-null-ls.nvim",
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-    },
-    -- overrides `require("mason-null-ls").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "prettierd",
-        "stylua",
-        -- "pint",
-        "tfsec",
-      })
-    end,
-  },
+  -- {
+  --   "jay-babu/mason-null-ls.nvim",
+  --   dependencies = {
+  --     "williamboman/mason-lspconfig.nvim",
+  --   },
+  --   opts = {
+  --     ensure_installed = {
+  --       "prettierd",
+  --       "stylua",
+  --       -- "pint",
+  --       "tfsec",
+  --     }
+  --   }
+  -- },
 }
