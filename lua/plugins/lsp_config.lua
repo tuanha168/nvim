ee1052fb8eb056cce85c39bc341cb195ef86b497
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-01 09:03:27 +0700

    Auto Update: 2025-04-01 09:03:27 from Arch Linux

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -8,11 +8,11 @@ return {
         opts = {},
       },
       "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/cmp-nvim-lsp",
+      -- "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+      -- capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
       local servers = {
         lua_ls = {
