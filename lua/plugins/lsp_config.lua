495eab2785f1e3883a290fd556361843cddd48c3
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-01 09:27:23 +0700

    Auto Update: 2025-04-01 09:27:23 from Arch Linux

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -69,6 +69,8 @@ return {
         update_in_insert = false,
         show_diagnostic_autocmds = { 'InsertLeave', 'TextChanged' },
       })
+
+      vim.lsp.inlay_hint.enable(true)
     end,
   },
 
