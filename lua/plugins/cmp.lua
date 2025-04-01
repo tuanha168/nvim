084a00ee8f48619e1e7ce1b2291722d5767a9573
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-01 09:09:46 +0700

    Auto Update: 2025-04-01 09:09:45 from Arch Linux

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -122,7 +122,7 @@ return {
       keymap = {
         preset = 'none',
         ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-        ['<C-e>'] = { 'hide' },
+        ['<C-e>'] = { 'hide', 'fallback' },
         ['<C-y>'] = { 'select_and_accept' },
 
         ['<Up>'] = { 'select_prev', 'fallback' },
@@ -130,11 +130,19 @@ return {
         ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
         ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
 
-        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
-
-        ['<Tab>'] = { 'select_next', 'fallback_to_mappings' },
-        ['<S-Tab>'] = { 'select_prev', 'fallback_to_mappings' },
+        ['<Tab>'] = {
+          function(cmp)
+            if cmp.snippet_active() then
+              return cmp.accept()
+            else
+              return cmp.select_and_accept()
+            end
+          end,
+          'snippet_forward',
+          'fallback'
+        },
+        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+        ['<CR>'] = { 'accept', 'fallback' },
 
         ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
       },
