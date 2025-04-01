b3364de9146afc702717163666bedd35dbe300ec
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-01 09:11:49 +0700

    Auto Update: 2025-04-01 09:11:49 from Arch Linux

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -130,18 +130,12 @@ return {
         ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
         ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
 
-        ['<Tab>'] = {
-          function(cmp)
-            if cmp.snippet_active() then
-              return cmp.accept()
-            else
-              return cmp.select_and_accept()
-            end
-          end,
-          'snippet_forward',
-          'fallback'
-        },
-        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+        ['<Tab>'] = { 'select_next', 'fallback_to_mappings' },
+        ['<S-Tab>'] = { 'select_prev', 'fallback_to_mappings' },
+
         ['<CR>'] = { 'accept', 'fallback' },
 
         ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
