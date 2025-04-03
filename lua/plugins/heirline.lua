182d70bec21d897b9c824cd7829e89cfac8d91fc
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-03 17:58:32 +0700

    Auto Update: 2025-04-03 17:58:32 from Arch Linux

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -2,7 +2,6 @@ return {
   {
     "rebelot/heirline.nvim",
     dependencies = {
-      { "echasnovski/mini.bufremove", version = "*" },
       "zeioth/heirline-components.nvim",
       "nvim-tree/nvim-web-devicons",
       { "stevearc/aerial.nvim", opts = {} },
