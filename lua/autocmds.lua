3e7955c205238801c1bb616e2439084fcae4ad48
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-03 11:14:36 +0700

    Auto Update: 2025-04-03 11:14:36 from Arch Linux

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -101,6 +101,8 @@ return {
   {
     { "BufRead", "BufEnter" },
     function(event)
+      if not Snacks then return end
+
       if vim.api.nvim_get_option_value("filetype", { buf = event.buf }) ~= "snacks_terminal" then
         local instance = Snacks.terminal.get({ "lazygit" }, { create = false })
         if instance then instance.hide(instance) end
