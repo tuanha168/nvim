57566371bdd4b77c1610375ce33d6d3cf5dc5759
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-03 18:15:39 +0700

    Auto Update: 2025-04-03 18:15:39 from Arch Linux

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -268,6 +268,19 @@ return {
 
       vim.o.showtabline = 2
       vim.cmd [[au FileType * if index(['wipe', 'delete'], &bufhidden) >= 0 | set nobuflisted | endif]]
+
+      -- vim.keymap.set("n", "gbp", function()
+      --   local tabline = require("heirline").tabline
+      --   local buflist = tabline._buflist[1]
+      --   buflist._picker_labels = {}
+      --   buflist._show_picker = true
+      --   vim.cmd.redrawtabline()
+      --   local char = vim.fn.getcharstr()
+      --   local bufnr = buflist._picker_labels[char]
+      --   if bufnr then vim.api.nvim_win_set_buf(0, bufnr) end
+      --   buflist._show_picker = false
+      --   vim.cmd.redrawtabline()
+      -- end)
     end,
   },
 }
