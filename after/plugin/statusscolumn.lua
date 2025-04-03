f345e7abd995c3277d56f2030190460de0c73677
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-03 11:29:23 +0700

    Auto Update: 2025-04-03 11:29:23 from Arch Linux

--- after/plugin/statusscolumn.lua
+++ after/plugin/statusscolumn.lua
@@ -5,6 +5,6 @@ _G.StatusColumn = {}
 StatusColumn.set_window = function(value, defer, win)
   vim.defer_fn(function()
     win = win or vim.api.nvim_get_current_win()
-    vim.api.nvim_win_set_option(win, "statuscolumn", value)
+    vim.api.set_win_option(win, "statuscolumn", value)
   end, defer or 1)
 end
