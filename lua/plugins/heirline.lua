1329976f2790d6284b1f105d14f66c1b0296a8c1
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-03 18:04:18 +0700

    Auto Update: 2025-04-03 18:04:18 from Arch Linux

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -7,26 +7,21 @@ local function find_unique_subpath(target_path, paths)
     split_paths[path] = vim.split(path, "/", { plain = true })
   end
 
-  -- Find the first unique segment
-  for level = 1, #target_parts do
-    local candidate = table.concat(vim.list_slice(target_parts, level, #target_parts), "/")
-    local is_unique = true
-
-    for _, other_parts in pairs(split_paths) do
-      if other_parts ~= target_parts then
-        local other_candidate = table.concat(vim.list_slice(other_parts, level, #other_parts), "/")
-        if other_candidate == candidate then
-          is_unique = false
+  -- Determine the longest common prefix
+  local min_common_level = 1
+  for _, other_parts in pairs(split_paths) do
+    if other_parts ~= target_parts then
+      for i = 1, math.min(#target_parts, #other_parts) do
+        if target_parts[i] ~= other_parts[i] then
           break
         end
+        min_common_level = i + 1
       end
     end
-
-    if is_unique then return candidate end
   end
 
-  -- Fallback (shouldn't happen)
-  return target_path
+  -- Ensure uniqueness by returning only the necessary subpath
+  return table.concat(vim.list_slice(target_parts, min_common_level, #target_parts), "/")
 end
 
 local function get_unique_path(bufnr)
@@ -47,16 +42,18 @@ local function get_unique_path(bufnr)
         local b_filename = vim.fn.fnamemodify(b_filepath, ":t")
         local b_path = vim.fn.fnamemodify(b_filepath, ":.:h")
 
-        if not file_map[b_filename] then file_map[b_filename] = {} end
+        if not file_map[b_filename] then
+          file_map[b_filename] = {}
+        end
         table.insert(file_map[b_filename], b_path)
       end
     end
   end
 
-  if not file_map[filename] then return "" end
-
   -- If the filename is unique, return it directly
-  if #file_map[filename] == 1 then return filename end
+  if #file_map[filename] == 1 then
+    return filename
+  end
 
   -- Otherwise, determine the minimal unique path
   local paths = file_map[filename]
@@ -68,15 +65,10 @@ end
 return {
   {
     "rebelot/heirline.nvim",
-    dependencies = {
-      "zeioth/heirline-components.nvim",
-      "nvim-tree/nvim-web-devicons",
-      { "stevearc/aerial.nvim", opts = {} },
-    },
     event = "VeryLazy",
-    opts = function()
-      local lib = require "heirline-components.all"
-      local utils = require "heirline.utils"
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local utils = require("heirline.utils")
 
       local leftDelimiter = { provider = "   ", hl = "red" }
       local rightDelimiter = { provider = "   ", hl = "red" }
@@ -87,7 +79,9 @@ return {
 
       -- we redefine the filename component, as we probably only want the tail and not the relative path
       local TablineFileName = {
-        provider = function(self) return " " .. get_unique_path(self.bufnr) .. " " end,
+        provider = function(self)
+          return " " .. get_unique_path(self.bufnr) .. " "
+        end,
         hl = function(self) return { bold = self.is_active or self.is_visible, italic = true } end,
       }
 
@@ -103,7 +97,7 @@ return {
         {
           condition = function(self)
             return not vim.api.nvim_get_option_value("modifiable", { buf = self.bufnr })
-              or vim.api.nvim_get_option_value("readonly", { buf = self.bufnr })
+                or vim.api.nvim_get_option_value("readonly", { buf = self.bufnr })
           end,
           provider = function(self)
             if vim.api.nvim_get_option_value("buftype", { buf = self.bufnr }) == "terminal" then
@@ -121,7 +115,7 @@ return {
           local filename = self.filename
           local extension = vim.fn.fnamemodify(filename, ":e")
           self.icon, self.icon_color =
-            require("nvim-web-devicons").get_icon_color(filename, extension, { default = true })
+              require("nvim-web-devicons").get_icon_color(filename, extension, { default = true })
         end,
         provider = function(self) return self.icon and (self.icon .. " ") end,
         hl = function(self) return { fg = self.icon_color } end,
@@ -195,85 +189,33 @@ return {
         TablineBufferBlock,
         { provider = "", hl = { fg = "gray" } }, -- left truncation, optional (defaults to "<")
         { provider = "", hl = { fg = "gray" } } -- right trunctation, also optional (defaults to ...... yep, ">")
-        -- by the way, open a lot of buffers and try clicking them ;)
+      -- by the way, open a lot of buffers and try clicking them ;)
       )
 
-      return {
-        opts = {
-          disable_winbar_cb = function(args) -- We do this to avoid showing it on the greeter.
-            local is_disabled = not require("heirline-components.buffer").is_valid(args.buf)
-              or lib.condition.buffer_matches({
-                buftype = { "terminal", "prompt", "nofile", "help", "quickfix" },
-                filetype = { "NvimTree", "neo%-tree", "dashboard", "Outline", "aerial" },
-              }, args.buf)
-            return is_disabled
-          end,
-        },
-        -- tabline = { -- UI upper bar
-        --   lib.component.tabline_conditional_padding(),
-        --   lib.component.tabline_buffers(),
-        --   lib.component.fill { hl = { bg = "tabline_bg" } },
-        --   lib.component.tabline_tabpages(),
-        -- },
+      require("heirline").setup {
         tabline = { BufferLine },
-        winbar = { -- UI breadcrumbs bar
-          init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
-          fallthrough = false,
-          -- Winbar for terminal, neotree, and aerial.
+        color = {
           {
-            condition = function() return not lib.condition.is_active() end,
-            {
-              lib.component.neotree(),
-              lib.component.compiler_play(),
-              lib.component.fill(),
-              lib.component.compiler_build_type(),
-              lib.component.compiler_redo(),
-              lib.component.aerial(),
-            },
+            bright_bg = utils.get_highlight("Folded").bg,
+            bright_fg = utils.get_highlight("Folded").fg,
+            red = utils.get_highlight("DiagnosticError").fg,
+            dark_red = utils.get_highlight("DiffDelete").bg,
+            green = utils.get_highlight("String").fg,
+            blue = utils.get_highlight("Function").fg,
+            gray = utils.get_highlight("NonText").fg,
+            orange = utils.get_highlight("Constant").fg,
+            purple = utils.get_highlight("Statement").fg,
+            cyan = utils.get_highlight("Special").fg,
+            diag_warn = utils.get_highlight("DiagnosticWarn").fg,
+            diag_error = utils.get_highlight("DiagnosticError").fg,
+            diag_hint = utils.get_highlight("DiagnosticHint").fg,
+            diag_info = utils.get_highlight("DiagnosticInfo").fg,
+            git_del = utils.get_highlight("diffDeleted").fg,
+            git_add = utils.get_highlight("diffAdded").fg,
+            git_change = utils.get_highlight("diffChanged").fg,
           },
-          -- Regular winbar
-          {
-            -- lib.component.neotree(),
-            -- lib.component.compiler_play(),
-            lib.component.fill(),
-            lib.component.breadcrumbs(),
-            lib.component.fill(),
-            -- lib.component.compiler_redo(),
-            lib.component.aerial(),
-          },
-        },
-        statuscolumn = { -- UI left column
-          init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
-          lib.component.foldcolumn(),
-          lib.component.numbercolumn(),
-          lib.component.signcolumn(),
-        } or nil,
-        statusline = { -- UI statusbar
-          hl = { fg = "fg", bg = "bg" },
-          lib.component.mode(),
-          lib.component.git_branch(),
-          lib.component.file_info(),
-          lib.component.git_diff(),
-          lib.component.diagnostics(),
-          lib.component.fill(),
-          lib.component.cmd_info(),
-          lib.component.fill(),
-          lib.component.lsp(),
-          lib.component.compiler_state(),
-          lib.component.virtual_env(),
-          lib.component.nav(),
-          lib.component.mode { surround = { separator = "right" } },
         },
       }
-    end,
-    config = function(_, opts)
-      local heirline = require "heirline"
-      local heirline_components = require "heirline-components.all"
-
-      -- Setup
-      heirline_components.init.subscribe_to_events()
-      heirline.load_colors(heirline_components.hl.get_colors())
-      heirline.setup(opts)
 
       vim.o.showtabline = 2
       vim.cmd [[au FileType * if index(['wipe', 'delete'], &bufhidden) >= 0 | set nobuflisted | endif]]
