84ca1332fe880bc631dfdc57e7df8463f27860c7
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-04 14:46:03 +0700

    Auto Update: 2025-04-04 14:46:03 from Arch Linux

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -139,14 +139,6 @@ return {
             },
           },
         },
-        exe = {
-          "dotnet",
-          vim.fs.joinpath(roslynPath, "libexec", "Microsoft.CodeAnalysis.LanguageServer.dll"),
-        },
-        args = {
-          "--logLevel=Information",
-          "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
-        },
       }
     end,
   },
