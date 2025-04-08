13c04133a2e156da3fb2955c05e98486c4d91ef6
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-08 09:31:12 +0700

    Auto Update: 2025-04-08 09:31:12 from Arch Linux

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -207,32 +207,32 @@ return {
           lib.component.tabline_tabpages(),
         },
         -- tabline = { BufferLine },
-        winbar = { -- UI breadcrumbs bar
-          init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
-          fallthrough = false,
-          -- Winbar for terminal, neotree, and aerial.
-          {
-            condition = function() return not lib.condition.is_active() end,
-            {
-              lib.component.neotree(),
-              lib.component.compiler_play(),
-              lib.component.fill(),
-              lib.component.compiler_build_type(),
-              lib.component.compiler_redo(),
-              lib.component.aerial(),
-            },
-          },
-          -- Regular winbar
-          {
-            -- lib.component.neotree(),
-            -- lib.component.compiler_play(),
-            lib.component.fill(),
-            lib.component.breadcrumbs(),
-            lib.component.fill(),
-            -- lib.component.compiler_redo(),
-            lib.component.aerial(),
-          },
-        },
+        -- winbar = { -- UI breadcrumbs bar
+        --   init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
+        --   fallthrough = false,
+        --   -- Winbar for terminal, neotree, and aerial.
+        --   {
+        --     condition = function() return not lib.condition.is_active() end,
+        --     {
+        --       lib.component.neotree(),
+        --       lib.component.compiler_play(),
+        --       lib.component.fill(),
+        --       lib.component.compiler_build_type(),
+        --       lib.component.compiler_redo(),
+        --       lib.component.aerial(),
+        --     },
+        --   },
+        --   -- Regular winbar
+        --   {
+        --     -- lib.component.neotree(),
+        --     -- lib.component.compiler_play(),
+        --     lib.component.fill(),
+        --     lib.component.breadcrumbs(),
+        --     lib.component.fill(),
+        --     -- lib.component.compiler_redo(),
+        --     lib.component.aerial(),
+        --   },
+        -- },
         statuscolumn = { -- UI left column
           init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
           lib.component.foldcolumn(),
