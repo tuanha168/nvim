ea752bf4a8ef1af8431aab8106a6944ef04ff13e
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-17 10:04:38 +0700

    Auto Update: 2025-04-17 10:04:38 from Arch Linux

--- lua/lsp/on_attach.lua
+++ lua/lsp/on_attach.lua
@@ -1,5 +1,3 @@
-return function(client, bufnr) 
-  if client.name == "eslint" then
-    vim.b[bufnr].haveEslint = true
-  end
+return function(client, bufnr)
+  if client.name == "eslint" or client.name == "eslintd" then vim.b[bufnr].haveEslint = true end
 end
