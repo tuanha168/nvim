9dca0d57727ca0c6c3bc001c7856ad3d8aeebbea
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-17 09:59:24 +0700

    Auto Update: 2025-04-17 09:59:24 from Arch Linux

---
+++ lua/Chiruno/func/format_lsp.lua
@@ -0,0 +1,10 @@
+function Chiruno.func.format_lsp()
+  local ignoreLsp = { "tsserver", "volar" }
+
+  vim.lsp.buf.format {
+    filter = function(client) return not vim.tbl_contains(ignoreLsp, client.name) end,
+    async = true,
+  }
+end
+
+return Chiruno.func.format_lsp()
