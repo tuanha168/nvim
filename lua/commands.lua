fa059fe8a0d5097af19397fae165d6c36e73343d
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-21 17:46:44 +0700

    Auto Update: 2025-04-21 17:46:44 from Arch Linux

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,3 +1,11 @@
 return {
-
+  {
+    "Curl",
+    function()
+      local servername = vim.v.servername
+      local cmd = "curlcp " .. vim.fn.shellescape(servername)
+      local output = vim.fn.system(cmd)
+      Print(output)
+    end,
+  },
 }
