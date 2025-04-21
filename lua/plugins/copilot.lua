8084056059c616e3a446494c8648d2162cc9122b
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-21 23:58:55 +0700

    Auto Update: 2025-04-21 23:58:54 from Arch Linux

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -23,8 +23,5 @@ return {
     vim.g.copilot_nes_debounce = 500
     vim.lsp.enable "copilot"
     vim.keymap.set("n", "<C-f>", function() require("copilot-lsp.nes").apply_pending_nes() end)
-
-    vim.keymap.set("n", "<C-j>", function() require("copilot-lsp.suggestions").next() end)
-    vim.keymap.set("n", "<C-k>", function() require("copilot-lsp.suggestions").prev() end)
   end,
 }
