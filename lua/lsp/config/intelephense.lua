c502ebc4ad4b3a445def423bca574859cac52782
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-21 21:58:40 +0700

    Auto Update: 2025-04-21 21:58:39 from Arch Linux

--- lua/lsp/config/intelephense.lua
+++ lua/lsp/config/intelephense.lua
@@ -1,5 +1,20 @@
 local util = require "lspconfig.util"
 
+---@type lspconfig.options.intelephense
 return {
   root_dir = util.root_pattern("docker-compose.yml", "composer.json", ".git"),
+  settings = {
+    intelephense = {
+      format = { braces = "k&r" },
+      diagnostics = {
+        undefinedFunctions = false,
+        undefinedConstants = false,
+        undefinedClassConstants = false,
+        undefinedMethods = false,
+        undefinedProperties = false,
+        undefinedVariables = false,
+        undefinedTypes = false,
+      },
+    },
+  },
 }
