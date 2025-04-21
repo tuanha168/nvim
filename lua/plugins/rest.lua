c4371cd0da254967c1bc8ffa1772704a5a9c8d18
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-21 15:19:43 +0700

    Auto Update: 2025-04-21 15:19:43 from Arch Linux

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -18,6 +18,19 @@ return {
           },
         },
       }
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "rest_nvim_result" },
+        callback = function(e)
+          vim.api.nvim_buf_set_keymap(
+            e.buf,
+            "n",
+            "q",
+            "<cmd>close<CR>",
+            { noremap = true, silent = true, desc = "Close" }
+          )
+        end,
+      })
     end,
   },
 }
