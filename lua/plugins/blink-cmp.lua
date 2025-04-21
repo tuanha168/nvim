2d04c729ca7c079b9b6e560c5653d958b947f0eb
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-22 00:05:53 +0700

    Auto Update: 2025-04-22 00:05:53 from Arch Linux

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -129,22 +129,7 @@ return {
         ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
         ["<C-n>"] = { "select_next", "fallback_to_mappings" },
 
-        -- ["<Tab>"] = { "select_next", "fallback_to_mappings" },
-        ["<Tab>"] = {
-            function(cmp)
-                if vim.b[vim.api.nvim_get_current_buf()].nes_state then
-                    cmp.hide()
-                    return require("copilot-lsp.nes").apply_pending_nes()
-                end
-                if cmp.snippet_active() then
-                    return cmp.accept()
-                else
-                    return cmp.select_and_accept()
-                end
-            end,
-            "snippet_forward",
-            "fallback",
-        },
+        ["<Tab>"] = { "select_next", "fallback_to_mappings" },
         ["<S-Tab>"] = { "select_prev", "fallback_to_mappings" },
 
         ["<CR>"] = { "accept", "fallback" },
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,27 +1,30 @@
 ---@type LazySpec
 return {
-  -- {
-  --   "zbirenbaum/copilot.lua",
-  --   event = "InsertEnter",
-  --   opts = {
-  --     panel = {
-  --       enabled = false,
-  --     },
-  --     suggestion = {
-  --       enabled = true,
-  --       auto_trigger = true,
-  --       keymap = {
-  --         accept = "<C-f>",
-  --         next = "<C-j>",
-  --         prev = "<C-k>",
-  --       },
-  --     },
-  --   },
-  -- },
-  "copilotlsp-nvim/copilot-lsp",
-  init = function()
-    vim.g.copilot_nes_debounce = 500
-    vim.lsp.enable "copilot"
-    vim.keymap.set("n", "<C-f>", function() require("copilot-lsp.nes").apply_pending_nes() end)
-  end,
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    opts = {
+      panel = {
+        enabled = false,
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<C-f>",
+          next = "<C-j>",
+          prev = "<C-k>",
+        },
+      },
+    },
+  },
+
+  {
+    "copilotlsp-nvim/copilot-lsp",
+    init = function()
+      vim.g.copilot_nes_debounce = 500
+      vim.lsp.enable "copilot"
+      vim.keymap.set("n", "<C-f>", function() require("copilot-lsp.nes").apply_pending_nes() end)
+    end,
+  },
 }
