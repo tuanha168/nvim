02077ce56147af33bed8b59e47847a5fb1c49eb2
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-21 23:51:20 +0700

    Auto Update: 2025-04-21 23:51:20 from Arch Linux

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -46,7 +46,7 @@ return {
         html = require "lsp.config.html",
         ts_ls = require "lsp.config.ts_ls",
         stylelint_lsp = require "lsp.config.stylelint_lsp",
-        copilot = {}
+        ["copilot-language-server"] = {},
       }
 
       require("mason-lspconfig").setup {
