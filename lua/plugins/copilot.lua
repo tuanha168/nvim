1284d86e4ea62cd56295298cf0a8f2642c850a28
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-22 00:16:40 +0700

    Auto Update: 2025-04-22 00:16:40 from Arch Linux

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,30 +1,30 @@
 ---@type LazySpec
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    event = "InsertEnter",
-    opts = {
-      panel = {
-        enabled = false,
-      },
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        keymap = {
-          accept = "<C-f>",
-          next = "<C-j>",
-          prev = "<C-k>",
-        },
-      },
-    },
-  },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   event = "InsertEnter",
+  --   opts = {
+  --     panel = {
+  --       enabled = false,
+  --     },
+  --     suggestion = {
+  --       enabled = true,
+  --       auto_trigger = true,
+  --       keymap = {
+  --         accept = "<C-f>",
+  --         next = "<C-j>",
+  --         prev = "<C-k>",
+  --       },
+  --     },
+  --   },
+  -- },
 
   {
     "copilotlsp-nvim/copilot-lsp",
     init = function()
-      vim.g.copilot_nes_debounce = 500
+      vim.g.copilot_nes_debounce = 10
       vim.lsp.enable "copilot"
-      vim.keymap.set("n", "<Tab>", function() require("copilot-lsp.nes").apply_pending_nes() end)
+      vim.keymap.set("n", "<C-f>", function() require("copilot-lsp.nes").apply_pending_nes() end)
     end,
   },
 }
