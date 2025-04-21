28e506cd3c48869b91bf7cb53308ef7153df66ce
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-21 12:47:05 +0700

    Auto Update: 2025-04-21 12:47:05 from Arch Linux

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -14,9 +14,9 @@ return {
           curl = {
             opts = {
               set_compressed = true,
-            }
-          }
-        }
+            },
+          },
+        },
       }
     end,
   },
