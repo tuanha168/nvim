e0fb2e8a796c99384d6ab68b0cdffdbfe86e3917
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-21 11:58:43 +0700

    Auto Update: 2025-04-21 11:58:43 from Arch Linux

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -3,15 +3,22 @@ return {
   {
     "rest-nvim/rest.nvim",
     dependencies = { { "nvim-lua/plenary.nvim" } },
-    build = "yarn global add curlconverter",
     ft = { "http" },
     keys = {
       { "<leader>er", "<cmd>Rest run<CR>", desc = "Run" },
     },
     config = function()
       ---@type rest.Opts
-      -- vim.g.rest_nvim = {
-      -- }
+      vim.g.rest_nvim = {
+        -- Open request results in a horizontal split
+        result_split_horizontal = false,
+        -- Keep the http file buffer above|left when split horizontal|vertical
+        result_split_in_place = false,
+        -- Skip SSL verification, useful for unknown certificates
+        skip_ssl_verification = false,
+        -- Encode URL before making request
+        encode_url = true,
+      }
     end,
   },
 }
