bc7bfa1e6090f0192eae45fc3798c938ad533608
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-21 22:31:42 +0700

    Auto Update: 2025-04-21 22:31:42 from Arch Linux

--- lua/lsp/config/tailwindcss.lua
+++ lua/lsp/config/tailwindcss.lua
@@ -5,20 +5,20 @@ return {
   root_dir = util.root_pattern("tailwind.config.ts", "tailwind.config.js"),
   settings = {
     tailwindCSS = {
-      classAttributes = {
-        "class",
-        "className",
-        "ngClass",
-        ".*[cC]lass.*",
-        ".*[cC]lasses.*",
-      },
-      experimental = {
-        classRegex = {
-          "[a-zA-Z]*[cC]lass(es)?\\s*=\\s*'([^']*)'",
-          '[a-zA-Z]*[cC]lass(es)?\\s*=\\s*"([^"]*)"',
-          "[a-zA-Z]*[cC]lass(es)?\\s*=\\s*\\{(['`\"][^\\{\\}'`\"]*['`\"])*\\}",
-        },
-      },
+      -- classAttributes = {
+      --   "class",
+      --   "className",
+      --   "ngClass",
+      --   ".*[cC]lass.*",
+      --   ".*[cC]lasses.*",
+      -- },
+      -- experimental = {
+      --   classRegex = {
+      --     "[a-zA-Z]*[cC]lass(es)?\\s*=\\s*'([^']*)'",
+      --     '[a-zA-Z]*[cC]lass(es)?\\s*=\\s*"([^"]*)"',
+      --     "[a-zA-Z]*[cC]lass(es)?\\s*=\\s*\\{(['`\"][^\\{\\}'`\"]*['`\"])*\\}",
+      --   },
+      -- },
       emmetCompletions = true,
     },
   },
