2476b5aeea31bfb40f705a61f878609ffcffc109
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-21 21:59:52 +0700

    Auto Update: 2025-04-21 21:59:52 from Arch Linux

--- lua/lsp/config/tailwindcss.lua
+++ lua/lsp/config/tailwindcss.lua
@@ -1,5 +1,25 @@
 local util = require "lspconfig.util"
 
+---@type lspconfig.options.tailwindcss
 return {
   root_dir = util.root_pattern("tailwind.config.ts", "tailwind.config.js"),
+  settings = {
+    tailwindCSS = {
+      classAttributes = {
+        "class",
+        "className",
+        "ngClass",
+        ".*[cC]lass.*",
+        ".*[cC]lasses.*",
+      },
+      experimental = {
+        classRegex = {
+          "[a-zA-Z]*[cC]lass(es)?\\s*=\\s*'([^']*)'",
+          '[a-zA-Z]*[cC]lass(es)?\\s*=\\s*"([^"]*)"',
+          "[a-zA-Z]*[cC]lass(es)?\\s*=\\s*\\{(['`\"][^\\{\\}'`\"]*['`\"])*\\}",
+        },
+      },
+      emmetCompletions = true,
+    },
+  },
 }
--- lua/neoconf/generate.lua
+++ lua/neoconf/generate.lua
@@ -26,61 +26,6 @@ return {
       },
       lspconfig = {
         volar = {
-          vue = {
-            inlayHints = {
-              missingProps = true,
-              optionsWrapper = true,
-              vBindShorthand = true,
-              destructuredProps = true,
-              inlineHandlerLeading = true,
-            },
-          },
-          typescript = {
-            tsserver = {
-              web = {
-                projectWideIntellisense = {
-                  enabled = "true",
-                },
-              },
-            },
-            inlayHints = {
-              enumMemberValues = { enabled = "true" },
-              propertyDeclarationTypes = { enabled = "true" },
-              parameterNames = {
-                enabled = "all",
-                suppressWhenArgumentMatchesName = "true",
-              },
-              -- functionLikeReturnTypes = { enabled = "true" },
-              -- variableTypes = {
-              --   enabled = "true",
-              --   suppressWhenTypeMatchesName = "true",
-              -- },
-              parameterTypes = { enabled = "true" },
-            },
-          },
-          css = {
-            customData = {
-              home .. "/.config/nvim/.vscode/tailwind.json",
-            },
-          },
-          javascript = {
-            inlayHints = {
-              enumMemberValues = { enabled = "true" },
-              propertyDeclarationTypes = { enabled = "true" },
-              parameterNames = {
-                enabled = "all",
-                suppressWhenArgumentMatchesName = "true",
-              },
-              -- functionLikeReturnTypes = { enabled = "true" },
-              -- variableTypes = {
-              --   enabled = "true",
-              --   suppressWhenTypeMatchesName = "true",
-              -- },
-              parameterTypes = { enabled = "true" },
-            },
-          },
-        },
-        tailwindcss = {
         },
         rust_analyzer = {
           ["rust-analyzer"] = {
