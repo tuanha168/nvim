2e7c02f02f67f2a6a8964471f702ecd0b6eca28a
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-22 20:00:48 +0700

    Auto Update: 2025-04-22 20:00:48 from Arch Linux

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -3,6 +3,6 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^6", -- Recommended
     lazy = false, -- This plugin is already lazy
-    opts = function() vim.g.rustaceanvim = { dap = { autoload_configurations = false } } end,
+    config = function() vim.g.rustaceanvim = { dap = { autoload_configurations = false } } end,
   },
 }
