4dd7ed47a4b3157d6d43c3450305fbfb62d66afe
yopaz-tuanha <tuanha@yopaz.vn>
2025-04-25 18:19:45 +0700

    Auto Update: 2025-04-25 18:19:45 from Arch Linux

--- lua/Chiruno/func/auto_push.lua
+++ lua/Chiruno/func/auto_push.lua
@@ -1,14 +1,17 @@
-local logsDir = os.getenv("HOME") .. "/.dotfile/logs/git-auto-push"
+local logsDir = os.getenv "HOME" .. "/.dotfile/logs/git-auto-push"
 
 function Chiruno.func.auto_push(path)
-  if vim.fn.executable("git-auto-push") == 1 then
-    if not Chiruno.func.isdir_exist(logsDir) then
-      vim.fn.mkdir(logsDir, "p")
-    end
-    print("Auto Pushing", path)
+  local is_git_auto_push_available = vim.fn.executable "git-auto-push" == 1
+  if is_git_auto_push_available then
+    -- Ensure logs directory exists
+    if not Chiruno.func.isdir_exist(logsDir) then vim.fn.mkdir(logsDir, "p") end
+
+    -- Prepare log file and command
     local folder_name = vim.fn.fnamemodify(path, ":t")
     local log_file = string.format("%s/%s.log", logsDir, folder_name)
     local cmd = string.format("git-auto-push %s >> %s &", path, log_file)
+
+    -- Start the job asynchronously
     vim.fn.jobstart(cmd, { detach = true })
   end
 end
