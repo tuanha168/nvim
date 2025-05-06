6597ca716188fd6b3538364699d0adfce2daa16c
yopaz-tuanha <tuanha@yopaz.vn>
2025-05-06 11:16:59 +0700

    Auto Update: 2025-05-06 11:16:58 from Arch Linux

--- lua/lsp/config/eslint.lua
+++ lua/lsp/config/eslint.lua
@@ -4,16 +4,15 @@ local util = require "lspconfig.util"
 return {
   -- filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "json", "svg" },
   root_dir = util.root_pattern(
-    ".git"
-    -- ".eslintrc",
-    -- ".eslintrc.cjs",
-    -- ".eslintrc.js",
-    -- ".eslintrc.ts",
-    -- ".eslintrc.json",
-    -- ".eslintrc.yml",
-    -- "eslint.config.js",
-    -- "eslint.config.mjs",
-    -- "eslint.config.ts"
+    ".eslintrc",
+    ".eslintrc.cjs",
+    ".eslintrc.js",
+    ".eslintrc.ts",
+    ".eslintrc.json",
+    ".eslintrc.yml",
+    "eslint.config.js",
+    "eslint.config.mjs",
+    "eslint.config.ts"
   ),
   settings = {
     eslint = {
