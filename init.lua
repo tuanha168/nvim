7396fbe7d1874210dd559a2ce2155020af11b7fa
yopaz-tuanha <tuanha@yopaz.vn>
2025-05-14 17:51:23 +0700

    Auto Update: 2025-05-14 17:51:23 from Arch Linux

--- init.lua
+++ init.lua
@@ -23,5 +23,5 @@ require "options"
 require "lazy_setup"
 require "polish"
 
-vim.lsp.config("sonarlint", require("lsp.sonarlint"))
-vim.lsp.enable("sonarlint")
+vim.lsp.config("tsgo", require("lsp.tsgo"))
+vim.lsp.enable("tsgo")
