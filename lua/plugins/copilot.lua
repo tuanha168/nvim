3979dfe9ea2c1a4b0aacba79629b6b1aa2b9eee0
yopaz-tuanha <tuanha@yopaz.vn>
2025-05-27 14:25:10 +0700

    Auto Update: 2025-05-27 14:25:09 from Arch Linux

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,62 +1,62 @@
 ---@type LazySpec
 return {
+  {
+    "zbirenbaum/copilot.lua",
+    event = { "InsertEnter" },
+    keys = {
+      { "<C-f>", mode = "i" },
+      { "<C-j>", mode = "i" },
+      { "<C-k>", mode = "i" },
+    },
+    opts = {
+      panel = {
+        enabled = false,
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<C-f>",
+          next = "<C-j>",
+          prev = "<C-k>",
+        },
+      },
+    },
+  },
+
   -- {
-  --   "zbirenbaum/copilot.lua",
-  --   event = { "InsertEnter" },
-  --   keys = {
-  --     { "<C-f>", mode = "i" },
-  --     { "<C-j>", mode = "i" },
-  --     { "<C-k>", mode = "i" },
-  --   },
-  --   opts = {
-  --     panel = {
-  --       enabled = false,
-  --     },
-  --     suggestion = {
-  --       enabled = true,
-  --       auto_trigger = true,
-  --       keymap = {
-  --         accept = "<C-f>",
-  --         next = "<C-j>",
-  --         prev = "<C-k>",
-  --       },
-  --     },
-  --   },
+  --   "copilotlsp-nvim/copilot-lsp",
+  --   init = function()
+  --     vim.g.copilot_nes_debounce = 10
+  --     vim.lsp.enable "copilot_ls"
+  --     vim.keymap.set(
+  --       { "n", "i" },
+  --       "<C-f>",
+  --       function()
+  --         local _ = require("copilot-lsp.nes").walk_cursor_start_edit()
+  --           or (require("copilot-lsp.nes").apply_pending_nes() and require("copilot-lsp.nes").walk_cursor_end_edit())
+  --       end
+  --     )
+  --   end,
   -- },
 
-  {
-    "copilotlsp-nvim/copilot-lsp",
-    init = function()
-      vim.g.copilot_nes_debounce = 10
-      vim.lsp.enable "copilot_ls"
-      vim.keymap.set(
-        { "n", "i" },
-        "<C-f>",
-        function()
-          local _ = require("copilot-lsp.nes").walk_cursor_start_edit()
-            or (require("copilot-lsp.nes").apply_pending_nes() and require("copilot-lsp.nes").walk_cursor_end_edit())
-        end
-      )
-    end,
-  },
-
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    cmd = {
-      "CopilotChat",
-      "CopilotChatToggle",
-      "CopilotChatPrompts",
-      "CopilotChatModels",
-      "CopilotChatAgents",
-    },
-    dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or zbirenbaum/copilot.lua
-      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
-    },
-    build = "make tiktoken", -- Only on MacOS or Linux
-    opts = {
-      -- See Configuration section for options
-    },
-    -- See Commands section for default commands if you want to lazy load on them
-  },
+  -- {
+  --   "CopilotC-Nvim/CopilotChat.nvim",
+  --   cmd = {
+  --     "CopilotChat",
+  --     "CopilotChatToggle",
+  --     "CopilotChatPrompts",
+  --     "CopilotChatModels",
+  --     "CopilotChatAgents",
+  --   },
+  --   dependencies = {
+  --     { "zbirenbaum/copilot.lua" }, -- or zbirenbaum/copilot.lua
+  --     { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+  --   },
+  --   build = "make tiktoken", -- Only on MacOS or Linux
+  --   opts = {
+  --     -- See Configuration section for options
+  --   },
+  --   -- See Commands section for default commands if you want to lazy load on them
+  -- },
 }
