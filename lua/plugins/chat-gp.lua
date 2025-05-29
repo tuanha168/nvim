ddef0d874af7e6d5149ae1f9561ebfaf0d9937e2
yopaz-tuanha <tuanha@yopaz.vn>
2025-05-29 11:20:29 +0700

    Auto Update: 2025-05-29 11:20:29 from Arch Linux

--- lua/plugins/chat-gp.lua
+++ lua/plugins/chat-gp.lua
@@ -2,13 +2,22 @@
 return {
   {
     "robitx/gp.nvim",
+    cmd = {
+      "GpChatToggle",
+    },
     config = function()
-      local conf = {
-        -- For customization, refer to Install > Configuration in the Documentation/Readme
+      require("gp").setup {
+        provider = {
+          copilot = {
+            endpoint = "https://api.githubcopilot.com/chat/completions",
+            secret = {
+              "bash",
+              "-c",
+              "cat ~/.config/github-copilot/hosts.json | sed -e 's/.*oauth_token...//;s/\".*//'",
+            },
+          },
+        },
       }
-      require("gp").setup(conf)
-
-      -- Setup shortcuts here (see Usage > Shortcuts in the Documentation/Readme)
     end,
   },
 }
