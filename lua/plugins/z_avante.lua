ce43ef435c999fd2fdc26ccb78aa8de2e75c3ee5
yopaz-tuanha <tuanha@yopaz.vn>
2025-06-02 11:13:30 +0700

    Auto Update: 2025-06-02 11:13:30 from Arch Linux

--- lua/plugins/z_avante.lua
+++ lua/plugins/z_avante.lua
@@ -3,34 +3,26 @@ return {
   {
     "yetone/avante.nvim",
     version = false, -- Never set this value to "*"! Never!
-    keys = function(_, keys)
-      ---@type avante.Config
-      local opts =
-        require("lazy.core.plugin").values(require("lazy.core.config").spec.plugins["avante.nvim"], "opts", false)
-
-      local mappings = {
-        {
-          "<leader>ua",
-          function() require("avante.api").ask() end,
-          desc = "avante: ask",
-          mode = { "n", "v" },
-        },
-        {
-          "<leader>ur",
-          function() require("avante.api").refresh() end,
-          desc = "avante: refresh",
-          mode = "v",
-        },
-        {
-          "<leader>ue",
-          function() require("avante.api").edit() end,
-          desc = "avante: edit",
-          mode = { "n", "v" },
-        },
-      }
-      mappings = vim.tbl_filter(function(m) return m[1] and #m[1] > 0 end, mappings)
-      return vim.list_extend(mappings, keys)
-    end,
+    keys = {
+      {
+        "<leader>ua",
+        function() require("avante.api").ask { new_chat = true } end,
+        desc = "avante: ask",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>ur",
+        function() require("avante.api").refresh() end,
+        desc = "avante: refresh",
+        mode = "v",
+      },
+      {
+        "<leader>ue",
+        function() require("avante.api").edit() end,
+        desc = "avante: edit",
+        mode = { "n", "v" },
+      },
+    },
     opts = {
       provider = "copilot",
       file_selector = {
