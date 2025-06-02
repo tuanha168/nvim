51023975d5970f8c876774b6bae2654190119b46
yopaz-tuanha <tuanha@yopaz.vn>
2025-06-02 16:13:30 +0700

    Auto Update: 2025-06-02 16:13:30 from Arch Linux

--- lua/plugins/z_avante.lua
+++ lua/plugins/z_avante.lua
@@ -3,7 +3,7 @@ return {
   {
     "yetone/avante.nvim",
     version = false, -- Never set this value to "*"! Never!
-    event = { "User", pattern = "VisualEnter" },
+    event = "User VisualEnter",
     keys = {
       "<leader>aa",
       "<leader>an",
