d11ca2528739f3b2627a444fde02ad8ba36ff573
yopaz-tuanha <tuanha@yopaz.vn>
2025-06-03 16:45:53 +0700

    Auto Update: 2025-06-03 16:45:52 from Arch Linux

--- lua/lsp/config/volar.lua
+++ lua/lsp/config/volar.lua
@@ -15,6 +15,7 @@ local filetypes = vim.g.vue_hybrid and {
 
 ---@type lspconfig.options.volar
 return {
+  enabled = false,
   root_dir = util.root_pattern(".git", "tsconfig.json", "tsconfig.ts", "tsconfig.js", "package.json"),
   filetypes = filetypes,
   init_options = { vue = { hybridMode = vim.g.vue_hybrid } },
