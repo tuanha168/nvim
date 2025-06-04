a615d3cf2a4ffcf92d402c2bc38da7929363267c
yopaz-tuanha <tuanha@yopaz.vn>
2025-06-04 11:19:19 +0700

    Auto Update: 2025-06-04 11:19:19 from Arch Linux

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -39,10 +39,10 @@ return {
         opts = {
           ensure_installed = vim.tbl_keys(servers),
           automatic_enable = {
-            exclude = vim.tbl_filter(function(server)
-              Print(server, servers[server].enabled)
-              return not servers[server].enabled
-            end, vim.tbl_keys(servers)),
+            exclude = vim.tbl_filter(
+              function(server) return servers[server].enabled == false end,
+              vim.tbl_keys(servers)
+            ),
           },
         },
       },
