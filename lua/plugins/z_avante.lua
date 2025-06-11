35311538886cdc5620025aa0869916b89abc8545
yopaz-tuanha <tuanha@yopaz.vn>
2025-06-11 15:40:51 +0700

    Auto Update: 2025-06-11 15:40:51 from Arch Linux

--- lua/plugins/z_avante.lua
+++ lua/plugins/z_avante.lua
@@ -12,15 +12,15 @@ return {
     opts = {
       provider = "copilot",
       ---@bug: https://github.com/yetone/avante.nvim/issues/1048
-      auto_suggestions_provider = "openai",
+      -- auto_suggestions_provider = "openai",
       selector = {
         provider = "snacks",
       },
       behaviour = {
+        -- auto_suggestions = true,
+        -- auto_suggestions_respect_ignore = true,
         use_cwd_as_project_root = true,
-        auto_suggestions = true,
         enable_cursor_planning_mode = true,
-        auto_suggestions_respect_ignore = true,
         enable_claude_text_editor_tool_mode = false,
       },
       suggestion = {
