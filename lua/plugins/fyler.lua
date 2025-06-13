130f05384634839cca6f288c075d014e745d5ba3
yopaz-tuanha <tuanha@yopaz.vn>
2025-06-13 13:23:55 +0700

    Auto Update: 2025-06-13 13:23:55 from Arch Linux

--- lua/plugins/fyler.lua
+++ lua/plugins/fyler.lua
@@ -1,9 +1,10 @@
 return {
   {
     "A7Lavinraj/fyler.nvim",
+    enabled = false,
     cmd = "Fyler",
     keys = {
-      { "<C-n>", "<cmd>Fyler split=left<CR>" }
+      { "<C-n>", "<cmd>Fyler split=left<CR>" },
     },
     dependencies = { "echasnovski/mini.icons" },
     opts = {},
