eb0aaf73469e4882101bb541685da8fd41bf03d6
yopaz-tuanha <tuanha@yopaz.vn>
2025-06-23 15:20:33 +0700

    Auto Update: 2025-06-23 15:20:33 from Arch Linux

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -58,7 +58,7 @@ return {
           local _ = require("copilot-lsp.nes").walk_cursor_start_edit()
             or (require("copilot-lsp.nes").apply_pending_nes() and require("copilot-lsp.nes").walk_cursor_end_edit())
         end,
-        mode = { "n", "x" },
+        mode = { "n", "i" },
       },
     },
     event = "VeryLazy",
