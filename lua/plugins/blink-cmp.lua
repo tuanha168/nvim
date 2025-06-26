b6d0e79b8294a07aec12ca0a2deb958192012b1e
yopaz-tuanha <tuanha@yopaz.vn>
2025-06-26 15:14:03 +0700

    Auto Update: 2025-06-26 15:14:03 from Arch Linux

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -166,7 +166,7 @@ return {
         list = {
           selection = {
             preselect = true,
-            auto_insert = false,
+            auto_insert = true,
           },
         },
         ghost_text = {
