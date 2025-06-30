9422ef18da9eacb605c60bb4f42c815e00bf340e
yopaz-tuanha <tuanha@yopaz.vn>
2025-06-30 17:39:18 +0700

    Auto Update: 2025-06-30 17:39:18 from Arch Linux

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -18,7 +18,6 @@ return {
     { "VimEnter", "VimLeave" },
     function()
       if vim.env.TMUX_PLUGIN_MANAGER_PATH then
-        Print(vim.env.TMUX_PLUGIN_MANAGER_PATH .. "/tmux-window-name/scripts/rename_session_windows.py")
         uv.spawn(vim.env.TMUX_PLUGIN_MANAGER_PATH .. "/tmux-window-name/scripts/rename_session_windows.py", {})
       end
     end,
