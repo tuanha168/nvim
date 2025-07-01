ea25ccac675e47b9be7a239c58675042d35c7776
yopaz-tuanha <tuanha@yopaz.vn>
2025-07-01 16:07:21 +0700

    Auto Update: 2025-07-01 16:07:21 from Arch Linux

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -199,7 +199,12 @@ return {
       -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
       --
       -- See the fuzzy documentation for more information
-      fuzzy = { implementation = "prefer_rust_with_warning" },
+      fuzzy = {
+        implementation = "prefer_rust_with_warning",
+        prebuilt_binaries = {
+          download = true,
+        },
+      },
     },
     opts_extend = { "sources.default" },
   },
