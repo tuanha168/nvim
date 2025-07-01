7ec88765b14cad1d2ed64bccfc0f38b59469a1d4
yopaz-tuanha <tuanha@yopaz.vn>
2025-07-01 15:50:23 +0700

    Auto Update: 2025-07-01 15:50:23 from Arch Linux

--- lua/plugins/z_avante.lua
+++ lua/plugins/z_avante.lua
@@ -60,20 +60,19 @@ return {
           "Kaiser-Yang/blink-cmp-avante",
         },
         opts = function(_, opts)
-          Print(opts)
-          -- table.insert(opts.sources.default, 1, "avante")
+          table.insert(opts.sources.default, 1, "avante")
 
-          -- opts.sources = vim.tbl_deep_extend("force", opts.sources or {}, {
-          --   providers = {
-          --     avante = {
-          --       module = "blink-cmp-avante",
-          --       name = "Avante",
-          --       opts = {
-          --         -- options for blink-cmp-avante
-          --       },
-          --     },
-          --   },
-          -- })
+          opts.sources = vim.tbl_deep_extend("force", opts.sources or {}, {
+            providers = {
+              avante = {
+                module = "blink-cmp-avante",
+                name = "Avante",
+                opts = {
+                  -- options for blink-cmp-avante
+                },
+              },
+            },
+          })
           return opts
         end,
       },
