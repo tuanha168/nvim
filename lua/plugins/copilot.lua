67b43a7fb30d295062a9957006d6680bdee0ef76
yopaz-tuanha <tuanha@yopaz.vn>
2025-07-02 15:05:12 +0700

    Auto Update: 2025-07-02 15:05:12 from Arch Linux

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -88,6 +88,7 @@ return {
     },
     build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
+      model = "claude-sonnet-4",
       -- See Configuration section for options
     },
     -- See Commands section for default commands if you want to lazy load on them
