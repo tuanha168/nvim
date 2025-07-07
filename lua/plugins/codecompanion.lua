71557dd22272aa1ab97b9050c4662913ca7d43a6
yopaz-tuanha <tuanha@yopaz.vn>
2025-07-07 12:45:06 +0700

    Auto Update: 2025-07-07 12:45:06 from Arch Linux

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -14,16 +14,16 @@ return {
           })
         end,
       },
-      extensions = {
-        mcphub = {
-          callback = "mcphub.extensions.codecompanion",
-          opts = {
-            show_result_in_chat = true, -- Show mcp tool results in chat
-            make_vars = true, -- Convert resources to #variables
-            make_slash_commands = true, -- Add prompts as /slash commands
-          },
-        },
-      },
+      -- extensions = {
+      --   mcphub = {
+      --     callback = "mcphub.extensions.codecompanion",
+      --     opts = {
+      --       show_result_in_chat = true, -- Show mcp tool results in chat
+      --       make_vars = true, -- Convert resources to #variables
+      --       make_slash_commands = true, -- Add prompts as /slash commands
+      --     },
+      --   },
+      -- },
     },
     dependencies = {
       "nvim-lua/plenary.nvim",
