811aa50e50d2aa554f080c00a1b56bbe14cee4b7
yopaz-tuanha <tuanha@yopaz.vn>
2025-07-07 16:42:07 +0700

    Auto Update: 2025-07-07 16:42:07 from Arch Linux

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -21,6 +21,14 @@ return {
         end,
       },
       extensions = {
+        mcphub = {
+          callback = "mcphub.extensions.codecompanion",
+          opts = {
+            show_result_in_chat = true, -- Show mcp tool results in chat
+            make_vars = true, -- Convert resources to #variables
+            make_slash_commands = true, -- Add prompts as /slash commands
+          },
+        },
         history = {
           enabled = true,
           opts = {
@@ -111,6 +119,14 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
       "ravitemer/codecompanion-history.nvim",
+      {
+        "ravitemer/mcphub.nvim",
+        dependencies = {
+          "nvim-lua/plenary.nvim",
+        },
+        build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+        opts = {},
+      },
     },
   },
 }
