1d3334f78dc7e14ddf6534724a9c3efc1c2de3ff
yopaz-tuanha <tuanha@yopaz.vn>
2025-07-07 09:23:26 +0700

    Auto Update: 2025-07-07 09:23:26 from Arch Linux

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,7 +1,16 @@
 return {
   {
     "olimorris/codecompanion.nvim",
-    opts = {},
+    opts = {
+      strategies = {
+        chat = {
+          adapter = {
+            name = "copilot",
+            model = "claude-sonnet-4",
+          },
+        },
+      },
+    },
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
