41689d70de31b15e0a2d3e4340065794a5d117e1
yopaz-tuanha <tuanha@yopaz.vn>
2025-07-08 10:51:02 +0700

    Auto Update: 2025-07-08 10:51:02 from Arch Linux

--- lsp/qmlls.lua
+++ lsp/qmlls.lua
@@ -1,4 +1,4 @@
 return {
-  cmd = { "/usr/lib/qt6/bin/qmlls" },
+  cmd = { "/usr/lib/qt6/bin/qmlls", "-E", "-I", "/usr/lib/qt6/qml", "-I", "/usr/lib/qt/qml" },
   root_markers = { ".qmlls.ini", ".git" },
 }
