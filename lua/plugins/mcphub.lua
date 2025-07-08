1011cff14208cbb2dec0212cc67eb120c03c4c0d
yopaz-tuanha <tuanha@yopaz.vn>
2025-07-08 17:58:19 +0700

    Auto Update: 2025-07-08 17:58:19 from Arch Linux

--- lua/plugins/mcphub.lua
+++ lua/plugins/mcphub.lua
@@ -8,9 +8,11 @@ return {
     build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
     opts = {
       config = vim.fn.stdpath "config" .. "/mcphub.servers.json",
-      level = vim.log.levels.DEBUG,
-      to_file = true,
-      file_path = vim.fn.expand "~/mcphub.log",
+      log = {
+        level = vim.log.levels.DEBUG,
+        to_file = true,
+        file_path = vim.fn.expand "~/mcphub.log",
+      }
     },
   },
 }
