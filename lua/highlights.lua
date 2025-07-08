3ae5061627236a1f0a11e1633fcb15d619bf542c
yopaz-tuanha <tuanha@yopaz.vn>
2025-07-08 20:19:04 +0700

    Auto Update: 2025-07-08 20:19:04 from Arch Linux

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -3,4 +3,6 @@ return {
   Visual = { bg = "#576b8a" },
   DiffAdd = { bg = "#4b5632" },
   DiffText = { bg = "#4b5632" },
+  diffRemoved = { bg = "#ff0000" },
+  DiffChange = { bg = "#4b5632" },
 }
