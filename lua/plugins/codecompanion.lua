c5bd5fd62447db9f78f447b7bc894e8bf65fa486
yopaz-tuanha <tuanha@yopaz.vn>
2025-07-08 19:05:42 +0700

    Auto Update: 2025-07-08 19:05:41 from Arch Linux

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -14,7 +14,8 @@ return {
           return require("codecompanion.adapters").extend("copilot", {
             schema = {
               model = {
-                default = "claude-sonnet-4",
+                -- default = "claude-sonnet-4",
+                default = "gpt-4o",
               },
             },
           })
