18892880b8447899332e18025e1a2aca84f7503f
yopaz-tuanha <tuanha@yopaz.vn>
2025-07-08 19:47:43 +0700

    Auto Update: 2025-07-08 19:47:43 from Arch Linux

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -21,28 +21,28 @@ return {
           })
         end,
       },
-      -- display = {
-      --   diff = {
-      --     provider = "mini_diff", -- default|mini_diff
-      --   },
-      -- },
-      strategies = {
-        chat = {
-          opts = {
-            ---Decorate the user message before it's sent to the LLM
-            ---@param message string
-            ---@param adapter CodeCompanion.Adapter
-            ---@param context table
-            ---@return string
-            prompt_decorator = function(message, adapter, context)
-              return string.format(
-                [[<prompt>If you happend to use mcp, or any actions that create, edit,... modify a file, don't stop to ask, i have diffirent layer of permission so you can just call the tool immidiely. Do the following: %s</prompt>]],
-                message
-              )
-            end,
-          },
+      display = {
+        diff = {
+          provider = "mini_diff", -- default|mini_diff
         },
       },
+      -- strategies = {
+      --   chat = {
+      --     opts = {
+      --       ---Decorate the user message before it's sent to the LLM
+      --       ---@param message string
+      --       ---@param adapter CodeCompanion.Adapter
+      --       ---@param context table
+      --       ---@return string
+      --       prompt_decorator = function(message, adapter, context)
+      --         return string.format(
+      --           [[<prompt>If you happend to use mcp, or any actions that create, edit,... modify a file, don't stop to ask, i have diffirent layer of permission so you can just call the tool immidiely. Do the following: %s</prompt>]],
+      --           message
+      --         )
+      --       end,
+      --     },
+      --   },
+      -- },
       extensions = {
         mcphub = {
           callback = "mcphub.extensions.codecompanion",
