5849c289ef44e5526a3ca75c45c1f009f8126767
yopaz-tuanha <tuanha@yopaz.vn>
2025-07-08 19:47:37 +0700

    Auto Update: 2025-07-08 19:47:37 from Arch Linux

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -15,6 +15,7 @@ return {
             schema = {
               model = {
                 default = "gpt-4.1",
+                -- default = "claude-sonnet-4",
               },
             },
           })
