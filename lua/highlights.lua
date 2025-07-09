f9b19e4f9983a93b11fa207368e4374f93741d9b
yopaz-tuanha <tuanha@yopaz.vn>
2025-07-09 16:04:34 +0700

    Auto Update: 2025-07-09 16:04:34 from Arch Linux

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -1,9 +1,9 @@
 ---@type table<string, vim.api.keyset.highlight>
 return {
-  Visual = { bg = "#c8e1ff", fg = "none" },
-  DiffAdd = { bg = "#d2fedb", fg = "none" },
-  DiffText = { bg = "#c8e1ff", fg = "none" },
-  diffRemoved = { bg = "#ffdce0", fg = "none" },
-  DiffChange = { bg = "#fff5b1", fg = "none" },
-  DiffDelete = { bg = "#ffdce0", fg = "none" },
+  Visual = { bg = "#1f6feb", fg = "none" },
+  DiffAdd = { bg = "#033a16", fg = "#aff5b4" },
+  DiffText = { bg = "#1f6feb", fg = "none" },
+  diffRemoved = { bg = "#3c0614", fg = "#ffd7d5" },
+  DiffChange = { bg = "#633c01", fg = "#fff8c5" },
+  DiffDelete = { bg = "#3c0614", fg = "#ffd7d5" },
 }
