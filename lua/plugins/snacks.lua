f821c12a9c7161505889bec4b6b3599dcbf38e62
yopaz-tuanha <tuanha@yopaz.vn>
2025-07-11 13:35:59 +0700

    Auto Update: 2025-07-11 13:35:59 from Arch Linux

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -81,6 +81,7 @@ return {
       -- git
       { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
       { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
+			{ "<leader>gi", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
       { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
       { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
       { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
