d725d2434e507d3339dc63727d9635fc14f57552
yopaz-tuanha <tuanha@yopaz.vn>
2025-07-17 16:15:17 +0700

    Auto Update: 2025-07-17 16:15:17 from Arch Linux

--- lua/polish.lua
+++ lua/polish.lua
@@ -80,8 +80,8 @@ vim.filetype.add {
     [".*"] = {
       function(path, bufnr)
         local content = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] or ""
-        if vim.regex([[^#.*bun[&| ].*&]]):match_str(content) ~= nil then
-          return "mine"
+        if vim.regex([[^#.*bun&]]):match_str(content) ~= nil then
+          return "javascript"
         end
       end,
       { priority = -math.huge },
