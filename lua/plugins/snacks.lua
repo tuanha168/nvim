7e2d5023b3b186d3bf09a07c782417ba74556255
yopaz-tuanha <tuanha@yopaz.vn>
2025-07-17 11:46:32 +0700

    Auto Update: 2025-07-17 11:46:32 from Arch Linux

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -62,7 +62,7 @@ return {
     },
     keys = {
       -- Top Pickers & Explorer
-      { "<C-n>", function() Snacks.picker.explorer() end, desc = "Explorer" },
+      { "<C-n>", function() Snacks.picker.explorer { toggles = true } end, desc = "Explorer" },
       { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
       { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
       { "<leader>fw", function() Snacks.picker.grep() end, desc = "Grep" },
@@ -82,7 +82,7 @@ return {
       -- git
       { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
       { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
-			{ "<leader>gi", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
+      { "<leader>gi", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
       { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
       { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
       { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
