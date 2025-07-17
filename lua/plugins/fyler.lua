ec68ca302f2c7b03194ce7c737a63c9b0ef4d1a6
yopaz-tuanha <tuanha@yopaz.vn>
2025-07-17 12:01:50 +0700

    Auto Update: 2025-07-17 12:01:50 from Arch Linux

--- lua/plugins/fyler.lua
+++ lua/plugins/fyler.lua
@@ -4,7 +4,7 @@ return {
     -- enabled = false,
     cmd = "Fyler",
     keys = {
-      { "<C-n>", "<cmd>Fyler split=left<CR>" },
+      { "<C-n>", "<cmd>Fyler kind=split:left<CR>" },
     },
     -- commit = "d87e4281e18712361f82a07f9fca71957244ef33",
     dependencies = { "echasnovski/mini.icons" },
