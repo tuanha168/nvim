9c08c6b4a93d9095cfb990f35b7cccf741453497
TuanHA <achicir2@gmail.com>
2025-07-22 15:47:44 +0700

    Auto Update: 2025-07-22 15:47:44 from Arch Linux

--- lua/plugins/magenta.lua
+++ lua/plugins/magenta.lua
@@ -2,7 +2,7 @@
 return {
   {
     "dlants/magenta.nvim",
-    enabled = true,
+    enabled = false,
     lazy = false, -- you could also bind to <leader>mt
     build = "npm install --frozen-lockfile",
     opts = {
