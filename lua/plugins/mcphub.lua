84c260257b07c0c7f096a14dcf3ccc1d9fcb75df
TuanHA <achicir2@gmail.com>
2025-07-22 09:57:25 +0700

    Auto Update: 2025-07-22 09:57:25 from Arch Linux

--- lua/plugins/mcphub.lua
+++ lua/plugins/mcphub.lua
@@ -5,9 +5,11 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
-    build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+    -- build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+    build = "bundled_build.lua", -- Installs `mcp-hub` node binary globally
     opts = {
       config = vim.fn.stdpath "config" .. "/mcphub.servers.json",
+      use_bundled_binary = true,
       log = {
         level = vim.log.levels.DEBUG,
         to_file = true,
