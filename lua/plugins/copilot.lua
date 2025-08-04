17b643265c518912f0f4cc359f6b0a093cc0be89
yopaz-tuanha <tuanha@yopaz.vn>
2025-08-04 09:06:40 +0700

    Auto Update: 2025-08-04 09:06:40 from Arch Linux

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -62,7 +62,7 @@ return {
       { "zbirenbaum/copilot.lua" }, -- or zbirenbaum/copilot.lua
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
-    build = "make tiktoken", -- Only on MacOS or Linux
+    -- build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
       model = "claude-sonnet-4",
       mappings = {
