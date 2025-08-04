864001e702e83e3f5be80184153ddaa4c041a10c
yopaz-tuanha <tuanha@yopaz.vn>
2025-08-04 09:11:31 +0700

    Auto Update: 2025-08-04 09:11:30 from Arch Linux

--- lua/plugins/mcphub.lua
+++ lua/plugins/mcphub.lua
@@ -9,6 +9,14 @@ return {
     build = "npm install -g mcp-hub@3.7.0", -- Installs `mcp-hub` node binary globally
     opts = {
       config = vim.fn.stdpath "config" .. "/mcphub.servers.json",
+      extensions = {
+        copilotchat = {
+          enabled = true,
+          convert_tools_to_functions = true,       -- Convert MCP tools to CopilotChat functions
+          convert_resources_to_functions = true,   -- Convert MCP resources to CopilotChat functions
+          add_mcp_prefix = false,                  -- Add "mcp_" prefix to function names
+        }
+      },
       log = {
         level = vim.log.levels.DEBUG,
         to_file = true,
