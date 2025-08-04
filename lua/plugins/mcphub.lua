17284dd30568f1b32938ac09fdf14817580a2bf3
yopaz-tuanha <tuanha@yopaz.vn>
2025-08-04 09:39:14 +0700

    Auto Update: 2025-08-04 09:39:14 from Arch Linux

--- lua/plugins/mcphub.lua
+++ lua/plugins/mcphub.lua
@@ -6,8 +6,9 @@ return {
       "nvim-lua/plenary.nvim",
       "CopilotC-Nvim/CopilotChat.nvim",
     },
-    commit = "15d490149502a8fb523a148dd564d597d34c3eb4",
-    build = "npm install -g mcp-hub@3.7.0", -- Installs `mcp-hub` node binary globally
+    -- commit = "15d490149502a8fb523a148dd564d597d34c3eb4",
+    -- build = "npm install -g mcp-hub@3.7.0", -- Installs `mcp-hub` node binary globally
+    build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
     opts = {
       config = vim.fn.stdpath "config" .. "/mcphub.servers.json",
       extensions = {
