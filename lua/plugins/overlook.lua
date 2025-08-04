dc4860c11eb192880132f2cc6fa0c74efafa77b2
yopaz-tuanha <tuanha@yopaz.vn>
2025-08-04 12:56:13 +0700

    Auto Update: 2025-08-04 12:56:12 from Arch Linux

--- lua/plugins/overlook.lua
+++ lua/plugins/overlook.lua
@@ -2,12 +2,10 @@ return {
   {
     "WilliamHsieh/overlook.nvim",
     opts = {},
-
-    -- Optional: set up common keybindings
     keys = {
-      { "<leader>gpd", function() require("overlook.api").peek_definition() end, desc = "Overlook: Peek definition" },
-      { "<leader>gpc", function() require("overlook.api").close_all() end, desc = "Overlook: Close all popup" },
-      { "<leader>gpu", function() require("overlook.api").restore_popup() end, desc = "Overlook: Restore popup" },
+      { "gpd", function() require("overlook.api").peek_definition() end, desc = "Overlook: Peek definition" },
+      { "gpc", function() require("overlook.api").close_all() end, desc = "Overlook: Close all popup" },
+      { "gpu", function() require("overlook.api").restore_popup() end, desc = "Overlook: Restore popup" },
     },
   },
 }
