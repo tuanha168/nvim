fb388b8bfc3a92a591d74cddedcaa87ee5caa006
yopaz-tuanha <tuanha@yopaz.vn>
2025-08-04 09:34:54 +0700

    Auto Update: 2025-08-04 09:34:54 from Arch Linux

--- lua/plugins/mcphub.lua
+++ lua/plugins/mcphub.lua
@@ -4,6 +4,7 @@ return {
     cmd = { "MCPHub" },
     dependencies = {
       "nvim-lua/plenary.nvim",
+      "CopilotC-Nvim/CopilotChat.nvim",
     },
     commit = "15d490149502a8fb523a148dd564d597d34c3eb4",
     build = "npm install -g mcp-hub@3.7.0", -- Installs `mcp-hub` node binary globally
@@ -12,16 +13,16 @@ return {
       extensions = {
         copilotchat = {
           enabled = true,
-          convert_tools_to_functions = true,       -- Convert MCP tools to CopilotChat functions
-          convert_resources_to_functions = true,   -- Convert MCP resources to CopilotChat functions
-          add_mcp_prefix = true,                  -- Add "mcp_" prefix to function names
-        }
+          convert_tools_to_functions = false, -- Convert MCP tools to CopilotChat functions
+          convert_resources_to_functions = false, -- Convert MCP resources to CopilotChat functions
+          add_mcp_prefix = true, -- Add "mcp_" prefix to function names
+        },
       },
       log = {
         level = vim.log.levels.DEBUG,
         to_file = true,
         file_path = vim.fn.expand "~/mcphub.log",
-      }
+      },
     },
   },
 }
