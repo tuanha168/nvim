587e84f7a4289177bd87a111fe30299ef011b713
yopaz-tuanha <tuanha@yopaz.vn>
2025-08-25 14:07:58 +0700

    Auto Update: 2025-08-25 14:07:58 from Arch Linux

--- lua/plugins/grug-far.lua
+++ lua/plugins/grug-far.lua
@@ -1,6 +1,10 @@
 return {
   {
     "MagicDuck/grug-far.nvim",
+    keys = {
+      { "<leader>fr", ":GrugFar<cr>", mode = "v", desc = "Find and Replace in files" },
+      { "<leader>fR", ":GrugFarWithin<cr>", mode = "v", desc = "Find and Replace in current file" },
+    },
     opts = {},
   },
 }
