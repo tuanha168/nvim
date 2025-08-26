193f12301d9e0da73081b3e20f8917832083ceaa
TuanHA <achicir2@gmail.com>
2025-08-26 16:13:22 +0700

    Auto Update: 2025-08-26 16:13:22 from Arch Linux

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -25,37 +25,37 @@ return {
     },
   },
 
-  {
-    "copilotlsp-nvim/copilot-lsp",
-    keys = {
-      {
-        "<C-f>",
-        function()
-          local bufnr = vim.api.nvim_get_current_buf()
-          local state = vim.b[bufnr].nes_state
-          if state then
-            -- Try to jump to the start of the suggestion edit.
-            -- If already at the start, then apply the pending suggestion and jump to the end of the edit.
-            local _ = require("copilot-lsp.nes").walk_cursor_start_edit()
-              or (require("copilot-lsp.nes").apply_pending_nes() and require("copilot-lsp.nes").walk_cursor_end_edit())
-          end
-        end,
-        desc = "Accept Copilot NES suggestion",
-        expr = true,
-        mode = { "n" },
-      },
-    },
-    event = "BufEnter",
-    opts = {
-      nes = {
-        move_count_threshold = 3, -- Clear after 3 cursor movements
-      },
-    },
-    init = function()
-      vim.g.copilot_nes_debounce = 500
-      vim.lsp.enable "copilot_ls"
-    end,
-  },
+  -- {
+  --   "copilotlsp-nvim/copilot-lsp",
+  --   keys = {
+  --     {
+  --       "<C-f>",
+  --       function()
+  --         local bufnr = vim.api.nvim_get_current_buf()
+  --         local state = vim.b[bufnr].nes_state
+  --         if state then
+  --           -- Try to jump to the start of the suggestion edit.
+  --           -- If already at the start, then apply the pending suggestion and jump to the end of the edit.
+  --           local _ = require("copilot-lsp.nes").walk_cursor_start_edit()
+  --             or (require("copilot-lsp.nes").apply_pending_nes() and require("copilot-lsp.nes").walk_cursor_end_edit())
+  --         end
+  --       end,
+  --       desc = "Accept Copilot NES suggestion",
+  --       expr = true,
+  --       mode = { "n" },
+  --     },
+  --   },
+  --   event = "BufEnter",
+  --   opts = {
+  --     nes = {
+  --       move_count_threshold = 3, -- Clear after 3 cursor movements
+  --     },
+  --   },
+  --   init = function()
+  --     vim.g.copilot_nes_debounce = 500
+  --     vim.lsp.enable "copilot_ls"
+  --   end,
+  -- },
 
   -- {
   --   "CopilotC-Nvim/CopilotChat.nvim",
