e276fea7f10aed3ade120587425da7b6db9a425e
yopaz-tuanha <tuanha@yopaz.vn>
2025-09-09 13:48:00 +0700

    Auto Update: 2025-09-09 13:47:59 from Arch Linux

--- lua/plugins/regexplainer.lua
+++ lua/plugins/regexplainer.lua
@@ -3,6 +3,14 @@ return {
   "bennypowers/nvim-regexplainer",
   opts = {
     auto = true,
+    mappings = {
+      toggle = "<Leader>gR",
+      -- examples, not defaults:
+      -- show = 'gS',
+      -- hide = 'gH',
+      -- show_split = 'gP',
+      -- show_popup = 'gU',
+    },
   },
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
