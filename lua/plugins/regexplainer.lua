c3bc024288af6cecf54600974c2a4cdbdd7088c2
yopaz-tuanha <tuanha@yopaz.vn>
2025-09-09 13:43:46 +0700

    Auto Update: 2025-09-09 13:43:46 from Arch Linux

---
+++ lua/plugins/regexplainer.lua
@@ -0,0 +1,11 @@
+---@type LazySpec
+return {
+  "bennypowers/nvim-regexplainer",
+  opts = {
+    auto = true,
+  },
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "MunifTanjim/nui.nvim",
+  },
+}
