f779e302f76300c333aa37b09d5a202f3e9f24fe
yopaz-tuanha <tuanha@yopaz.vn>
2025-09-17 17:22:10 +0700

    Auto Update: 2025-09-17 17:22:10 from Arch Linux

--- lua/plugins/meow.yarn.lua
+++ lua/plugins/meow.yarn.lua
@@ -6,6 +6,32 @@ return {
       require("meow.yarn").setup {
         -- Your custom configuration goes here
       }
+
+      -- Using lua functions
+      vim.keymap.set(
+        "n",
+        "<leader>yt",
+        function() require("meow.yarn").open_tree("type_hierarchy", "supertypes") end,
+        { desc = "Yarn: Type Hierarchy (Super)" }
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>yT",
+        function() require("meow.yarn").open_tree("type_hierarchy", "subtypes") end,
+        { desc = "Yarn: Type Hierarchy (Sub)" }
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>yc",
+        function() require("meow.yarn").open_tree("call_hierarchy", "callers") end,
+        { desc = "Yarn: Call Hierarchy (Callers)" }
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>yC",
+        function() require("meow.yarn").open_tree("call_hierarchy", "callees") end,
+        { desc = "Yarn: Call Hierarchy (Callees)" }
+      )
     end,
   },
 }
