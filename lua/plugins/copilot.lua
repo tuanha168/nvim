2e7f7767f7c094a693a5044b548a44b99d9e636e
yopaz-tuanha <tuanha@yopaz.vn>
2025-09-24 13:49:42 +0700

    Auto Update: 2025-09-24 13:49:41 from Arch Linux

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -23,7 +23,7 @@ return {
       nes = {
         enabled = true,
         keymap = {
-          accept_and_goto = "<C-f>",
+          accept_and_goto = "<C-F>",
           accept = false,
           dismiss = "<Esc>",
         },
@@ -53,38 +53,6 @@ return {
   --     "zbirenbaum/copilot.lua",
   --   },
   --   opts = {},
-  -- },
-
-  -- {
-  --   "copilotlsp-nvim/copilot-lsp",
-  --   keys = {
-  --     {
-  --       "<C-f>",
-  --       function()
-  --         local bufnr = vim.api.nvim_get_current_buf()
-  --         local state = vim.b[bufnr].nes_state
-  --         if state then
-  --           -- Try to jump to the start of the suggestion edit.
-  --           -- If already at the start, then apply the pending suggestion and jump to the end of the edit.
-  --           local _ = require("copilot-lsp.nes").walk_cursor_start_edit()
-  --             or (require("copilot-lsp.nes").apply_pending_nes() and require("copilot-lsp.nes").walk_cursor_end_edit())
-  --         end
-  --       end,
-  --       desc = "Accept Copilot NES suggestion",
-  --       expr = true,
-  --       mode = { "n" },
-  --     },
-  --   },
-  --   event = "BufEnter",
-  --   opts = {
-  --     nes = {
-  --       move_count_threshold = 3, -- Clear after 3 cursor movements
-  --     },
-  --   },
-  --   init = function()
-  --     vim.g.copilot_nes_debounce = 500
-  --     vim.lsp.enable "copilot_ls"
-  --   end,
   -- },
 
   -- {
