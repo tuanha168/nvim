d834d40f5f1cdcb4d1a39dd4d6d6faee0a62997f
yopaz-tuanha <tuanha@yopaz.vn>
2025-09-24 13:27:20 +0700

    Auto Update: 2025-09-24 13:27:19 from Arch Linux

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -4,8 +4,8 @@ return {
     "stevearc/aerial.nvim",
     opts = {},
     key = {
-      { "<Leader>o", function() require("aerial").snacks_picker() end, desc = "Symbols outline" },
-      { "<Leader>O", function() require("aerial").toggle() end,        desc = "Symbols outline" },
+      { "<leader>o", function() require("aerial").snacks_picker() end, desc = "Symbols outline" },
+      { "<leader>O", function() require("aerial").toggle() end,        desc = "Symbols outline" },
     }
   },
 }
