54d3e192c6cf1d23ab037baed3c136390f87d020
yopaz-tuanha <tuanha@yopaz.vn>
2025-09-24 13:36:22 +0700

    Auto Update: 2025-09-24 13:36:22 from Arch Linux

--- lsp/vtsls.lua
+++ lsp/vtsls.lua
@@ -56,4 +56,13 @@ return {
       inlayHints = inlayHints,
     },
   },
+
+  on_init = function(_client)
+    local old_on_attach = _client.on_attach
+    _client.on_attach = function(client, buf)
+      Print(client, buf)
+      old_on_attach(client, buf)
+      vim.b[buf].formatByLsp = true
+    end
+  end,
 }
