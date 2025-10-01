3e2ff3d5a673edff888cdeed2d2d0a84c2d3bdf5
yopaz-tuanha <tuanha@yopaz.vn>
2025-10-01 11:53:38 +0700

    Auto Update: 2025-10-01 11:53:37 from Arch Linux

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -12,7 +12,7 @@ return {
     },
     keys = {
       {
-        "<tab>",
+        "<Tab>",
         function()
           -- if there is a next edit, jump to it, otherwise apply it if any
           if require("sidekick").nes_jump_or_apply() then
@@ -25,32 +25,32 @@ return {
           -- any other things (like snippets) you want to do on <tab> go here.
 
           -- fall back to normal tab
-          return "<tab>"
+          return "<Tab>"
         end,
         mode = { "i", "n" },
         expr = true,
         desc = "Goto/Apply Next Edit Suggestion",
       },
       {
-        "<c-.>",
+        "<C-.>",
         function() require("sidekick.cli").focus() end,
         mode = { "n", "x", "i", "t" },
         desc = "Sidekick Switch Focus",
       },
       {
-        "<leader>ca",
+        "<Leader>ca",
         function() require("sidekick.cli").toggle { focus = true } end,
         desc = "Sidekick Toggle CLI",
         mode = { "n", "v" },
       },
       {
-        "<leader>cc",
+        "<Leader>cc",
         function() require("sidekick.cli").toggle { name = "opencode", focus = true } end,
         desc = "Sidekick Claude Toggle",
         mode = { "n", "v" },
       },
       {
-        "<leader>cp",
+        "<Leader>cp",
         function() require("sidekick.cli").select_prompt() end,
         desc = "Sidekick Ask Prompt",
         mode = { "n", "v" },
