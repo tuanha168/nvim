e2c2f5f91e55665d6401e4a6b886c7a66e58a2d7
yopaz-tuanha <tuanha@yopaz.vn>
2025-10-01 13:29:07 +0700

    Auto Update: 2025-10-01 13:29:07 from Arch Linux

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -95,7 +95,9 @@ return {
 
   { "gG", ":<c-u>normal! ggVG<cr>", desc = "Buffer as text object", mode = { "x", "o" } },
 
-  { "<Tab>", "<cmd>bn<CR>", desc = "Move to next buffer" },
+  { "<Tab>", function()
+    Print("Tab pressed, normal")
+return  "<cmd>bn<CR>" end), desc = "Move to next buffer" },
   { "<S-Tab>", "<cmd>bp<CR>", desc = "Move to previous buffer" },
 
   { "<ESC>", "<cmd>noh<CR>", desc = "No Highlight" },
