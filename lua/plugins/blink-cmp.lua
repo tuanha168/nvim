2141052cf496ca587493cbc963c72f1f1bb9bdf9
yopaz-tuanha <tuanha@yopaz.vn>
2025-10-01 11:51:57 +0700

    Auto Update: 2025-10-01 11:51:57 from Arch Linux

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -135,9 +135,9 @@ return {
           function() -- sidekick next edit suggestion
             return require("sidekick").nes_jump_or_apply()
           end,
-          -- function() -- if you are using Neovim's native inline completions
-          --   return vim.lsp.inline_completion.get()
-          -- end,
+          function() -- if you are using Neovim's native inline completions
+            return vim.lsp.inline_completion.get()
+          end,
           "fallback",
         },
         ["<S-Tab>"] = { "select_prev", "fallback_to_mappings" },
--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -20,12 +20,12 @@ return {
           end
 
           -- if you are using Neovim's native inline completions
-          -- if vim.lsp.inline_completion.get() then return end
+          if vim.lsp.inline_completion.get() then return end
 
           -- any other things (like snippets) you want to do on <tab> go here.
 
           -- fall back to normal tab
-          return "<tab>"
+          -- return "<tab>"
         end,
         mode = { "i", "n" },
         expr = true,
