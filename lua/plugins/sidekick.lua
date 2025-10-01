8a81f982855e677621bc4111041766f63c2c8b05
yopaz-tuanha <tuanha@yopaz.vn>
2025-10-01 13:35:29 +0700

    Auto Update: 2025-10-01 13:35:29 from Arch Linux

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -29,7 +29,7 @@ return {
           end
           Print "heirline"
           local heirline, ok = require "heirline-components.buffer"
-          Print("ok " .. string(ok) .. " ")
+          Print("ok " .. tostring(ok) .. " ")
           if not ok then return "<Tab>" end
 
           heirline.nav(1)
