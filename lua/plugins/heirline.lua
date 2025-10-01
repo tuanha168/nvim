afbaa39350313c7c4dac747a31601b98fb98ce99
yopaz-tuanha <tuanha@yopaz.vn>
2025-10-01 11:55:57 +0700

    Auto Update: 2025-10-01 11:55:57 from Arch Linux

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -43,6 +43,22 @@ return {
         },
       }
 
+      local Sidekick = {
+        {
+          condition = function()
+            local status = require "sidekick.status"
+            return status.get() ~= nil
+          end,
+          provider = " ",
+          color = function()
+            local status = require("sidekick.status").get()
+            if status then
+              return status.kind == "Error" and "DiagnosticError" or status.busy and "DiagnosticWarn" or "Special"
+            end
+          end,
+        },
+      }
+
       return {
         opts = {
           disable_winbar_cb = function(args) -- We do this to avoid showing it on the greeter.
