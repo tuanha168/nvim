47ad5c77b479f316fc502d451dae868f23e9d688
yopaz-tuanha <tuanha@yopaz.vn>
2025-10-02 09:51:45 +0700

    Auto Update: 2025-10-02 09:51:44 from Arch Linux

--- lsp/vtsls.lua
+++ lsp/vtsls.lua
@@ -29,6 +29,17 @@ return {
   settings = {
     vtsls = {
       -- autoUseWorkspaceTsdk = true,
+      tsserver = {
+        globalPlugins = {
+          {
+            name = "@vue/typescript-plugin",
+            location = vim.fn.expand "$MASON/packages/vue-language-server/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin",
+            languages = { "vue" },
+            configNamespace = "typescript",
+            enableForWorkspaceTypeScriptVersions = true,
+          },
+        },
+      },
       experimental = { maxInlayHintLength = 20 },
     },
     typescript = {
