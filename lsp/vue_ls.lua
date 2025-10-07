320febbb9fca4b956971c33e983c54740db4439d
TuanHA <achicir2@gmail.com>
2025-10-07 10:29:43 +0700

    Auto Update: 2025-10-07 10:29:43 from Arch Linux

--- lsp/vue_ls.lua
+++ lsp/vue_ls.lua
@@ -1,8 +1,11 @@
 ---@module "vim.lsp.client"
 ---@class vim.lsp.ClientConfig
+---@type lspconfig.options.vuels
 return {
   root_markers = { ".git", "tsconfig.json", "tsconfig.ts", "tsconfig.js", "package.json" },
   settings = {
+    vetur = {
 
+    }
   }
 }
