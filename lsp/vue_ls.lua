a117faa4519ddbe8c112b07e96570e703d1f564d
TuanHA <achicir2@gmail.com>
2025-10-07 10:26:56 +0700

    Auto Update: 2025-10-07 10:26:55 from Arch Linux

--- lsp/vue_ls.lua
+++ lsp/vue_ls.lua
@@ -3,25 +3,5 @@ local home = os.getenv "HOME"
 return {
   root_markers = { ".git", "tsconfig.json", "tsconfig.ts", "tsconfig.js", "package.json" },
   settings = {
-    typescript = {
-      inlayHints = {
-        enumMemberValues = {
-          enabled = true,
-        },
-        functionLikeReturnTypes = {
-          enabled = true,
-        },
-        propertyDeclarationTypes = {
-          enabled = true,
-        },
-        parameterTypes = {
-          enabled = true,
-          suppressWhenArgumentMatchesName = true,
-        },
-        variableTypes = {
-          enabled = true,
-        },
-      },
-    },
   },
 }
