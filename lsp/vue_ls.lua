cf7304a113b592240f10366c2ceabda91fe73590
TuanHA <achicir2@gmail.com>
2025-10-07 10:27:56 +0700

    Auto Update: 2025-10-07 10:27:55 from Arch Linux

--- lsp/vue_ls.lua
+++ lsp/vue_ls.lua
@@ -3,5 +3,24 @@ local home = os.getenv "HOME"
 return {
   root_markers = { ".git", "tsconfig.json", "tsconfig.ts", "tsconfig.js", "package.json" },
   settings = {
+    vue = {
+      inlayHints = {
+        destructuredProps = {
+          enabled = true,
+        },
+        inlineHandlerLoading = {
+          enabled = true,
+        },
+        missingProps = {
+          enabled = true,
+        },
+        optionsWrapper = {
+          enabled = true,
+        },
+        vBindShorthand = {
+          enabled = true,
+        },
+      },
+    },
   },
 }
