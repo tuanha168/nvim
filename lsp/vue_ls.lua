739fd7a164873b79364366bffc9fba4f2e7233c5
TuanHA <achicir2@gmail.com>
2025-10-07 10:25:00 +0700

    Auto Update: 2025-10-07 10:25:00 from Arch Linux

--- lsp/vue_ls.lua
+++ lsp/vue_ls.lua
@@ -3,52 +3,24 @@ local home = os.getenv "HOME"
 return {
   root_markers = { ".git", "tsconfig.json", "tsconfig.ts", "tsconfig.js", "package.json" },
   settings = {
-    -- vue = {
-    --   inlayHints = {
-    --     missingProps = true,
-    --     optionsWrapper = true,
-    --     vBindShorthand = true,
-    --     destructuredProps = true,
-    --     inlineHandlerLeading = true,
-    --   },
-    -- },
     typescript = {
-      tsserver = {
-        web = {
-          projectWideIntellisense = {
-            enabled = "true",
-          },
-        },
-      },
       inlayHints = {
-        enumMemberValues = { enabled = "true" },
-        propertyDeclarationTypes = { enabled = "true" },
-        parameterNames = {
-          enabled = "all",
-          suppressWhenArgumentMatchesName = "true",
+        enumMemberValues = {
+          enabled = true,
         },
-        -- functionLikeReturnTypes = { enabled = "true" },
-        -- variableTypes = {
-        --   enabled = "true",
-        --   suppressWhenTypeMatchesName = "true",
-        -- },
-        parameterTypes = { enabled = "true" },
-      },
-    },
-    javascript = {
-      inlayHints = {
-        enumMemberValues = { enabled = "true" },
-        propertyDeclarationTypes = { enabled = "true" },
-        parameterNames = {
-          enabled = "all",
-          suppressWhenArgumentMatchesName = "true",
+        functionLikeReturnTypes = {
+          enabled = true,
         },
-        -- functionLikeReturnTypes = { enabled = "true" },
-        -- variableTypes = {
-        --   enabled = "true",
-        --   suppressWhenTypeMatchesName = "true",
-        -- },
-        parameterTypes = { enabled = "true" },
+        propertyDeclarationTypes = {
+          enabled = true,
+        },
+        parameterTypes = {
+          enabled = true,
+          suppressWhenArgumentMatchesName = true,
+        },
+        variableTypes = {
+          enabled = true,
+        },
       },
     },
   },
