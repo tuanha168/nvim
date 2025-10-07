7da0ca61f607619842cafecb4eea048f03b86f26
TuanHA <achicir2@gmail.com>
2025-10-07 10:32:22 +0700

    Auto Update: 2025-10-07 10:32:22 from Arch Linux

--- lsp/vue_ls.lua
+++ lsp/vue_ls.lua
@@ -1,8 +1,55 @@
----@module "vim.lsp.client"
----@class vim.lsp.ClientConfig
----@type lspconfig.options.vuels
+local home = os.getenv "HOME"
+
 return {
   root_markers = { ".git", "tsconfig.json", "tsconfig.ts", "tsconfig.js", "package.json" },
   settings = {
-  }
+    vue = {
+      inlayHints = {
+        missingProps = true,
+        optionsWrapper = true,
+        vBindShorthand = true,
+        destructuredProps = true,
+        inlineHandlerLeading = true,
+      },
+    },
+    typescript = {
+      tsserver = {
+        web = {
+          projectWideIntellisense = {
+            enabled = "true",
+          },
+        },
+      },
+      inlayHints = {
+        enumMemberValues = { enabled = "true" },
+        propertyDeclarationTypes = { enabled = "true" },
+        parameterNames = {
+          enabled = "all",
+          suppressWhenArgumentMatchesName = "true",
+        },
+        -- functionLikeReturnTypes = { enabled = "true" },
+        -- variableTypes = {
+        --   enabled = "true",
+        --   suppressWhenTypeMatchesName = "true",
+        -- },
+        parameterTypes = { enabled = "true" },
+      },
+    },
+    javascript = {
+      inlayHints = {
+        enumMemberValues = { enabled = "true" },
+        propertyDeclarationTypes = { enabled = "true" },
+        parameterNames = {
+          enabled = "all",
+          suppressWhenArgumentMatchesName = "true",
+        },
+        -- functionLikeReturnTypes = { enabled = "true" },
+        -- variableTypes = {
+        --   enabled = "true",
+        --   suppressWhenTypeMatchesName = "true",
+        -- },
+        parameterTypes = { enabled = "true" },
+      },
+    },
+  },
 }
