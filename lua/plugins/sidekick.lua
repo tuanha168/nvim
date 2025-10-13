e6e082780d85a975f2f33ea670159748265acaa9
yopaz-tuanha <tuanha@yopaz.vn>
2025-10-13 10:02:54 +0700

    Auto Update: 2025-10-13 10:02:54 from Arch Linux

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -20,7 +20,7 @@ return {
         function()
           -- if there is a next edit, jump to it, otherwise apply it if any
           if not require("sidekick").nes_jump_or_apply() then
-            return require("sidekick.nes").update()
+            return "<C-f>"
           end
         end,
         expr = true,
