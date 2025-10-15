86a986b6f89c6f7ff7d6e423a43912fc6fb93230
yopaz-tuanha <tuanha@yopaz.vn>
2025-10-15 15:03:53 +0700

    Auto Update: 2025-10-15 15:03:52 from Arch Linux

---
+++ lsp/eslint_d.lua
@@ -0,0 +1,23 @@
+---@module "vim.lsp.client"
+---@class vim.lsp.ClientConfig
+return {
+  -- filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "json", "svg" },
+  root_markers = {
+    ".eslintrc",
+    ".eslintrc.cjs",
+    ".eslintrc.js",
+    ".eslintrc.ts",
+    ".eslintrc.json",
+    ".eslintrc.yml",
+    "eslint.config.js",
+    "eslint.config.mjs",
+    "eslint.config.ts",
+  },
+  on_init = function(_client)
+    local old_on_attach = _client.on_attach
+    _client.on_attach = function(client, buf)
+      old_on_attach(client, buf)
+      vim.b[buf].formatByLsp = true
+    end
+  end,
+}
