11fda48fdeb87319cf39e277edbb565ffb410700
yopaz-tuanha <tuanha@yopaz.vn>
2025-10-15 17:09:56 +0700

    Auto Update: 2025-10-15 17:09:56 from Arch Linux

--- lua/plugins/lint_conform.lua
+++ lua/plugins/lint_conform.lua
@@ -30,11 +30,8 @@ return {
       },
       formatters_by_ft = {
         lua = { "stylua", "lua_ls" },
-        -- Conform will run multiple formatters sequentially
         python = { "isort", "black" },
-        -- You can customize some of the format options for the filetype (:help conform.format)
         rust = { "rustfmt" },
-        -- Conform will run the first available formatter
         javascript = { "prettier" },
         typescript = { "prettier" },
         vue = { "prettier" },
