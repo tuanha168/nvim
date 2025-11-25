f0318dde635549dbfd9d8d0b4d16aaf21514fda3
yopaz-tuanha <tuanha@yopaz.vn>
2025-11-25 11:48:39 +0700

    Auto Update: 2025-11-25 11:48:39 from Arch Linux

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -4,6 +4,7 @@ return {
   DiffAdd = { bg = "#085c24", fg = "none" },
   DiffText = { bg = "#085c24", fg = "none" },
   diffRemoved = { bg = "#7a1431", fg = "none" },
+
   -- DiffChange = { bg = "#576b8a", fg = "none" },
   DiffDelete = { bg = "#7a1431", fg = "none" },
   ComplHint = { fg = "#b6dee3" },
