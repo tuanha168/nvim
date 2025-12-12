8406c4db67d1f8049794c3dccd36186cde07a1a5
yopaz-tuanha <tuanha@yopaz.vn>
2025-12-12 16:17:37 +0700

    Auto Update: 2025-12-12 16:17:36 from Arch Linux

--- lua/plugins/lint_conform.lua
+++ lua/plugins/lint_conform.lua
@@ -43,12 +43,38 @@ return {
       formatters = {
         prettier_json = {
           command = "prettier",
-          args = {
-            "--stdin-filepath",
-            "$FILENAME",
-            "--parser",
-            "json",
-          },
+          args = function()
+            local cwd = vim.fn.getcwd()
+            local prettierrc_files = {
+              ".prettierrc",
+              ".prettierrc.json",
+              ".prettierrc.js",
+              ".prettierrc.yaml",
+              ".prettierrc.yml",
+              "prettier.config.js",
+            }
+
+            -- Check if any prettierrc file exists
+            local has_config = false
+            for _, file in ipairs(prettierrc_files) do
+              if vim.fn.filereadable(cwd .. "/" .. file) == 1 then
+                has_config = true
+                break
+              end
+            end
+
+            local args = { "--stdin-filepath", "$FILENAME" }
+
+            -- If no config file, add fallback options
+            if not has_config then
+              vim.list_extend(args, {
+                "--parser",
+                "json",
+              })
+            end
+
+            return args
+          end,
           stdin = true,
         },
       },
