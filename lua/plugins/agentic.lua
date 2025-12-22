1cdae12bc6b50ca1c7559b44f76588cb64bf48e8
yopaz-tuanha <tuanha@yopaz.vn>
2025-12-22 16:13:51 +0700

    Auto Update: 2025-12-22 16:13:51 from Arch Linux

---
+++ lua/plugins/agentic.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "carlos-algms/agentic.nvim",
+
+    event = "VeryLazy",
+
+    opts = {
+      -- Available by default: "claude-acp" | "gemini-acp" | "codex-acp" | "opencode-acp" | "cursor-acp"
+      provider = "claude-acp", -- setting the name here is all you need to get started
+    },
+
+    -- these are just suggested keymaps; customize as desired
+    keys = {
+      {
+        "<Leader>cc",
+        function() require("agentic").toggle() end,
+        mode = { "n", "v", "i" },
+        desc = "Toggle Agentic Chat",
+      },
+      {
+        "<C-'>",
+        function() require("agentic").add_selection_or_file_to_context() end,
+        mode = { "n", "v" },
+        desc = "Add file or selection to Agentic to Context",
+      },
+      {
+        "<C-,>",
+        function() require("agentic").new_session() end,
+        mode = { "n", "v", "i" },
+        desc = "New Agentic Session",
+      },
+    },
+  },
+}
