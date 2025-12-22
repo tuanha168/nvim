3a4872f32b9f20f758531b986b7567854fbe2603
yopaz-tuanha <tuanha@yopaz.vn>
2025-12-22 17:54:12 +0700

    Auto Update: 2025-12-22 17:54:11 from Arch Linux

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -21,9 +21,6 @@ return {
         chat = {
           adapter = "opencode",
         },
-        title_generation_opts = {
-          adapter = { name = "copilot", model = "gpt-4o" },
-        },
       },
       extensions = {
         history = {
@@ -51,9 +48,9 @@ return {
             auto_generate_title = true,
             title_generation_opts = {
               ---Adapter for generating titles (defaults to current chat adapter)
-              adapter = nil, -- "copilot"
+              adapter = "copilot",
               ---Model for generating titles (defaults to current chat model)
-              model = nil, -- "gpt-4o"
+              model = "gpt-4o",
               ---Number of user prompts after which to refresh the title (0 to disable)
               refresh_every_n_prompts = 0, -- e.g., 3 to refresh after every 3rd user prompt
               ---Maximum number of times to refresh the title (default: 3)
