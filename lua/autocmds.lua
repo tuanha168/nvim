abd51e6686262b238bcca05eada629874448eb32
yopaz-tuanha <tuanha@yopaz.vn>
2025-12-26 13:29:41 +0700

    Auto Update: 2025-12-26 13:29:41 from Arch Linux

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -36,8 +36,6 @@ return {
         local win = vim.api.nvim_get_current_win()
         vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
       end
-
-       Chiruno.func.vueCheck(client, e.buf)
     end,
   },
   {
