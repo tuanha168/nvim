856686693e281a6d29e5d0301f8b92883ccc2e75
yopaz-tuanha <tuanha@yopaz.vn>
2025-12-26 13:22:13 +0700

    Auto Update: 2025-12-26 13:22:13 from Arch Linux

--- lua/Chiruno/func/swap_to_vtsls.lua
+++ lua/Chiruno/func/swap_to_vtsls.lua
@@ -4,13 +4,12 @@ function Chiruno.func.swapToVtsls(client, buf)
   -- Use vim.fs.root to find root directory by searching for markers from current file outward
   local root_dir = vim.fs.root(buf or 0, { "package.json", ".git" })
   if not root_dir then return end
-  Print("Root dir: " .. root_dir)
 
   local package_json = vim.fs.joinpath(root_dir, "package.json")
   local f = io.open(package_json, "r")
   if not f then
     vim.lsp.enable("tsgo", true)
-    vim.lsp.enable("vtsls", false)
+    vim.lsp.buf_detach_client(buf,vim.lsp.get_clients({ name = "vtsls" })[1].id)
     return "tsgo"
   end
 
@@ -20,7 +19,7 @@ function Chiruno.func.swapToVtsls(client, buf)
   local ok, package_data = pcall(vim.fn.json_decode, content)
   if not ok then
     vim.lsp.enable("tsgo", true)
-    vim.lsp.enable("vtsls", false)
+    vim.lsp.buf_detach_client(buf,vim.lsp.get_clients({ name = "vtsls" })[1].id)
     return "tsgo"
   end
 
@@ -28,13 +27,12 @@ function Chiruno.func.swapToVtsls(client, buf)
     or (package_data.devDependencies and (package_data.devDependencies.vue or package_data.devDependencies.nuxt))
   if not has_vue then
     vim.lsp.enable("tsgo", true)
-    vim.lsp.enable("vtsls", false)
+    vim.lsp.buf_detach_client(buf,vim.lsp.get_clients({ name = "vtsls" })[1].id)
     return "tsgo"
   end
 
   if client.name == "tsgo" then
-    vim.lsp.stop_client(vim.lsp.get_clients({ name = "tsgo" })[1].id, true)
-    vim.lsp.enable("tsgo", false)
+    vim.lsp.buf_detach_client(buf,vim.lsp.get_clients({ name = "tsgo" })[1].id)
   end
 
   vim.lsp.enable("vtsls", true)
