172d10e9bdccd6f4a50735ca3f1bb2356077380e
yopaz-tuanha <tuanha@yopaz.vn>
2025-12-30 14:57:24 +0700

    Auto Update: 2025-12-30 14:57:24 from Arch Linux

--- lua/plugins/git-conflict.lua
+++ lua/plugins/git-conflict.lua
@@ -1,32 +1,34 @@
 return {
-  "akinsho/git-conflict.nvim",
-  enabled = false,
-  event = "BufRead",
-  opts = {
-    default_mappings = {
-      ours = "cc",
-      theirs = "ic",
-      none = "dd",
-      both = "<Enter>",
-      next = "n",
-      prev = "N",
+  {
+    "akinsho/git-conflict.nvim",
+    enabled = false,
+    event = "BufRead",
+    opts = {
+      default_mappings = {
+        ours = "cc",
+        theirs = "ic",
+        none = "dd",
+        both = "<Enter>",
+        next = "n",
+        prev = "N",
+      },
+      disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
+      highlights = { -- They must have background color, otherwise the default color will be used
+        incoming = "DiffDelete",
+        current = "DiffText",
+      },
     },
-    disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
-    highlights = { -- They must have background color, otherwise the default color will be used
-      incoming = "DiffDelete",
-      current = "DiffText",
-    },
-  },
-  config = function(_, opts)
-    require("git-conflict").setup(opts)
+    config = function(_, opts)
+      require("git-conflict").setup(opts)
 
-    vim.api.nvim_create_autocmd({ "FocusGained", "BufReadPost" }, {
-      callback = function()
-        vim.cmd "checktime"
-        local ok, _ = pcall(require, "git-conflict")
-        if not ok then return end
-        vim.cmd "GitConflictRefresh"
-      end,
-    })
-  end,
+      vim.api.nvim_create_autocmd({ "FocusGained", "BufReadPost" }, {
+        callback = function()
+          vim.cmd "checktime"
+          local ok, _ = pcall(require, "git-conflict")
+          if not ok then return end
+          vim.cmd "GitConflictRefresh"
+        end,
+      })
+    end,
+  }
 }
