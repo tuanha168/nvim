87332bfeabd45f9bde6d3fce1693b5a2ce6bedf6
yopaz-tuanha <tuanha@yopaz.vn>
2026-01-10 17:32:03 +0700

    Auto Update: 2026-01-10 17:32:03 from Arch Linux

---
+++ lua/plugins/codediff.lua
@@ -0,0 +1,8 @@
+---@type LazySpec
+return {
+  {
+    "esmuellert/codediff.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    cmd = "CodeDiff",
+  },
+}
--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,5 +1,7 @@
+---@type LazySpec
 return {
   {
     "sindrets/diffview.nvim",
+    enabled = false,
   },
 }
