4faa639420722b70d8310bdc801d5431217b9caf
yopaz-tuanha <tuanha@yopaz.vn>
2026-01-21 18:06:29 +0700

    Auto Update: 2026-01-21 18:06:29 from Arch Linux

--- lsp/copilot.lua
+++ lsp/copilot.lua
@@ -3,4 +3,11 @@
 return {
   root_markers = { "tsconfig.json", "tsconfig.ts", "tsconfig.js", "package.json", ".git" },
   cmd = { "mise", "exec", "node@22", "--", "copilot-language-server", "--stdio" },
+  settings = {
+    github = {
+      copilot = {
+        selectedCompletionModel = "gpt-5",
+      },
+    },
+  },
 }
