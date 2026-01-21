6064f73da2bc17cd498f14dd604480e6d412adf1
yopaz-tuanha <tuanha@yopaz.vn>
2026-01-21 18:22:08 +0700

    Auto Update: 2026-01-21 18:22:08 from Arch Linux

--- lsp/copilot.lua
+++ lsp/copilot.lua
@@ -3,9 +3,6 @@
 return {
   root_markers = { "tsconfig.json", "tsconfig.ts", "tsconfig.js", "package.json", ".git" },
   cmd = { "mise", "exec", "node@22", "--", "copilot-language-server", "--stdio" },
-  init_options = {
-    copilotIntegrationId = "vscode-chat",
-  },
   settings = {
     github = {
       copilot = {
