0fd3c41fc1fdd92c7de68c891fe01038b7e6fb45
yopaz-tuanha <tuanha@yopaz.vn>
2026-03-09 14:55:53 +0700

    Auto Update: 2026-03-09 14:55:53 from Arch Linux

--- lsp/eslint.lua
+++ lsp/eslint.lua
@@ -12,12 +12,5 @@ return {
     "eslint.config.js",
     "eslint.config.mjs",
     "eslint.config.ts",
-  },
-  on_init = function(_client)
-    local old_on_attach = _client.on_attach
-    _client.on_attach = function(client, buf)
-      old_on_attach(client, buf)
-      vim.b[buf].formatByLsp = true
-    end
-  end,
+  }
 }
