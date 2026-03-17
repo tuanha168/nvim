4062b8e3961a4fe2ce620120d22129220f7291b1
yopaz-tuanha <tuanha@yopaz.vn>
2026-03-17 09:07:09 +0700

    Auto Update: 2026-03-17 09:07:09 from Arch Linux

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -134,4 +134,13 @@ return {
     "RRethy/vim-illuminate",
     event = "BufRead",
   },
+
+  {
+    'mawkler/demicolon.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    opts = {}
+  }
 }
