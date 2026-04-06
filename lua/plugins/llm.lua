62a5f7999019cdaec36904a58f274ce63f98636d
yopaz-tuanha <tuanha@yopaz.vn>
2026-04-06 15:17:46 +0700

    Auto Update: 2026-04-06 15:17:46 from Arch Linux

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -4,8 +4,8 @@ return {
     'huggingface/llm.nvim',
     opts = {
       model = "codellama:7b",
+      backend = "huggingface",        -- backend ID, "huggingface" | "ollama" | "openai" | "tgi"
       url = "http://localhost:11434", -- llm-ls uses "/api/generate"
-      -- cf https://github.com/ollama/ollama/blob/main/docs/api.md#parameters
       request_body = {
         -- Modelfile options for the model you use
         options = {
