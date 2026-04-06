66b37929350fce2d0a75670eb5f854495f9e652e
yopaz-tuanha <tuanha@yopaz.vn>
2026-04-06 18:11:56 +0700

    Auto Update: 2026-04-06 18:11:55 from Arch Linux

--- lua/plugins/minuet.lua
+++ lua/plugins/minuet.lua
@@ -17,7 +17,7 @@ return {
           api_key = 'TERM',
           name = 'Ollama',
           end_point = 'http://localhost:11434/v1/completions',
-          model = 'qwen2.5-coder:0.5b',
+          model = 'qwen2.5-coder:14b',
           optional = {
             max_tokens = 56,
             top_p = 0.9,
