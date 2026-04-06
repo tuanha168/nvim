6d9dc258046e51d74bf817ed3a88dbd0a0c43b92
yopaz-tuanha <tuanha@yopaz.vn>
2026-04-06 16:35:53 +0700

    Auto Update: 2026-04-06 16:35:53 from Arch Linux

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -1,32 +1,21 @@
 ---@type LazySpec
 return {
   {
-    'huggingface/llm.nvim',
+    'meeehdi-dev/bropilot.nvim',
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
     opts = {
-      backend = "ollama",             -- backend ID, "huggingface" | "ollama" | "openai" | "tgi"
-      -- model = "qwen2.5-coder:7b",
+      provider = "ollama",
+      auto_suggest = true,
+      excluded_filetypes = {},
       model = "qwen2.5-coder:14b",
-      url = "http://localhost:11434", -- llm-ls uses "/api/generate"
-      -- cf https://github.com/ollama/ollama/blob/main/docs/api.md#parameters
-      request_body = {
-        -- Modelfile options for the model you use
-        options = {
-          temperature = 0.2,
-          top_p = 0.95,
-        }
+      keymap = {
+        accept_word = "<C-f>",
+        accept_line = "<C-g>",
+        accept_block = "<Tab>",
+        suggest = "<C-Down>",
       },
-      fim = {
-        enabled = true,
-        prefix = "<PRE> ",
-        middle = " <MID>",
-        suffix = " <SUF>",
-      },
-      debounce_ms = 150,
-      accept_keymap = "<Tab>",
-      dismiss_keymap = "<S-Tab>",
-      context_window = 1024,             -- max number of tokens for the context window
-      enable_suggestions_on_startup = true,
-      enable_suggestions_on_files = "*", -- pattern matching syntax to enable suggestions on specific files, either a string or a list of strings
     },
   },
 }
