c37826693242dda298c6466d1e0ca7f785e02e50
yopaz-tuanha <tuanha@yopaz.vn>
2026-04-06 10:11:52 +0700

    Auto Update: 2026-04-06 10:11:52 from Arch Linux

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -97,15 +97,15 @@ return {
 
   { "<ESC>", "<cmd>noh<CR>", desc = "No Highlight" },
 
-  { "<C-h>", function() require("smart-splits").move_cursor_left() end },
-  { "<C-j>", function() require("smart-splits").move_cursor_down() end },
-  { "<C-k>", function() require("smart-splits").move_cursor_up() end },
-  { "<C-l>", function() require("smart-splits").move_cursor_right() end },
+  { "<C-h>", function() require("smart-splits").move_cursor_left() end, mode = { "n", "t" } },
+  { "<C-j>", function() require("smart-splits").move_cursor_down() end, mode = { "n", "t" } },
+  { "<C-k>", function() require("smart-splits").move_cursor_up() end, mode = { "n", "t" } },
+  { "<C-l>", function() require("smart-splits").move_cursor_right() end, mode = { "n", "t" } },
 
-  { "<A-h>", function() require("smart-splits").resize_left() end },
-  { "<A-j>", function() require("smart-splits").resize_down() end },
-  { "<A-k>", function() require("smart-splits").resize_up() end },
-  { "<A-l>", function() require("smart-splits").resize_right() end },
+  { "<A-h>", function() require("smart-splits").resize_left() end, mode = { "n", "t" } },
+  { "<A-j>", function() require("smart-splits").resize_down() end, mode = { "n", "t" } },
+  { "<A-k>", function() require("smart-splits").resize_up() end, mode = { "n", "t" } },
+  { "<A-l>", function() require("smart-splits").resize_right() end, mode = { "n", "t" } },
 
   { "/", "<Esc>/\\%V", desc = "Search in visual mode", mode = { "x" } },
   { "?", "<Esc>?\\%V", desc = "Search in visual mode", mode = { "x" } },
--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -93,24 +93,6 @@ return {
             require('opencode.terminal').setup(win.win)
           end,
         },
-        keys = {
-          term_normal = {
-            "<C-v>",
-            function(self)
-              self.esc_timer = self.esc_timer or (vim.uv or vim.loop).new_timer()
-              if self.esc_timer:is_active() then
-                self.esc_timer:stop()
-                vim.cmd("stopinsert")
-              else
-                self.esc_timer:start(200, 0, function() end)
-                return "<C-v>"
-              end
-            end,
-            mode = "t",
-            expr = true,
-            desc = "Double escape to normal mode",
-          },
-        }
       }
       ---@type opencode.Opts
       vim.g.opencode_opts = {
