c90efbf3905a0360832937428a808c796f5b24b1
yopaz-tuanha <tuanha@yopaz.vn>
2026-04-06 15:48:33 +0700

    Auto Update: 2026-04-06 15:48:33 from Arch Linux

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -3,17 +3,26 @@ return {
   {
     'huggingface/llm.nvim',
     opts = {
-      backend = "ollama",               -- backend ID, "huggingface" | "ollama" | "openai" | "tgi"
+      backend = "ollama",             -- backend ID, "huggingface" | "ollama" | "openai" | "tgi"
       model = "codellama:7b",
-      url = "http://localhost:11434",   -- llm-ls uses "/api/generate"
+      url = "http://localhost:11434", -- llm-ls uses "/api/generate"
       -- cf https://github.com/ollama/ollama/blob/main/docs/api.md#parameters
       request_body = {
         -- Modelfile options for the model you use
         options = {
           temperature = 0.2,
           top_p = 0.95,
+        },
+        headers = {
+
         }
-      }
-    }
+      },
+      debounce_ms = 150,
+      accept_keymap = "<Tab>",
+      dismiss_keymap = "<S-Tab>",
+      context_window = 1024,         -- max number of tokens for the context window
+      enable_suggestions_on_startup = true,
+      enable_suggestions_on_files = "*", -- pattern matching syntax to enable suggestions on specific files, either a string or a list of strings
+    },
   },
 }
