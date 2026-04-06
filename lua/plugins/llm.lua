362559e1271d0eec8c8cae459e723fd0b05da51e
yopaz-tuanha <tuanha@yopaz.vn>
2026-04-06 15:17:08 +0700

    Auto Update: 2026-04-06 15:17:07 from Arch Linux

---
+++ lua/plugins/llm.lua
@@ -0,0 +1,18 @@
+---@type LazySpec
+return {
+  {
+    'huggingface/llm.nvim',
+    opts = {
+      model = "codellama:7b",
+      url = "http://localhost:11434", -- llm-ls uses "/api/generate"
+      -- cf https://github.com/ollama/ollama/blob/main/docs/api.md#parameters
+      request_body = {
+        -- Modelfile options for the model you use
+        options = {
+          temperature = 0.2,
+          top_p = 0.95,
+        }
+      }
+    }
+  },
+}
