6d78399a7ccafe8742fbc7bddbd824151c770d17
yopaz-tuanha <tuanha@yopaz.vn>
2026-04-06 18:25:22 +0700

    Auto Update: 2026-04-06 18:25:22 from Arch Linux

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -190,7 +190,7 @@ return {
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
+        default = { "minuet", "lsp", "path", "snippets", "buffer" },
         providers = {
           minuet = {
             name = 'minuet',
