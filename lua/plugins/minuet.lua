0c47683bbd26730a9cf8d7d548709d95158ebc15
yopaz-tuanha <tuanha@yopaz.vn>
2026-04-06 18:46:16 +0700

    Auto Update: 2026-04-06 18:46:16 from Arch Linux

--- lua/plugins/minuet.lua
+++ lua/plugins/minuet.lua
@@ -10,9 +10,9 @@ return {
       -- power. Once you have a reliable estimate of your local computing power,
       -- you should adjust the context window to a larger value.
       context_window = 512,
-      context_ratio = 0.6,
-      throttle = 5000,
-      debounce = 4000,
+      context_ratio = 0.7,
+      throttle = 1500,
+      debounce = 800,
       blink = {
         enable_auto_complete = false,
       },
