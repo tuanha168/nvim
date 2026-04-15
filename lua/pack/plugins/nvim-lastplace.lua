52e666b4e2b2448fe27733ef3927aa722cac9ce4
yopaz-tuanha <tuanha@yopaz.vn>
2026-04-15 15:39:30 +0700

    Auto Update: 2026-04-15 15:39:30 from Arch Linux

--- lua/pack/plugins/nvim-lastplace.lua
+++ lua/pack/plugins/nvim-lastplace.lua
@@ -4,10 +4,12 @@ lazy.on_event(
   "https://github.com/ethanholz/nvim-lastplace",
   "VimEnter",
   function()
-    require("nvim-lastplace").setup {
-      lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
-      lastplace_ignore_filetype = { "gitcommit", "gitrebase", "svn", "hgcommit" },
-      lastplace_open_folds = true,
-    }
+    vim.schedule(function()
+      require("nvim-lastplace").setup {
+        lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+        lastplace_ignore_filetype = { "gitcommit", "gitrebase", "svn", "hgcommit" },
+        lastplace_open_folds = true,
+      }
+    end)
   end
 )
