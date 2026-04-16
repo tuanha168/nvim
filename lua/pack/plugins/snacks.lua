87ab9c331cafa55064c4ea35d02b76a07e87c188
yopaz-tuanha <tuanha@yopaz.vn>
2026-04-16 17:42:01 +0700

    Auto Update: 2026-04-16 17:42:00 from Arch Linux

--- lua/pack/plugins/snacks.lua
+++ lua/pack/plugins/snacks.lua
@@ -31,25 +31,27 @@ local opts = {
     },
   },
   gitbrowse = {
-    remote_patterns = {
-      { "^git@yopaz:(.+)$",                   "https://github.com/%1" },
-      { "^git@phinx:(.+)$",                   "https://github.com/%1" },
-      { "^git@yopaz*-liberty:(.+)$",          "https://github.com/%1" },
+    config = function(opts, defaults)
+      table.insert(opts.remote_patterns, {
+        { "^git@yopaz:(.+)$",                   "https://github.com/%1" },
+        { "^git@phinx:(.+)$",                   "https://github.com/%1" },
+        { "^git@yopaz*-liberty:(.+)$",          "https://github.com/%1" },
 
-      { "^(https?://.*)%.git$",               "%1" },
-      { "^git@(.+):(.+)%.git$",               "https://%1/%2" },
-      { "^git@(.+):(.+)$",                    "https://%1/%2" },
-      { "^git@(.+)/(.+)$",                    "https://%1/%2" },
-      { "^org%-%d+@(.+):(.+)%.git$",          "https://%1/%2" },
-      { "^ssh://git@(.*)$",                   "https://%1" },
-      { "^ssh://([^:/]+)(:%d+)/(.*)$",        "https://%1/%3" },
-      { "^ssh://([^/]+)/(.*)$",               "https://%1/%2" },
-      { "ssh%.dev%.azure%.com/v3/(.*)/(.*)$", "dev.azure.com/%1/_git/%2" },
-      { "^https://%w*@(.*)",                  "https://%1" },
-      { "^git@(.*)",                          "https://%1" },
-      { ":%d+",                               "" },
-      { "%.git$",                             "" },
-    },
+        { "^(https?://.*)%.git$",               "%1" },
+        { "^git@(.+):(.+)%.git$",               "https://%1/%2" },
+        { "^git@(.+):(.+)$",                    "https://%1/%2" },
+        { "^git@(.+)/(.+)$",                    "https://%1/%2" },
+        { "^org%-%d+@(.+):(.+)%.git$",          "https://%1/%2" },
+        { "^ssh://git@(.*)$",                   "https://%1" },
+        { "^ssh://([^:/]+)(:%d+)/(.*)$",        "https://%1/%3" },
+        { "^ssh://([^/]+)/(.*)$",               "https://%1/%2" },
+        { "ssh%.dev%.azure%.com/v3/(.*)/(.*)$", "dev.azure.com/%1/_git/%2" },
+        { "^https://%w*@(.*)",                  "https://%1" },
+        { "^git@(.*)",                          "https://%1" },
+        { ":%d+",                               "" },
+        { "%.git$",                             "" },
+      })
+    end
   },
   zen = {
     toggles = {
