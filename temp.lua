69d0b255a073e48f97671aadea9ad260744d5db4
yopaz-tuanha <tuanha@yopaz.vn>
2026-04-16 18:01:30 +0700

    Auto Update: 2026-04-16 18:01:30 from Arch Linux

--- temp.lua
+++ temp.lua
@@ -1,3 +1,2 @@
 local a = "git@yopaz:myhm-inc/knot-builder-web.git"
-a:gsub("^git@yopaz:(.+)%.git$","https://github.com/%1")
-print(a)
+print(a:gsub("^git@yopaz:(.+)%.git$","https://github.com/%1"))
