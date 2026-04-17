f83c45904f6616860040431e49227501e650caaa
yopaz-tuanha <tuanha@yopaz.vn>
2026-04-17 11:20:00 +0700

    Auto Update: 2026-04-17 11:19:59 from Arch Linux

--- lua/Chiruno/func/vue_check.lua
+++ lua/Chiruno/func/vue_check.lua
@@ -3,7 +3,6 @@ function Chiruno.func.vueCheck(client, buf)
 
   -- Use vim.fs.root to find root directory by searching for markers from current file outward
   local root_dir = vim.fs.root(buf or 0, { "package.json", ".git" })
-  Print(root_dir)
   if not root_dir then return "tsgo" end
 
   local package_json = vim.fs.joinpath(root_dir, "package.json")
@@ -16,8 +15,14 @@ function Chiruno.func.vueCheck(client, buf)
   local ok, package_data = pcall(vim.fn.json_decode, content)
   if not ok then return "tsgo" end
 
-  local has_vue = (package_data.dependencies and (package_data.dependencies.vue or package_data.dependencies.nuxt))
-    or (package_data.devDependencies and (package_data.devDependencies.vue or package_data.devDependencies.nuxt))
+  local function has_vue_pkg(deps)
+    if not deps then return false end
+    for name in pairs(deps) do
+      if name:find("vue", 1, true) or name:find("nuxt", 1, true) then return true end
+    end
+    return false
+  end
+  local has_vue = has_vue_pkg(package_data.dependencies) or has_vue_pkg(package_data.devDependencies)
 
   if not has_vue then return "tsgo" end
 
