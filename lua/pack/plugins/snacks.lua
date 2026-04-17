985dfc0418843f684f5f158c7c94f1ccc424b97a
yopaz-tuanha <tuanha@yopaz.vn>
2026-04-17 10:04:54 +0700

    Auto Update: 2026-04-17 10:04:54 from Arch Linux

--- lua/pack/plugins/snacks.lua
+++ lua/pack/plugins/snacks.lua
@@ -143,9 +143,23 @@ local keys = {
             line_start = selection.startRow,
             line_end = selection.finishRow,
             remote_patterns = {
-              { "^git@yopaz:(.+)$",          "https://github.com/%1" },
-              { "^git@phinx:(.+)$",          "https://github.com/%1" },
-              { "^git@yopaz*-liberty:(.+)$", "https://github.com/%1" },
+              { "^git@yopaz:(.+)$",                   "https://github.com/%1" },
+              { "^git@phinx:(.+)$",                   "https://github.com/%1" },
+              { "^git@yopaz*-liberty:(.+)$",          "https://github.com/%1" },
+
+              { "^(https?://.*)%.git$",               "%1" },
+              { "^git@(.+):(.+)%.git$",               "https://%1/%2" },
+              { "^git@(.+):(.+)$",                    "https://%1/%2" },
+              { "^git@(.+)/(.+)$",                    "https://%1/%2" },
+              { "^org%-%d+@(.+):(.+)%.git$",          "https://%1/%2" },
+              { "^ssh://git@(.*)$",                   "https://%1" },
+              { "^ssh://([^:/]+)(:%d+)/(.*)$",        "https://%1/%3" },
+              { "^ssh://([^/]+)/(.*)$",               "https://%1/%2" },
+              { "ssh%.dev%.azure%.com/v3/(.*)/(.*)$", "dev.azure.com/%1/_git/%2" },
+              { "^https://%w*@(.*)",                  "https://%1" },
+              { "^git@(.*)",                          "https://%1" },
+              { ":%d+",                               "" },
+              { "%.git$",                             "" },
             },
           }
         end)
