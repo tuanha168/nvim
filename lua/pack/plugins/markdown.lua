eaff1be6d647ca4d9d44cb79e00aefb63d248f65
yopaz-tuanha <tuanha@yopaz.vn>
2026-05-07 17:55:39 +0700

    Auto Update: 2026-05-07 17:55:39 from Arch Linux

--- lua/pack/plugins/markdown.lua
+++ lua/pack/plugins/markdown.lua
@@ -1,7 +1,22 @@
-  -- {
-  --   "iamcco/markdown-preview.nvim",
-  --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  --   build = "cd app && yarn install",
-  --   init = function() vim.g.mkdp_filetypes = { "markdown" } end,
-  --   ft = { "markdown" },
-  -- },
+local lazy = require("pack.lazy-load")
+
+lazy.on_event(
+  {
+    "https://github.com/MeanderingProgrammer/render-markdown.nvim",
+    "https://github.com/iamcco/markdown-preview.nvim",
+  },
+  "FileType",
+  function()
+    require("render-markdown").setup({
+      file_types = { "markdown" },
+    })
+
+    vim.g.mkdp_filetypes = { "markdown" }
+    vim.g.mkdp_auto_close = 1
+  end,
+  { pattern = "markdown" }
+)
+
+require("which-key").add({
+  { "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview Toggle" },
+})
