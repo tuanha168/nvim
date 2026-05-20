3217d888e2c703d6c14357f1555cc0a289050de9
yopaz-tuanha <tuanha@yopaz.vn>
2026-05-20 13:03:37 +0700

    Auto Update: 2026-05-20 13:03:37 from Arch Linux

--- lua/pack/plugins/csvview.lua
+++ lua/pack/plugins/csvview.lua
@@ -1,14 +1,10 @@
 local lazy = require("pack.lazy-load")
 
-vim.pack.add { "hat0uma/csvview.nvim" }
-
 lazy.on_event(
-  "https://github.com/toppair/peek.nvim",
+  "https://github.com/hat0uma/csvview.nvim",
   "FileType",
   function()
-    require("peek").setup()
-    vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-    vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    require("csvview").setup()
   end,
-  { pattern = "markdown" }
+  { pattern = "csv" }
 )
