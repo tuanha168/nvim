afffa055715e23c9913d7bb58799a934f4af0afb
yopaz-tuanha <tuanha@yopaz.vn>
2026-05-21 16:50:53 +0700

    Auto Update: 2026-05-21 16:50:53 from Arch Linux

--- lua/config/opencode_ensure.lua
+++ lua/config/opencode_ensure.lua
@@ -39,7 +39,7 @@ local function find_opencode_pid_in_window()
   return opencode_pid
 end
 
-local function is_connected() return require("sidekick.cli.session").attached()[1] ~= nil end
+local function is_connected() return next(require("sidekick.cli.session").attached()) ~= nil end
 
 local function wait_for_connected_server(timeout)
   return vim.wait(timeout, function() return is_connected() end, 100)
