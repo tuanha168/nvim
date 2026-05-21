56d6417a90c14e71e901d808b063792efd271dfb
yopaz-tuanha <tuanha@yopaz.vn>
2026-05-21 14:49:54 +0700

    Auto Update: 2026-05-21 14:49:54 from Arch Linux

--- lua/pack/plugins/cursortab.lua
+++ lua/pack/plugins/cursortab.lua
@@ -4,7 +4,7 @@ vim.api.nvim_create_autocmd("PackChanged", {
   callback = function(ev)
     local name, kind = ev.data.spec.name, ev.data.kind
     if name == "cursortab.nvim" and (kind == "install" or kind == "update") then
-      vim.system({ "cd", "server", "&&", "go", "build" }, { cwd = ev.data.path })
+      vim.system({ "go", "build" }, { cwd = ev.data.path + "/server" })
     end
   end,
 })
@@ -13,7 +13,7 @@ lazy.on_event("https://github.com/cursortab/cursortab.nvim", "BufRead", function
   vim.api.nvim_set_hl(0, "CursorTabAddition", { bg = "#1a3a1a" })
   require("cursortab").setup {
     provider = {
-      type = "copilot"
+      type = "copilot",
       -- type = "zeta-2",
       -- url = "http://localhost:8000",
     },
