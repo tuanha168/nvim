6e8a1687229b3c51590485dd5604bcb56c9af4e3
yopaz-tuanha <tuanha@yopaz.vn>
2026-05-21 14:47:34 +0700

    Auto Update: 2026-05-21 14:47:34 from Arch Linux

--- lua/pack/plugins/cursortab.lua
+++ lua/pack/plugins/cursortab.lua
@@ -1,25 +1,21 @@
-local lazy = require("pack.lazy-load")
+local lazy = require "pack.lazy-load"
 
 vim.api.nvim_create_autocmd("PackChanged", {
   callback = function(ev)
     local name, kind = ev.data.spec.name, ev.data.kind
-    if name == "peek.nvim" and (kind == "install" or kind == "update") then
-      vim.system({ "deno", "task", "--quiet", "build:fast" }, { cwd = ev.data.path })
+    if name == "cursortab.nvim" and (kind == "install" or kind == "update") then
+      vim.system({ "cd", "server", "&&", "go", "build" }, { cwd = ev.data.path })
     end
   end,
 })
 
-lazy.on_event(
-  "https://github.com/toppair/peek.nvim",
-  "FileType",
-  function()
-    require("peek").setup()
-    vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-    vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-  end,
-  { pattern = "markdown" }
-)
-
-require("which-key").add({
-  { "<leader>mp", "<cmd>PeekOpen<cr>", desc = "Markdown Preview Open" },
-})
+lazy.on_event("https://github.com/cursortab/cursortab.nvim", "BufRead", function()
+  vim.api.nvim_set_hl(0, "CursorTabAddition", { bg = "#1a3a1a" })
+  require("cursortab").setup {
+    provider = {
+      type = "copilot"
+      -- type = "zeta-2",
+      -- url = "http://localhost:8000",
+    },
+  }
+end)
