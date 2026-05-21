e4b0d6ce982ea89a940812f42f1d9d1681fce28c
yopaz-tuanha <tuanha@yopaz.vn>
2026-05-21 15:34:55 +0700

    Auto Update: 2026-05-21 15:34:55 from Arch Linux

--- lua/pack/plugins/cursortab.lua
+++ lua/pack/plugins/cursortab.lua
@@ -1,38 +1,30 @@
-local lazy = require "pack.lazy-load"
+-- local lazy = require "pack.lazy-load"
 
-vim.api.nvim_create_autocmd("PackChanged", {
-  callback = function(ev)
-    local name, kind = ev.data.spec.name, ev.data.kind
-    if name == "cursortab.nvim" and (kind == "install" or kind == "update") then
-      Print(ev.data.path .. "/server")
-      vim.system({ "go", "build" }, { cwd = ev.data.path .. "/server" })
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd("PackChanged", {
+--   callback = function(ev)
+--     local name, kind = ev.data.spec.name, ev.data.kind
+--     if name == "cursortab.nvim" and (kind == "install" or kind == "update") then
+--       Print(ev.data.path .. "/server")
+--       vim.system({ "go", "build" }, { cwd = ev.data.path .. "/server" })
+--     end
+--   end,
+-- })
 
-lazy.on_event("https://github.com/cursortab/cursortab.nvim", "BufRead", function()
-  vim.api.nvim_set_hl(0, "CursorTabAddition", { bg = "#1a3a1a" })
-  require("cursortab").setup {
-    provider = {
-      -- type = "copilot",
-      type = "zeta-2",
-      url = "http://localhost:8000",
-      temperature = 0.0,
-      context_size = 2048,
-      max_tokens = 64,
-      top_k = 40,
-      completion_timeout = 30000,
-      max_diff_history_tokens = 256,
-    },
-    keymaps = {
-      accept = "<C-f>",
-    },
-    ui = {
-      jump = {
-        symbol = "",
-        text = " Ctrl + F ",
-        show_distance = true,
-      },
-    },
-  }
-end)
+-- lazy.on_event("https://github.com/cursortab/cursortab.nvim", "BufRead", function()
+--   vim.api.nvim_set_hl(0, "CursorTabAddition", { bg = "#1a3a1a" })
+--   require("cursortab").setup {
+--     provider = {
+--       type = "copilot",
+--     },
+--     keymaps = {
+--       accept = "<C-f>",
+--     },
+--     ui = {
+--       jump = {
+--         symbol = "",
+--         text = " Ctrl + F ",
+--         show_distance = true,
+--       },
+--     },
+--   }
+-- end)
--- lua/pack/plugins/sidekick.lua
+++ lua/pack/plugins/sidekick.lua
@@ -1,27 +1,27 @@
--- vim.pack.add { "https://github.com/folke/sidekick.nvim" }
+vim.pack.add { "https://github.com/folke/sidekick.nvim" }
 
--- vim.api.nvim_create_autocmd("InsertEnter", {
---   callback = function()
---     require("sidekick").setup {
---       nes = {
---         enabled = false,
---       },
---     }
+vim.api.nvim_create_autocmd("InsertEnter", {
+  callback = function()
+    require("sidekick").setup {
+      nes = {
+        enabled = false,
+      },
+    }
 
---     -- require("which-key").add {
---     --   {
---     --     "<C-f>",
---     --     function() require("sidekick").nes_jump_or_apply() end,
---     --     expr = true,
---     --     desc = "Goto/Apply Next Edit Suggestion",
---     --   },
---     --   {
---     --     "<C-g>",
---     --     function() require("sidekick.nes").update() end,
---     --     expr = true,
---     --     desc = "Sidekick Update Suggestions",
---     --   },
---     -- }
---   end,
---   once = true,
--- })
+    require("which-key").add {
+      {
+        "<C-f>",
+        function() require("sidekick").nes_jump_or_apply() end,
+        expr = true,
+        desc = "Goto/Apply Next Edit Suggestion",
+      },
+      {
+        "<C-g>",
+        function() require("sidekick.nes").update() end,
+        expr = true,
+        desc = "Sidekick Update Suggestions",
+      },
+    }
+  end,
+  once = true,
+})
