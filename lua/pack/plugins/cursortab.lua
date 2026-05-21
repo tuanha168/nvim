f0530fd0f62c441bf90c54ddf27cb743e10edcd2
yopaz-tuanha <tuanha@yopaz.vn>
2026-05-21 14:45:13 +0700

    Auto Update: 2026-05-21 14:45:12 from Arch Linux

---
+++ lua/pack/plugins/cursortab.lua
@@ -0,0 +1,25 @@
+local lazy = require("pack.lazy-load")
+
+vim.api.nvim_create_autocmd("PackChanged", {
+  callback = function(ev)
+    local name, kind = ev.data.spec.name, ev.data.kind
+    if name == "peek.nvim" and (kind == "install" or kind == "update") then
+      vim.system({ "deno", "task", "--quiet", "build:fast" }, { cwd = ev.data.path })
+    end
+  end,
+})
+
+lazy.on_event(
+  "https://github.com/toppair/peek.nvim",
+  "FileType",
+  function()
+    require("peek").setup()
+    vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+    vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+  end,
+  { pattern = "markdown" }
+)
+
+require("which-key").add({
+  { "<leader>mp", "<cmd>PeekOpen<cr>", desc = "Markdown Preview Open" },
+})
