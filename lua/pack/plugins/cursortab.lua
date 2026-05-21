2da70a081b4bb5ffaf87e8c82c2072ae14a2d9bd
yopaz-tuanha <tuanha@yopaz.vn>
2026-05-21 15:08:37 +0700

    Auto Update: 2026-05-21 15:08:37 from Arch Linux

--- lua/pack/plugins/cursortab.lua
+++ lua/pack/plugins/cursortab.lua
@@ -19,7 +19,7 @@ lazy.on_event("https://github.com/cursortab/cursortab.nvim", "BufRead", function
       -- url = "http://localhost:8000",
     },
     keymaps = {
-      accept = "<leader>f",
+      accept = "<C-f>",
     },
     ui = {
       jump = {
