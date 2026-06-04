b9274b1fd9775605163edb1ff562435abc1bc0a7
yopaz-tuanha <tuanha@yopaz.vn>
2026-06-04 15:33:46 +0700

    Auto Update: 2026-06-04 15:33:46 from Arch Linux x86_64

--- lua/pack/plugins/markdown.lua
+++ lua/pack/plugins/markdown.lua
@@ -207,7 +207,7 @@ vim.api.nvim_create_autocmd("PackChanged", {
 
 lazy.on_event("https://github.com/toppair/peek.nvim", "FileType", function()
   require("peek").setup {
-    app = "browser",
+    app = "webview",
   }
   vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
   vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
