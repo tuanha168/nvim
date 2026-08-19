b13d031c584c3ace6095ef3b5084de55df17cc05
yopaz-tuanha <tuanha@yopaz.vn>
2026-08-19 13:14:53 +0700

    Auto Update: 2026-08-19 13:14:53 from Arch Linux x86_64

--- lua/pack/plugins/herdr-splits.lua
+++ lua/pack/plugins/herdr-splits.lua
@@ -1,13 +1,13 @@
-local ss = require("smart-splits")
+local hs = require("herdr-splits")
 
-ss.setup()
+hs.setup()
 
-vim.keymap.set("n", "<A-h>", ss.resize_left,  { desc = "Resize left" })
-vim.keymap.set("n", "<A-j>", ss.resize_down,  { desc = "Resize down" })
-vim.keymap.set("n", "<A-k>", ss.resize_up,    { desc = "Resize up" })
-vim.keymap.set("n", "<A-l>", ss.resize_right, { desc = "Resize right" })
+vim.keymap.set("n", "<A-h>", hs.resize_left,  { desc = "Resize left" })
+vim.keymap.set("n", "<A-j>", hs.resize_down,  { desc = "Resize down" })
+vim.keymap.set("n", "<A-k>", hs.resize_up,    { desc = "Resize up" })
+vim.keymap.set("n", "<A-l>", hs.resize_right, { desc = "Resize right" })
 
-vim.keymap.set("n", "<C-h>", ss.move_cursor_left,  { desc = "Move to left split" })
-vim.keymap.set("n", "<C-j>", ss.move_cursor_down,  { desc = "Move to down split" })
-vim.keymap.set("n", "<C-k>", ss.move_cursor_up,    { desc = "Move to up split" })
-vim.keymap.set("n", "<C-l>", ss.move_cursor_right, { desc = "Move to right split" })
+vim.keymap.set("n", "<C-h>", hs.move_cursor_left,  { desc = "Move to left split" })
+vim.keymap.set("n", "<C-j>", hs.move_cursor_down,  { desc = "Move to down split" })
+vim.keymap.set("n", "<C-k>", hs.move_cursor_up,    { desc = "Move to up split" })
+vim.keymap.set("n", "<C-l>", hs.move_cursor_right, { desc = "Move to right split" })
