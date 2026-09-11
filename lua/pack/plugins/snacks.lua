3f9e71fdc446f88e6c396c97c512b9876f414de8
yopaz-tuanha <tuanha@yopaz.vn>
2026-09-11 11:24:52 +0700

    Auto Update: 2026-09-11 11:24:52 from Arch Linux x86_64

--- lua/pack/plugins/snacks.lua
+++ lua/pack/plugins/snacks.lua
@@ -107,10 +107,10 @@ local keys = {
   { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
   { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
 
-  { "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "GitHub Issues (open)" },
-  { "<leader>gI", function() Snacks.picker.gh_issue { state = "all" } end, desc = "GitHub Issues (all)" },
-  { "<leader>gp", function() Snacks.picker.gh_pr() end, desc = "GitHub Pull Requests (open)" },
-  { "<leader>gP", function() Snacks.picker.gh_pr { state = "all" } end, desc = "GitHub Pull Requests (all)" },
+  -- { "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "GitHub Issues (open)" },
+  -- { "<leader>gI", function() Snacks.picker.gh_issue { state = "all" } end, desc = "GitHub Issues (all)" },
+  -- { "<leader>gp", function() Snacks.picker.gh_pr() end, desc = "GitHub Pull Requests (open)" },
+  -- { "<leader>gP", function() Snacks.picker.gh_pr { state = "all" } end, desc = "GitHub Pull Requests (all)" },
 
   -- Grep
   { "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
