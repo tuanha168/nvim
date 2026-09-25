d3ca810702fb4ae4819e368c49281aacde2d41eb
yopaz-tuanha <tuanha@yopaz.vn>
2026-09-25 11:44:35 +0700

    Auto Update: 2026-09-25 11:44:35 from Arch Linux x86_64

--- lua/pack/plugins/db-utilities.lua
+++ lua/pack/plugins/db-utilities.lua
@@ -1,14 +1,27 @@
 ---@type vim.SystemObj
 -- local dbUiSshConnection
 
-vim.pack.add {
-  "https://github.com/tpope/vim-dadbod",
-  "https://github.com/pbogut/vim-dadbod-ssh",
-  "https://github.com/kristijanhusak/vim-dadbod-ui",
-  "https://github.com/kristijanhusak/vim-dadbod-completion",
-}
+vim.api.nvim_create_autocmd("PackChanged", {
+  callback = function(ev)
+    if ev.data.spec.name == "sqmeow.nvim" and (ev.data.kind == "install" or ev.data.kind == "update") then
+      require("sqmeow").install {
+        query = {
+          max_rows = 100,
+          timeout_ms = 5000, -- 0 disables timeout
+          history_size = 32, -- results kept in memory
+          persist_history = true, -- save log and results to disk
+          history_limit = 500,
+          confirm_destructive = true, -- ask before destructive statements
+        },
+      }
+    end
+  end,
+})
 
-vim.g.db_ui_use_nerd_fonts = 1
+vim.pack.add {
+  "https://github.com/MunifTanjim/nui.nvim",
+  { src = "https://github.com/2giosangmitom/sqmeow.nvim", version = vim.version.range "*" },
+}
 
 -- vim.api.nvim_create_autocmd("VimLeavePre", {
 --   pattern = "*",
@@ -23,5 +36,5 @@ vim.keymap.set("n", "<leader>cdb", function()
   --   local obj = vim.system({ "ssh", "-L", "7000:localhost:3306", "-N", "test-ec2" }, { text = true })
   --   dbUiSshConnection = obj
   -- end
-  vim.cmd.DBUIToggle()
-end, { desc = "DBUIToggle" })
+  vim.cmd "Sqmeow toggle"
+end, { desc = "Toggle database UI" })
