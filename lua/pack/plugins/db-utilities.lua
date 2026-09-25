9d2a0c74050f621fd997f57394a095c345cdd3e5
yopaz-tuanha <tuanha@yopaz.vn>
2026-09-25 11:51:28 +0700

    Auto Update: 2026-09-25 11:51:28 from Arch Linux x86_64

--- lua/pack/plugins/db-utilities.lua
+++ lua/pack/plugins/db-utilities.lua
@@ -1,29 +1,35 @@
----@type vim.SystemObj
+local lazy = require "pack.lazy-load"
 
-vim.api.nvim_create_autocmd("PackChanged", {
-  callback = function(ev)
-    if ev.data.spec.name == "sqmeow.nvim" and (ev.data.kind == "install" or ev.data.kind == "update") then
-      require("sqmeow").install { callback = function() end }
-    end
-  end,
-})
-
-vim.pack.add {
+local plugin = {
   "https://github.com/MunifTanjim/nui.nvim",
   { src = "https://github.com/2giosangmitom/sqmeow.nvim", version = vim.version.range "*" },
 }
 
-require("sqmeow").setup {
-  query = {
-    max_rows = 100,
-    timeout_ms = 5000, -- 0 disables timeout
-    history_size = 32, -- results kept in memory
-    persist_history = true, -- save log and results to disk
-    history_limit = 500,
-    confirm_destructive = true, -- ask before destructive statements
-  },
-}
+local function setup_sqmeow()
+  require("sqmeow").setup {
+    query = {
+      max_rows = 100,
+      timeout_ms = 5000, -- 0 disables timeout
+      history_size = 32, -- results kept in memory
+      persist_history = true, -- save log and results to disk
+      history_limit = 500,
+      confirm_destructive = true, -- ask before destructive statements
+    },
+  }
+end
 
-vim.keymap.set("n", "<leader>cdb", function()
-  vim.cmd "Sqmeow toggle"
-end, { desc = "Toggle database UI" })
+vim.api.nvim_create_autocmd("PackChanged", {
+  callback = function(ev)
+    if ev.data.spec.name == "sqmeow.nvim" and (ev.data.kind == "install" or ev.data.kind == "update") then
+      require("sqmeow").install()
+    end
+  end,
+})
+
+lazy.on_key(plugin, {
+  {
+    "<leader>cdb",
+    function() vim.cmd "Sqmeow toggle" end,
+    desc = "Toggle database UI",
+  },
+}, setup_sqmeow)
--- lua/pack/plugins/markdown.lua
+++ lua/pack/plugins/markdown.lua
@@ -1,24 +1,16 @@
 local lazy = require "pack.lazy-load"
 
-local md_preview_plugin = "https://github.com/selimacerbas/markdown-preview.nvim"
-local live_server_plugin = "https://github.com/selimacerbas/live-server.nvim"
+local plugin = "https://github.com/blackhat-7/vellum.nvim"
 
-local function setup_markdown_preview()
-  require("markdown_preview").setup {
-    instance_mode = "takeover",
-    port = 0,
-    open_browser = true,
-    default_theme = "dark",
-    debounce_ms = 300,
-  }
-end
+vim.api.nvim_create_autocmd("PackChanged", {
+  callback = function(ev)
+    if ev.data.spec.name ~= "vellum.nvim" then return end
+    if ev.data.kind ~= "install" and ev.data.kind ~= "update" then return end
 
-lazy.on_key(md_preview_plugin, {
-  { "<leader>mp", "<cmd>MarkdownPreview<cr>", desc = "Markdown Preview Open" },
-  { "<leader>mps", "<cmd>MarkdownPreview<cr>", desc = "Markdown Preview Start" },
-  { "<leader>mpS", "<cmd>MarkdownPreviewStop<cr>", desc = "Markdown Preview Stop" },
-  { "<leader>mpr", "<cmd>MarkdownPreviewRefresh<cr>", desc = "Markdown Preview Refresh" },
-}, function()
-  vim.pack.add({ live_server_plugin }, { load = true })
-  setup_markdown_preview()
-end)
+    dofile(ev.data.path .. "/build.lua")
+  end,
+})
+
+lazy.on_key(plugin, {
+  { "<leader>mp", "<cmd>Vellum<cr>", desc = "Markdown Preview" },
+}, function() require("vellum").setup() end)
