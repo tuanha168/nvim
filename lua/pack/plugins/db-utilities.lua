299a8b02e889df6046588411868f9f32b2bbc781
yopaz-tuanha <tuanha@yopaz.vn>
2026-09-25 11:49:21 +0700

    Auto Update: 2026-09-25 11:49:21 from Arch Linux x86_64

--- lua/pack/plugins/db-utilities.lua
+++ lua/pack/plugins/db-utilities.lua
@@ -1,19 +1,9 @@
 ---@type vim.SystemObj
--- local dbUiSshConnection
 
 vim.api.nvim_create_autocmd("PackChanged", {
   callback = function(ev)
     if ev.data.spec.name == "sqmeow.nvim" and (ev.data.kind == "install" or ev.data.kind == "update") then
-      require("sqmeow").install {
-        query = {
-          max_rows = 100,
-          timeout_ms = 5000, -- 0 disables timeout
-          history_size = 32, -- results kept in memory
-          persist_history = true, -- save log and results to disk
-          history_limit = 500,
-          confirm_destructive = true, -- ask before destructive statements
-        },
-      }
+      require("sqmeow").install { callback = function() end }
     end
   end,
 })
@@ -23,18 +13,17 @@ vim.pack.add {
   { src = "https://github.com/2giosangmitom/sqmeow.nvim", version = vim.version.range "*" },
 }
 
--- vim.api.nvim_create_autocmd("VimLeavePre", {
---   pattern = "*",
---   callback = function()
---     if dbUiSshConnection then dbUiSshConnection:kill(9) end
---   end,
--- })
+require("sqmeow").setup {
+  query = {
+    max_rows = 100,
+    timeout_ms = 5000, -- 0 disables timeout
+    history_size = 32, -- results kept in memory
+    persist_history = true, -- save log and results to disk
+    history_limit = 500,
+    confirm_destructive = true, -- ask before destructive statements
+  },
+}
 
 vim.keymap.set("n", "<leader>cdb", function()
-  -- if not dbUiSshConnection then
-  --   -- ssh -L 7000:localhost:3306 -N yopaz-dev
-  --   local obj = vim.system({ "ssh", "-L", "7000:localhost:3306", "-N", "test-ec2" }, { text = true })
-  --   dbUiSshConnection = obj
-  -- end
   vim.cmd "Sqmeow toggle"
 end, { desc = "Toggle database UI" })
